ade0ad76387b32d7f3d73378b5667064e772f274
JoeDeVries <joe.devries@tdl.org>
2012-05-16 15:54:20 -0500

    Fixed a typo

--- bamboo.sh
+++ bamboo.sh
@@ -6,7 +6,7 @@ export PLAY_HOME=/tmp/play-1.2.5-RC3
 export PATH=$PATH:$PLAY_HOME
 curl -L -k -u TDLVireo:Vireo20 https://github.com/TexasDigitalLibrary/Vireo/zipball/master -o vireo.zip
 unzip vireo.zip
-mv cd TexasDigitalLibrary-Vireo-* vireo
+mv TexasDigitalLibrary-Vireo-* vireo
 cd vireo
 play dependencies
 play auto-test
