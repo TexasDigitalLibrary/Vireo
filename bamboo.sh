49c6947e2ef438d0e325912bebadeb147706f5d9
JoeDeVries <joe.devries@tdl.org>
2012-05-16 10:41:57 -0500

    Fixed bamboo build

--- bamboo.sh
+++ bamboo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd /tmp
 curl http://download.playframework.org/releases/play-1.2.5-RC3.zip -o /tmp/play-1.2.5-RC3.zip
-unzip /tmp/play-1.2.5-RC3.zip
+unzip /tmp/play-1.2.5-RC3.zip -d /tmp
 export PLAY_HOME=/tmp/play-1.2.5-RC3
 export PATH=$PATH:$PLAY_HOME
+sudo yum -y install git
 git clone https://TDLVireo:Vireo20@github.com/TexasDigitalLibrary/Vireo.git Vireo
 cd Vireo
 play dependencies
