44d51afab4d5dcbeb0bcba0352c2dd13ee2dbd1e
Joe DeVries <joe.devries@austin.utexas.edu>
2012-05-16 23:04:35 -0500

    Another bamboo attempt

--- bamboo.sh
+++ bamboo.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-rm -rf /tmp/play-1.2.5rc3/
-rm /tmp/play-1.2.5-RC3.zip
-curl http://download.playframework.org/releases/play-1.2.5-RC3.zip -o /tmp/play-1.2.5-RC3.zip
-unzip /tmp/play-1.2.5-RC3.zip -d /tmp
-#export PLAY_HOME=/tmp/play-1.2.5rc3
-#export PATH=$PATH:$PLAY_HOME
+rm -rf /tmp/play/
+rm /tmp/play.zip
+curl http://download.playframework.org/releases/play-1.2.5-RC3.zip -o /tmp/play.zip
+#curl -L -k https://github.com/playframework/play/zipball/master -o /tmp/play.zip
+unzip /tmp/play.zip -d /tmp/
+mv /tmp/play* /tmp/play
+export PATH=$PATH:/tmp/play
 rm -rf ./vireo/
 rm ./vireo.zip
 curl -L -k -u TDLVireo:Vireo20 https://github.com/TexasDigitalLibrary/Vireo/zipball/master -o ./vireo.zip
 unzip ./vireo.zip
 mv ./TexasDigitalLibrary-Vireo-* ./vireo
 cd ./vireo
-/tmp/play-1.2.5rc3/play dependencies
-/tmp/play-1.2.5rc3/play auto-test
-/tmp/play-1.2.5rc3/play war
+/tmp/play/play dependencies
+/tmp/play/play auto-test
