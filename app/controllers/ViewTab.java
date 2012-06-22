953f3f806a55940346f1173b4ef691be1a4effaa
Micah Cooper <jmicah@library.tamu.edu>
2012-06-22 08:48:23 -0500

    Fixed css for submission footer.

--- app/controllers/ViewTab.java
+++ app/controllers/ViewTab.java
@@ -2,12 +2,33 @@ package controllers;
 
 import play.mvc.Controller;
 
-public class ViewTab extends Controller {
+import org.tdl.vireo.model.DegreeLevel;
+import org.tdl.vireo.model.Person;
+import org.tdl.vireo.model.RoleType;
+import org.tdl.vireo.model.Submission;
 
-	
-	public static void view() {
+import play.modules.spring.Spring;
+import play.mvc.With;
+
+import java.text.DateFormatSymbols;
+
+@With(Authentication.class)
+public class ViewTab extends AbstractVireoController {
+
+	@Security(RoleType.REVIEWER)
+	public static void view() {		
+				
+		//TODO Find id by session.
+		long id = 1;
+		Submission submission = subRepo.findSubmission(id);
+		Person submitter = submission.getSubmitter();
+		
+		DegreeLevel degreeLevel = settingRepo.findDegreeByName(submission.getDegree()).getLevel();
+		
+		String gradMonth = new DateFormatSymbols().getMonths()[submission.getGraduationMonth()];
+		
 		String nav = "view";
-		render(nav);
+		render("Review/view.html", nav, submission, submitter, degreeLevel, gradMonth);
 	}
-
+	
 }
