79452d498868cbced2ad72f302781479e3b307d3
Scott Phillips <scott@scottphillips.com>
2012-07-02 15:03:17 -0500

    Added a system control panel for debugging and admin features like sending test emails, or rebuilding the index.

---
+++ app/controllers/System.java
@@ -0,0 +1,171 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.lucene.document.Field.Index;
+import org.tdl.vireo.model.AbstractModel;
+import org.tdl.vireo.model.EmailTemplate;
+import org.tdl.vireo.model.RoleType;
+import org.tdl.vireo.model.SettingsRepository;
+import org.tdl.vireo.search.Indexer;
+import org.tdl.vireo.search.impl.LuceneIndexerImpl;
+import org.tdl.vireo.services.EmailService;
+import org.tdl.vireo.services.SystemEmailTemplateService;
+import org.tdl.vireo.services.EmailService.TemplateParameters;
+
+import play.Play;
+import play.db.jpa.JPA;
+import play.modules.spring.Spring;
+import play.mvc.Router;
+import play.mvc.Router.ActionDefinition;
+
+/**
+ * System administrator control panel.
+ * 
+ * This is a way for administrators to access system information for debugging
+ * or testing. It is expected that this class will gain additional features and
+ * information feeds in the future.
+ * 
+ * @author <a href="http://www.scottphillips.com">Scott Phillips</a>
+ */
+public class System extends AbstractVireoController {
+
+	// The email template to use when sending a test email.
+	public static final String TEST_EMAIL_TEMPLATE = "SYSTEM Email Test";
+
+	// Spring dependencies indjected.
+	public static Indexer indexer = Spring.getBeanOfType(Indexer.class);
+	public static EmailService emailService = Spring.getBeanOfType(EmailService.class);
+	public static SystemEmailTemplateService templateService = Spring.getBeanOfType(SystemEmailTemplateService.class);
+	public static SettingsRepository settingRepo = Spring.getBeanOfType(SettingsRepository.class);
+	
+	/**
+	 * Redirect "system/" to "system/panel"
+	 */
+	@Security(RoleType.ADMINISTRATOR)
+	public static void controlPanelRedirect() {
+		controlPanel();
+	}
+	
+	/**
+	 * Display vital statistic information about vireo and allow access to some
+	 * administrator features.
+	 */
+	@Security(RoleType.ADMINISTRATOR)
+	public static void controlPanel() {
+
+		Runtime runtime = Runtime.getRuntime();	
+
+		
+		// Java Information
+		String javaVersion = java.lang.System.getProperty("java.version");
+		String osName = java.lang.System.getProperty("os.name");
+		String osArch = java.lang.System.getProperty("os.arch");
+		String osVersion = java.lang.System.getProperty("os.version");
+		int availableProcessors = runtime.availableProcessors();
+		
+		// Runtime Memory Information
+		long maximumMemory = runtime.maxMemory() / 1024 / 1024;
+		long allocatedMemory = runtime.totalMemory() / 1024 / 1024;
+		long freeMemory = runtime.freeMemory() / 1024 / 1024;
+		long usedMemory = allocatedMemory - freeMemory / 1024 / 1024;
+
+		// Play Information
+		String playMode = Play.mode.name();
+		String frameworkId = Play.id;
+		Date startedAt = new Date(Play.startedAt);
+		String httpAddress = Play.configuration.getProperty("http.address","n/a");
+		String httpPort = Play.configuration.getProperty("http.port","n/a");
+		String httpPath = Play.configuration.getProperty("http.path","n/a");
+		String logLevel = Play.configuration.getProperty("application.log","n/a");
+		String db = Play.configuration.getProperty("db","n/a");
+		String appPath = Play.applicationPath.getPath();
+		String attachmentsPath = Play.configuration.getProperty("attachments.path","n/a");
+		String indexPath = Play.configuration.getProperty("index.path","n/a");
+
+
+		// Mail Information
+		String mailMode = Play.configuration.getProperty("mail.smtp","n/a");
+		String mailHost = Play.configuration.getProperty("mail.smtp.host","n/a");
+		String mailUser = Play.configuration.getProperty("mail.smtp.user","n/a");
+		String mailPass = Play.configuration.getProperty("mail.smtp.pass");
+		String mailChannel = Play.configuration.getProperty("mail.smtp.channel","n/a");
+		String mailFrom = Play.configuration.getProperty("mail.from","n/a");
+		String mailReply = Play.configuration.getProperty("mail.replyto","n/a");
+		
+		if (mailPass != null) {
+			mailPass = mailPass.replaceAll(".", "*");
+		} else {
+			mailPass = "n/a";
+		}
+	
+		// Index Information
+		String indexImpl = indexer.getClass().getSimpleName();
+		String indexJob = "n/a";
+		if (indexer.isJobRunning()) {
+			indexJob = indexer.getCurrentJobLabel();
+			
+			float complete = indexer.getCurrentJobProgress() / indexer.getCurrentJobTotal();
+			indexJob += " ("+complete+"% complete)";
+		}
+
+		render(
+				// Java Info
+				javaVersion, osName, osArch, osVersion, availableProcessors,
+				
+				// Memory Info
+				maximumMemory, allocatedMemory, freeMemory, usedMemory,
+				
+				// Play Info
+				playMode, frameworkId, httpAddress, httpPort, httpPath, startedAt, appPath, logLevel, db, attachmentsPath, indexPath,
+				
+				// Mail Info
+				mailMode, mailHost, mailUser, mailPass, mailChannel, mailFrom, mailReply,
+				
+				// Index Information
+				indexImpl, indexJob
+				
+				);
+	}
+	
+	/**
+	 * Send a test email to the provided address.
+	 * 
+	 * @param email The email address to send the test.
+	 */
+	@Security(RoleType.ADMINISTRATOR)
+	public static void testEmail(String email) {
+		
+		templateService.generateAllSystemEmailTemplates();
+		EmailTemplate template = settingRepo.findEmailTemplateByName(TEST_EMAIL_TEMPLATE);
+		
+		List<String> recipients = new ArrayList<String>();
+		recipients.add(email);
+		emailService.sendEmail(template, new TemplateParameters(), recipients, null);
+		
+		render();
+	}
+	
+	/**
+	 * Rebuild the index, either destructively or in place.
+	 */
+	@Security(RoleType.ADMINISTRATOR)
+	public static void rebuildIndex() {
+		
+		if (params.get("submit_rebuild") != null) {
+			indexer.rebuild(false);
+		} if (params.get("submit_deleteAndRebuild") != null) {
+			indexer.deleteAndRebuild(false);
+		}
+		
+		// Redirect back to the control pannel.
+		controlPanel();
+	}
+	
+	
+	
+	
+}
---
+++ test/controllers/SystemTest.java
@@ -0,0 +1,135 @@
+package controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+import org.tdl.vireo.model.Person;
+import org.tdl.vireo.model.PersonRepository;
+import org.tdl.vireo.model.RoleType;
+import org.tdl.vireo.model.jpa.JpaEmailTemplateImpl;
+import org.tdl.vireo.search.Indexer;
+import org.tdl.vireo.security.SecurityContext;
+
+import play.Play;
+import play.db.jpa.JPA;
+import play.libs.Mail;
+import play.modules.spring.Spring;
+import play.mvc.Http.Response;
+import play.mvc.Router;
+import play.test.FunctionalTest;
+
+/**
+ * Test the system control panel.
+ * 
+ * @author <a href="http://www.scottphillips.com">Scott Phillips</a>
+ */
+public class SystemTest extends AbstractVireoFunctionalTest {
+	
+	// Spring dependencies
+	public static Indexer indexer = Spring.getBeanOfType(Indexer.class);
+	
+	/**
+	 * Test viewing the control panel page
+	 */
+	@Test
+	public void testControlPanel() {
+		
+		final String PANEL_URL = Router.reverse("System.controlPanel").url;
+		
+		LOGIN();
+		
+		Response response = GET(PANEL_URL);
+		
+		assertContentMatch("System Control Panel", response);
+		assertContentMatch("Java Information", response);
+		assertContentMatch("Memory Usage", response);
+		assertContentMatch("Play Information", response);
+		assertContentMatch("Mail Information", response);
+		assertContentMatch("Index Information", response);
+
+		// Check that the three controls are on the page.
+		assertContentMatch("name=\"submit_testEmail\"", response);
+		assertContentMatch("name=\"submit_rebuild\"", response);
+		assertContentMatch("name=\"submit_deleteAndRebuild\"", response);
+	}
+	
+	/**
+	 * Test sending a... test email?
+	 */
+	@Test
+	public void testEmail() throws InterruptedException {
+		
+		// Clear out the mail queues.
+		Mail.Mock.reset();
+		
+		final String EMAIL_URL = Router.reverse("System.testEmail").url;
+		
+		LOGIN();
+		
+		Map<String,String> params = new HashMap<String,String>();
+		params.put("email","test@email.com");
+		params.put("submit_testEmail","Send Test Email");
+		Response response = POST(EMAIL_URL,params);
+		
+		assertContentMatch("The test email has been sent.",response);
+		
+		String recieved = null;
+		for (int i = 0; i < 1000; i++) {
+			Thread.yield();
+			Thread.sleep(100);
+			recieved = Mail.Mock.getLastMessageReceivedBy("test@email.com");
+			if (recieved != null)
+				break;
+		}
+		assertNotNull(recieved);
+		assertTrue(recieved.contains("Subject: Vireo Email Test"));
+	}
+	
+	/** 
+	 * Test initiating an index rebuild from the control panel.
+	 */
+	@Test
+	public void testIndex() throws InterruptedException {
+		
+		final String PANEL_URL = Router.reverse("System.controlPanel").url;
+		final String EMAIL_URL = Router.reverse("System.rebuildIndex").url;
+		
+		LOGIN();
+		
+		// Step 1: Do a normal rebuild
+		Map<String,String> params = new HashMap<String,String>();
+		params.put("submit_rebuild","Rebuild Index");
+		Response response = POST(EMAIL_URL,params);
+		assertEquals(PANEL_URL, response.getHeader("location"));
+
+		assertTrue(indexer.isJobRunning());
+		assertEquals("Rebuild Index",indexer.getCurrentJobLabel());
+		
+		for (int i=0; i < 1000; i++) {
+			Thread.sleep(100);
+			if (!indexer.isJobRunning())
+				break;
+		}
+		assertFalse(indexer.isJobRunning());
+		
+		
+		// Step 2: Do a delete and rebuild
+		params.clear();
+		params.put("submit_deleteAndRebuild","Delete Index and Rebuild");
+		response = POST(EMAIL_URL,params);
+		assertEquals(PANEL_URL, response.getHeader("location"));
+
+		assertTrue(indexer.isJobRunning());
+		assertEquals("Rebuild Index",indexer.getCurrentJobLabel());
+		
+		for (int i=0; i < 1000; i++) {
+			Thread.sleep(100);
+			if (!indexer.isJobRunning())
+				break;
+		}
+		assertFalse(indexer.isJobRunning());
+	}
+}
