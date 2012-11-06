e01d9bd20b0f8c7af545c94a119f28c89856ab81
Scott Phillips <scott@scottphillips.com>
2012-11-05 20:22:51 -0600

    VIREO-103: Merge Application and Deposit Settings tabs.

--- test/controllers/settings/ApplicationSettingsTabTest.java
+++ test/controllers/settings/ApplicationSettingsTabTest.java
@@ -13,6 +13,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.tdl.vireo.model.Configuration;
 import org.tdl.vireo.model.CustomActionDefinition;
+import org.tdl.vireo.model.DepositLocation;
 import org.tdl.vireo.model.NameFormat;
 import org.tdl.vireo.model.Person;
 import org.tdl.vireo.model.PersonRepository;
@@ -397,6 +398,219 @@ public class ApplicationSettingsTabTest extends AbstractVireoFunctionalTest {
 		personRepo.findPerson(person.getId()).delete();
 		
 		
+	}
+	
+	
+	
+	
+	
+	/**
+	 * Test retrieving an updated list of deposit locations.
+	 */
+	@Test
+	public void testUpdateDepositLocationList() {
+		LOGIN();
+		
+		// Get our urls and a list of fields.
+		final String URL = Router.reverse("settings.ApplicationSettingsTab.updateDepositLocationList").url;
+
+		Response response = POST(URL);
+		
+		List<DepositLocation> locations = settingRepo.findAllDepositLocations();
+		for (DepositLocation location : locations) {
+			assertContentMatch(location.getName(),response);
+		}	
+	}
+	
+	/**
+	 * Test loading a deposit location dialog box.
+	 */
+	@Test
+	public void testLoadDepositLocation() {
+		LOGIN();
+		
+		// Get our urls and a list of fields.
+		final String URL = Router.reverse("settings.ApplicationSettingsTab.loadDepositLocation").url;
+
+		DepositLocation location = settingRepo.findAllDepositLocations().get(0);
+		
+		Map<String,String> params = new HashMap<String,String>();
+		params.put("depositLocationId", "depositLocation_"+location.getId());
+		Response response = POST(URL,params);
+		
+		assertContentMatch(location.getName(),response);
+		assertContentMatch("value=\""+location.getPackager().getBeanName()+"\" selected=\"true\"",response);
+		assertContentMatch(location.getRepository(),response);
+		assertContentMatch(location.getCollection(),response);
+		assertContentMatch(location.getUsername(),response);
+	}
+	
+	/**
+	 * Test saving a deposit location
+	 */
+	@Test
+	public void testSaveAndDeleteDepositLocation() {
+		
+		JPA.em().getTransaction().commit();
+		JPA.em().clear();
+		JPA.em().getTransaction().begin();
+		LOGIN();
+		
+		// Get our urls and a list of fields.
+		final String SAVE_URL = Router.reverse("settings.ApplicationSettingsTab.saveDepositLocation").url;
+		final String REMOVE_URL = Router.reverse("settings.ApplicationSettingsTab.removeDepositLocationJSON").url;
+
+		
+		Map<String,String> params = new HashMap<String,String>();
+		params.put("depositLocationId", "");
+		params.put("name","New Deposit Location");
+		params.put("packager","DSpaceMETS");
+		params.put("depositor","Sword1Deposit");
+		params.put("repository","http://localhost:8082/servicedocument");
+		params.put("username","testUser");
+		params.put("password","testPassword");
+		params.put("onBehalfOf","");
+		params.put("collection","http://localhost:8082/deposit/a");
+		Response response = POST(SAVE_URL,params);
+				
+		assertContentMatch("New Deposit Location",response);
+		assertContentMatch("packager",response);
+		assertContentMatch("DSpaceMETS",response);
+		assertContentMatch("Sword1Deposit",response);
+		assertContentMatch("servicedocument",response);
+		assertContentMatch("testUser",response);
+
+		// Extract the id of the newly created location.
+		Pattern ID_PATTERN = Pattern.compile("id=\"depositLocation-id\" value=\"depositLocation_([0-9]+)\"");
+		Matcher tokenMatcher = ID_PATTERN.matcher(getContent(response));
+		assertTrue(tokenMatcher.find());
+		String idString = tokenMatcher.group(1);
+		assertNotNull(idString);
+		Long id = Long.valueOf(idString);
+		
+		// Check if the location exists
+		JPA.em().getTransaction().commit();
+		JPA.em().clear();
+		JPA.em().getTransaction().begin();
+		DepositLocation location = settingRepo.findDepositLocation(id);
+		
+		assertNotNull(location);
+		assertEquals("New Deposit Location",location.getName());
+		
+		
+		// Try and delete 
+		params.clear();
+		params.put("depositLocationId", "depositLocation_"+location.getId());
+		response = POST(REMOVE_URL,params);
+		assertContentMatch("\"success\": \"true\"", response);
+		
+		// Check that it was deleted in the database.
+		JPA.em().getTransaction().commit();
+		JPA.em().clear();
+		JPA.em().getTransaction().begin();
+		assertNull(settingRepo.findDepositLocation(id));
+	}
+	
+	/**
+	 * Test reordering a set of deposit locations
+	 */
+	@Test
+	public void testReorderingTemplates() {
+		LOGIN();
+		
+		final String REORDER_URL = Router.reverse("settings.ApplicationSettingsTab.reorderDepositLocationsJSON").url;
+		
+		// Create two custom actions:
+		DepositLocation location1 = settingRepo.createDepositLocation("one").save();
+		DepositLocation location2 = settingRepo.createDepositLocation("two").save();
+		
+		JPA.em().getTransaction().commit();
+		JPA.em().clear();
+		JPA.em().getTransaction().begin();
+		
+		// Reorder the custom actions
+		Map<String,String> params = new HashMap<String,String>();
+		params.put("depositLocationIds", "depositLocation_"+location2.getId()+",depositLocation_"+location1.getId());
+		Response response = POST(REORDER_URL,params);
+		assertContentMatch("\"success\": \"true\"", response);
+		
+		// Verify that the actions were reorderd
+		JPA.em().getTransaction().commit();
+		JPA.em().clear();
+		JPA.em().getTransaction().begin();
+		location1 = settingRepo.findDepositLocation(location1.getId());
+		location2 = settingRepo.findDepositLocation(location2.getId());
+		assertTrue(location1.getDisplayOrder() > location2.getDisplayOrder());
+		
+		// Cleanup
+		location1.delete();
+		location2.delete();
+	}
+	
+	/**
+	 * Test submiting a test item to a test sword server... you know test the
+	 * test. But really it's broken several times now and no one noticed, so I'm
+	 * adding a test so we know when it breaks.
+	 */
+	@Test
+	public void testDepositingSubmission() {
+		LOGIN();
+		
+		// Get our urls and a list of fields.
+		final String SAVE_URL = Router.reverse("settings.ApplicationSettingsTab.saveDepositLocation").url;
+		final String REMOVE_URL = Router.reverse("settings.ApplicationSettingsTab.removeDepositLocationJSON").url;
+
+		
+		Map<String,String> params = new HashMap<String,String>();
+		params.put("depositLocationId", "");
+		params.put("name","New Deposit Location");
+		params.put("packager","DSpaceMETS");
+		params.put("depositor","Sword1Deposit");
+		params.put("repository","http://localhost:8082/servicedocument");
+		params.put("username","testUser");
+		params.put("password","testPassword");
+		params.put("onBehalfOf","");
+		params.put("collection","http://localhost:8082/deposit/a");
+		params.put("action","depositLocation-test-submit");
+		Response response = POST(SAVE_URL,params);
+				
+		assertContentMatch("New Deposit Location",response);
+		assertContentMatch("packager",response);
+		assertContentMatch("DSpaceMETS",response);
+		assertContentMatch("Sword1Deposit",response);
+		assertContentMatch("servicedocument",response);
+		assertContentMatch("testUser",response);
+		assertContentMatch("Test item successfully deposited: ",response);
+
+		// Extract the id of the newly created location.
+		Pattern ID_PATTERN = Pattern.compile("id=\"depositLocation-id\" value=\"depositLocation_([0-9]+)\"");
+		Matcher tokenMatcher = ID_PATTERN.matcher(getContent(response));
+		assertTrue(tokenMatcher.find());
+		String idString = tokenMatcher.group(1);
+		assertNotNull(idString);
+		Long id = Long.valueOf(idString);
+		
+		// Check if the location exists
+		JPA.em().getTransaction().commit();
+		JPA.em().clear();
+		JPA.em().getTransaction().begin();
+		DepositLocation location = settingRepo.findDepositLocation(id);
+		
+		assertNotNull(location);
+		assertEquals("New Deposit Location",location.getName());
+		
+		
+		// Try and delete 
+		params.clear();
+		params.put("depositLocationId", "depositLocation_"+location.getId());
+		response = POST(REMOVE_URL,params);
+		assertContentMatch("\"success\": \"true\"", response);
+		
+		// Check that it was deleted in the database.
+		JPA.em().getTransaction().commit();
+		JPA.em().clear();
+		JPA.em().getTransaction().begin();
+		assertNull(settingRepo.findDepositLocation(id));
 	}
 	
 	
