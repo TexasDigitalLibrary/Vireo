abcf03b71c8456c559e398ef8b1b3d3097b162d5
Scott Phillips <scott@scottphillips.com>
2012-12-17 15:06:49 -0600

    VIREO-114: IE 8 opening attachments

--- app/controllers/ViewTab.java
+++ app/controllers/ViewTab.java
@@ -1007,8 +1007,12 @@ public class ViewTab extends AbstractVireoController {
 		Attachment attachment = sub.findAttachmentById(id);
 		response.setContentTypeIfNotSet(attachment.getMimeType());
 		
+		// Fix problem with no-cache headers and ie8
+		response.setHeader("Pragma", "public");
+		response.setHeader("Cache-Control","public");
+		
 		try {
-			renderBinary( new FileInputStream(attachment.getFile()), attachment.getFile().length());
+			renderBinary(new FileInputStream(attachment.getFile()), attachment.getName(), attachment.getFile().length(), true);
 		} catch (FileNotFoundException ex) {
 			error("File not found");
 		}
