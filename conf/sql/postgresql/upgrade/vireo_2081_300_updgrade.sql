f2a787cbebe1d7a29a9ce2b1cbd624f98f64ddb0
Gad Krumholz <gad.krumholz@austin.utexas.edu>
2015-02-21 16:06:04 -0600

    converted the 2 temporary functions into a single DO function for vireo_2081_300_updgrade.sql

--- conf/sql/postgresql/upgrade/vireo_2081_300_updgrade.sql
+++ conf/sql/postgresql/upgrade/vireo_2081_300_updgrade.sql
@@ -1,38 +1,29 @@
--- Create a temporary function to help remove the old unique constraint in embargo_type that Hibernate 4.2 won't update with jpa.ddl=update
+-- DO function to help remove the old unique constraint (name) in embargo_type that Hibernate 4.2 won't update even with jpa.ddl=update
 -- It will also upgrade the embargo_type schema (adds the 'guarantor' column)
-CREATE OR REPLACE FUNCTION remove_unique_constraint_embargo_type() RETURNS TEXT AS $$
+-- It will lastly add the new UNIQUE CONSTRAINT against both (name, guarantor)
+DO $$
 DECLARE
-   _con TEXT;
+   _constraint_name TEXT;
 BEGIN
 
-_con := (SELECT constraint_name FROM information_schema.constraint_table_usage
-      WHERE table_name = 'embargo_type'
-      AND constraint_name LIKE 'uk%'
-      LIMIT 1);
+   _constraint_name := (SELECT constraint_name
+                        FROM information_schema.key_column_usage
+                        WHERE table_name = 'embargo_type'
+                        AND constraint_name LIKE 'uk%'
+                        AND column_name = 'name'
+                        LIMIT 1); -- there should only be 1
 
    EXECUTE '
-      ALTER TABLE embargo_type DROP CONSTRAINT ' || _con;
+      ALTER TABLE embargo_type DROP CONSTRAINT ' || _constraint_name;
 
    EXECUTE '
       ALTER TABLE embargo_type
       ADD guarantor integer DEFAULT 0 NOT NULL';
 
-RETURN _con;
-END;
-$$
-language plpgsql;
--- Create a temporary function to help add a new unique constraint in embargo_type that Hibernate 4.2 won't update with jpa.ddl=update
-CREATE OR REPLACE FUNCTION add_unique_constraint_embargo_type(_con TEXT) RETURNS TEXT AS $$
-BEGIN
-    EXECUTE '
+   EXECUTE '
       ALTER TABLE ONLY embargo_type
-      ADD CONSTRAINT ' || quote_ident(_con) || ' UNIQUE (name, guarantor)';
-RETURN _con;
+      ADD CONSTRAINT ' || _constraint_name || ' UNIQUE (name, guarantor)';
+
 END;
 $$
 language plpgsql;
--- Use the original name of the uk_% key to create a new one
-SELECT add_unique_constraint_embargo_type((SELECT remove_unique_constraint_embargo_type()));
--- Cleanup our temporary functions
-DROP FUNCTION remove_unique_constraint_embargo_type();
-DROP FUNCTION add_unique_constraint_embargo_type(_con TEXT);
