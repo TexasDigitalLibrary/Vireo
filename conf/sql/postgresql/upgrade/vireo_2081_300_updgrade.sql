f75b3e37d2b173e85d7a4b39f63babe940f5b8da
Gad Krumholz <gad.krumholz@austin.utexas.edu>
2015-02-23 11:13:49 -0600

    Updated vireo_2081_300_updgrade.sql to simplify updating production machines when their unique key didn't start with 'uk_%'

--- conf/sql/postgresql/upgrade/vireo_2081_300_updgrade.sql
+++ conf/sql/postgresql/upgrade/vireo_2081_300_updgrade.sql
@@ -1,6 +1,5 @@
--- DO function to help remove the old unique constraint (name) in embargo_type that Hibernate 4.2 won't update even with jpa.ddl=update
--- It will also upgrade the embargo_type schema (adds the 'guarantor' column)
--- It will lastly add the new UNIQUE CONSTRAINT against both (name, guarantor)
+-- DO function to help remove the old unique constraint (name) in embargo_type that Hibernate 4.2 won't remove even with jpa.ddl=update
+-- It needs to be a function because EXECUTE cannot have a sub-query within it.
 DO $$
 DECLARE
    _constraint_name TEXT;
@@ -9,20 +8,10 @@ BEGIN
    _constraint_name := (SELECT constraint_name
                         FROM information_schema.key_column_usage
                         WHERE table_name = 'embargo_type'
-                        AND constraint_name LIKE 'uk%'
                         AND column_name = 'name'
                         LIMIT 1); -- there should only be 1
 
-   EXECUTE '
-      ALTER TABLE embargo_type DROP CONSTRAINT ' || _constraint_name;
-
-   EXECUTE '
-      ALTER TABLE embargo_type
-      ADD guarantor integer DEFAULT 0 NOT NULL';
-
-   EXECUTE '
-      ALTER TABLE ONLY embargo_type
-      ADD CONSTRAINT ' || _constraint_name || ' UNIQUE (name, guarantor)';
+   EXECUTE 'ALTER TABLE embargo_type DROP CONSTRAINT ' || _constraint_name;
 
 END;
 $$
