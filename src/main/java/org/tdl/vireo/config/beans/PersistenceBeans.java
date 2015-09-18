411f747fe9c18556ed1b155172e77f65d1da1f39
Gad Krumholz <gad.krumholz@austin.utexas.edu>
2015-09-18 13:13:33 -0500

    Fixed JPA diagram, added h2 console to url /admin/h2console

---
+++ src/main/java/org/tdl/vireo/config/beans/PersistenceBeans.java
@@ -0,0 +1,17 @@
+package org.tdl.vireo.config.beans;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PersistenceBeans {
+	@Bean
+	public ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+		registrationBean.addUrlMappings("/admin/h2console/*");
+		registrationBean.addInitParameter("-webAllowOthers", "true");
+		return registrationBean;
+	}
+}
