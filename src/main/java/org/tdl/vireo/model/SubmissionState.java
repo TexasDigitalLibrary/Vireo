7c68c0b3e2e07b0a9abc1b4b262cbf66846f6fa8
Gad Krumholz <gad.krumholz@austin.utexas.edu>
2015-09-24 14:35:54 -0500

    Fixed SubmissionState transitionSubmissionState relationship

--- src/main/java/org/tdl/vireo/model/SubmissionState.java
+++ src/main/java/org/tdl/vireo/model/SubmissionState.java
@@ -9,193 +9,193 @@ import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class SubmissionState extends BaseEntity {
-	
-	@Column(columnDefinition = "TEXT", nullable = false)
-	private String name;
-	 
-	@Column(nullable = false)
-	private Boolean archived;
-	
-	@Column(nullable = false)
-	private Boolean publishable;
-	
-	@Column(nullable = false)
-	private Boolean deletable;
-	
-	@Column(nullable = false)
-	private Boolean editableByReviewer;
-	
-	@Column(nullable = false)
-	private Boolean editableByStudent;
-	
-	@Column(nullable = false)
-	private Boolean active;
-	
-	@ManyToOne(targetEntity = SubmissionState.class, cascade = { DETACH, REFRESH, MERGE }, optional = true)
-	private Set<SubmissionState> transitionSubmissionStates;
 
-	public SubmissionState() {
-		setTransitionSubmissionStates(new HashSet<SubmissionState>());
-	}
-	
-	/**
-	 * @param name
-	 * @param archived
-	 * @param publishable
-	 * @param deletable
-	 * @param editableByReviewer
-	 * @param editableByStudent
-	 * @param active
-	 */
-	public SubmissionState(String name, Boolean archived, Boolean publishable, Boolean deletable, Boolean editableByReviewer, Boolean editableByStudent, Boolean active) {
-		this();
-		setName(name);
-		setArchived(archived);
-		setPublishable(publishable);
-		setDeletable(deletable);
-		setEditableByReviewer(editableByReviewer);
-		setEditableByStudent(editableByStudent);
-		setActive(active);
-	}
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String name;
 
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
+    @Column(nullable = false)
+    private Boolean archived;
 
-	/**
-	 * @param name
-	 *            the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
+    @Column(nullable = false)
+    private Boolean publishable;
 
-	/**
-	 * @return the archived
-	 */
-	public Boolean getArchived() {
-		return archived;
-	}
+    @Column(nullable = false)
+    private Boolean deletable;
 
-	/**
-	 * @param archived
-	 *            the archived to set
-	 */
-	public void setArchived(Boolean archived) {
-		this.archived = archived;
-	}
+    @Column(nullable = false)
+    private Boolean editableByReviewer;
 
-	/**
-	 * @return the publishable
-	 */
-	public Boolean getPublishable() {
-		return publishable;
-	}
+    @Column(nullable = false)
+    private Boolean editableByStudent;
 
-	/**
-	 * @param publishable
-	 *            the publishable to set
-	 */
-	public void setPublishable(Boolean publishable) {
-		this.publishable = publishable;
-	}
+    @Column(nullable = false)
+    private Boolean active;
 
-	/**
-	 * @return the deletable
-	 */
-	public Boolean getDeletable() {
-		return deletable;
-	}
+    @ManyToMany(cascade = { DETACH, REFRESH, MERGE })
+    private Set<SubmissionState> transitionSubmissionStates;
 
-	/**
-	 * @param deletable
-	 *            the deletable to set
-	 */
-	public void setDeletable(Boolean deletable) {
-		this.deletable = deletable;
-	}
+    public SubmissionState() {
+        setTransitionSubmissionStates(new HashSet<SubmissionState>());
+    }
 
-	/**
-	 * @return the editableByReviewer
-	 */
-	public Boolean getEditableByReviewer() {
-		return editableByReviewer;
-	}
+    /**
+     * @param name
+     * @param archived
+     * @param publishable
+     * @param deletable
+     * @param editableByReviewer
+     * @param editableByStudent
+     * @param active
+     */
+    public SubmissionState(String name, Boolean archived, Boolean publishable, Boolean deletable, Boolean editableByReviewer, Boolean editableByStudent, Boolean active) {
+        this();
+        setName(name);
+        setArchived(archived);
+        setPublishable(publishable);
+        setDeletable(deletable);
+        setEditableByReviewer(editableByReviewer);
+        setEditableByStudent(editableByStudent);
+        setActive(active);
+    }
 
-	/**
-	 * @param editableByReviewer
-	 *            the editableByReviewer to set
-	 */
-	public void setEditableByReviewer(Boolean editableByReviewer) {
-		this.editableByReviewer = editableByReviewer;
-	}
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
 
-	/**
-	 * @return the editableByStudent
-	 */
-	public Boolean getEditableByStudent() {
-		return editableByStudent;
-	}
+    /**
+     * @param name
+     *            the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	/**
-	 * @param editableByStudent
-	 *            the editableByStudent to set
-	 */
-	public void setEditableByStudent(Boolean editableByStudent) {
-		this.editableByStudent = editableByStudent;
-	}
+    /**
+     * @return the archived
+     */
+    public Boolean getArchived() {
+        return archived;
+    }
 
-	/**
-	 * @return the active
-	 */
-	public Boolean getActive() {
-		return active;
-	}
+    /**
+     * @param archived
+     *            the archived to set
+     */
+    public void setArchived(Boolean archived) {
+        this.archived = archived;
+    }
 
-	/**
-	 * @param active
-	 *            the active to set
-	 */
-	public void setActive(Boolean active) {
-		this.active = active;
-	}
+    /**
+     * @return the publishable
+     */
+    public Boolean getPublishable() {
+        return publishable;
+    }
 
-	/**
-	 * @return the transitionSubmissionStates
-	 */
-	public Set<SubmissionState> getTransitionSubmissionStates() {
-		return transitionSubmissionStates;
-	}
+    /**
+     * @param publishable
+     *            the publishable to set
+     */
+    public void setPublishable(Boolean publishable) {
+        this.publishable = publishable;
+    }
 
-	/**
-	 * @param transitionSubmissionStates
-	 *            the transitionSubmissionStates to set
-	 */
-	public void setTransitionSubmissionStates(Set<SubmissionState> transitionSubmissionStates) {
-		this.transitionSubmissionStates = transitionSubmissionStates;
-	}
-	
-	/**
-	 * 
-	 * @param transitionSubmissionState
-	 */
-	public void addTransitionSubmissionState(SubmissionState transitionSubmissionState) {
-		getTransitionSubmissionStates().add(transitionSubmissionState);
-	}
-	
-	/**
-	 * 
-	 * @param transitionSubmissionState
-	 */
-	public void removeTransitionSubmissionState(SubmissionState transitionSubmissionState) {
-		getTransitionSubmissionStates().remove(transitionSubmissionState);
-	}
-	
+    /**
+     * @return the deletable
+     */
+    public Boolean getDeletable() {
+        return deletable;
+    }
+
+    /**
+     * @param deletable
+     *            the deletable to set
+     */
+    public void setDeletable(Boolean deletable) {
+        this.deletable = deletable;
+    }
+
+    /**
+     * @return the editableByReviewer
+     */
+    public Boolean getEditableByReviewer() {
+        return editableByReviewer;
+    }
+
+    /**
+     * @param editableByReviewer
+     *            the editableByReviewer to set
+     */
+    public void setEditableByReviewer(Boolean editableByReviewer) {
+        this.editableByReviewer = editableByReviewer;
+    }
+
+    /**
+     * @return the editableByStudent
+     */
+    public Boolean getEditableByStudent() {
+        return editableByStudent;
+    }
+
+    /**
+     * @param editableByStudent
+     *            the editableByStudent to set
+     */
+    public void setEditableByStudent(Boolean editableByStudent) {
+        this.editableByStudent = editableByStudent;
+    }
+
+    /**
+     * @return the active
+     */
+    public Boolean getActive() {
+        return active;
+    }
+
+    /**
+     * @param active
+     *            the active to set
+     */
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    /**
+     * @return the transitionSubmissionStates
+     */
+    public Set<SubmissionState> getTransitionSubmissionStates() {
+        return transitionSubmissionStates;
+    }
+
+    /**
+     * @param transitionSubmissionStates
+     *            the transitionSubmissionStates to set
+     */
+    public void setTransitionSubmissionStates(Set<SubmissionState> transitionSubmissionStates) {
+        this.transitionSubmissionStates = transitionSubmissionStates;
+    }
+
+    /**
+     * 
+     * @param transitionSubmissionState
+     */
+    public void addTransitionSubmissionState(SubmissionState transitionSubmissionState) {
+        getTransitionSubmissionStates().add(transitionSubmissionState);
+    }
+
+    /**
+     * 
+     * @param transitionSubmissionState
+     */
+    public void removeTransitionSubmissionState(SubmissionState transitionSubmissionState) {
+        getTransitionSubmissionStates().remove(transitionSubmissionState);
+    }
+
 }
--- src/test/java/org/tdl/vireo/model/SubmissionTest.java
+++ src/test/java/org/tdl/vireo/model/SubmissionTest.java
@@ -25,60 +25,61 @@ public class SubmissionTest {
     static final boolean TEST_PARENT_SUBMISSION_STATE_EDITABLE_BY_REVIEWER = true;
     static final boolean TEST_PARENT_SUBMISSION_STATE_EDITABLE_BY_STUDENT = true;
     static final boolean TEST_PARENT_SUBMISSION_STATE_ACTIVE = true;
-	
+
     SubmissionState parentSubmissionState;
-    
-	@Autowired
-	private SubmissionRepo submissionRepo;
-	
-	@Autowired
+
+    @Autowired
+    private SubmissionRepo submissionRepo;
+
+    @Autowired
     private SubmissionStateRepo submissionStateRepo;
-	
-	@BeforeClass
+
+    @BeforeClass
     public static void init() {
-		
+
     }
-	
-	@Before
-	public void setUp() {
-	    assertEquals("The submission repository was not empty!", 0, submissionRepo.count());
+
+    @Before
+    public void setUp() {
+        assertEquals("The submission repository was not empty!", 0, submissionRepo.count());
         parentSubmissionState = submissionStateRepo.create(TEST_PARENT_SUBMISSION_STATE_NAME, TEST_PARENT_SUBMISSION_STATE_ARCHIVED, TEST_PARENT_SUBMISSION_STATE_PUBLISHABLE, TEST_PARENT_SUBMISSION_STATE_DELETABLE, TEST_PARENT_SUBMISSION_STATE_EDITABLE_BY_REVIEWER, TEST_PARENT_SUBMISSION_STATE_EDITABLE_BY_STUDENT, TEST_PARENT_SUBMISSION_STATE_ACTIVE);
         assertEquals("The submission state does not exist!", 1, submissionStateRepo.count());
-	}
-	
-	@Test
-	@Order(value = 1)
-	public void testCreate() {
-		
-	}
-	
-	@Test
-	@Order(value = 2)
-	public void testDuplication() {
-		
-	}
-	
-	@Test
-	@Order(value = 3)
-	public void testFind() {
-		
-	}
-	
-	@Test
-	@Order(value = 4)
-	public void testDelete() {
-		
-	}
-	
-	@Test
-	@Order(value = 5)
-	public void testCascade() {
-		
-	}
-	
-	@After
-	public void cleanUp() {
-		submissionRepo.deleteAll();
-	}
+    }
+
+    @Test
+    @Order(value = 1)
+    public void testCreate() {
+
+    }
+
+    @Test
+    @Order(value = 2)
+    public void testDuplication() {
+
+    }
+
+    @Test
+    @Order(value = 3)
+    public void testFind() {
+
+    }
+
+    @Test
+    @Order(value = 4)
+    public void testDelete() {
+
+    }
+
+    @Test
+    @Order(value = 5)
+    public void testCascade() {
+
+    }
+
+    @After
+    public void cleanUp() {
+        submissionRepo.deleteAll();
+        submissionStateRepo.deleteAll();
+    }
 
 }
