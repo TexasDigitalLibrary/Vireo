405d039d0908c156edc104a264302f11a04ee938
jeremythuff <jeremythuff@gmail.com>
2015-09-25 16:33:52 -0500

    removed create method from field profile repo

--- src/main/java/org/tdl/vireo/model/repo/FieldProfileRepo.java
+++ src/main/java/org/tdl/vireo/model/repo/FieldProfileRepo.java
@@ -3,13 +3,10 @@ package org.tdl.vireo.model.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.tdl.vireo.model.FieldProfile;
-import org.tdl.vireo.model.FieldPredicate;
 import org.tdl.vireo.model.repo.custom.FieldProfileRepoCustom;
 
 @Repository
 public interface FieldProfileRepo extends JpaRepository<FieldProfile, Long>, FieldProfileRepoCustom {
-
-	public FieldProfile create(FieldPredicate fieldPredicate, Boolean repeatable, Boolean required);
 	
 	public void delete(FieldProfile fieldProfile);
 	
