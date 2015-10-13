f0d18716e97127fd160df2ef27d6960d33c4417e
Gad Krumholz <gad.krumholz@austin.utexas.edu>
2015-10-13 17:12:32 -0500

    Fixed EmbargoType and EmbargoGuarantor to work with Vireo 4

--- src/main/java/org/tdl/vireo/enums/EmbargoGuarantor.java
+++ src/main/java/org/tdl/vireo/enums/EmbargoGuarantor.java
@@ -1,8 +1,22 @@
-package org.tdl.vireo.model;
+package org.tdl.vireo.enums;
 
 public enum EmbargoGuarantor {
+    // NEVER CHANGE THE INT VALUES OR YOU'LL RUIN THE DB
+    DEFAULT(0),
+    PROQUEST(1);
 
-	DEFAULT,
-	PROQUEST
-	
+    private int value;
+
+    EmbargoGuarantor(int num) {
+        value = num;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return this.name();
+    }
 }
--- src/main/java/org/tdl/vireo/model/EmbargoType.java
+++ src/main/java/org/tdl/vireo/model/EmbargoType.java
@@ -1,295 +1,135 @@
-package org.tdl.vireo.model.jpa;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+package org.tdl.vireo.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
-import org.tdl.vireo.model.EmbargoGuarantor;
-import org.tdl.vireo.model.EmbargoType;
-import org.tdl.vireo.model.Submission;
-import org.tdl.vireo.search.Indexer;
+import org.tdl.vireo.enums.EmbargoGuarantor;
 
-import play.Logger;
-import play.modules.spring.Spring;
-
-/**
- * Jpa specific implementation of Vireo's Embargo Type interface.
- * 
- * @author <a href="http://www.scottphillips.com">Scott Phillips</a>
- */
 @Entity
-@Table(name = "embargo_type", uniqueConstraints=
-@UniqueConstraint(columnNames = {"name", "guarantor", "systemRequired"}))
-public class JpaEmbargoTypeImpl extends JpaAbstractModel<JpaEmbargoTypeImpl> implements EmbargoType {
-	
-	@Column(nullable = false)
-	public int displayOrder;
-	
-	@Column(nullable = false, length=255)
-	public String name;
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = { "name", "guarantor", "systemRequired" }) )
+public class EmbargoType extends BaseEntity {
 
-	@Column(nullable = false, length=32768) // 2^15
-	public String description;
+    @Column(nullable = false)
+    private String name;
 
-	public Integer duration;
-	
-	@Column(nullable = false)
-	public boolean active;
-	
-	@Column(nullable = false, columnDefinition="BOOLEAN DEFAULT false")
-	public Boolean systemRequired;
+    @Column(nullable = false, length = 32768) // 2^15
+    private String description;
 
-	// this requires a column definition in order to update the table column on old DB's (default value) 
-	@Column(nullable = false, columnDefinition="INTEGER DEFAULT '0'")
-	public EmbargoGuarantor guarantor;
-	
-	/**
-	 * Create a new JpaEmbargoTypeImpl.
-	 * 
-	 * @param name
-	 *            The unique name of the embargo
-	 * @param description
-	 *            The description of the embargo.
-	 * @param duration
-	 *            The duration of the embargo, or null, may not be negative
-	 * @param active
-	 *            Weather the embargo is active.
-	 */
-	protected JpaEmbargoTypeImpl(String name, String description,
-			Integer duration, boolean active, EmbargoGuarantor guarantor) {
+    @Column(nullable = false)
+    private Integer duration;
 
-		if (name == null || name.length() == 0)
-			throw new IllegalArgumentException("Name is required");
-		
-		if (description == null || description.length() == 0)
-			throw new IllegalArgumentException("Description is required");
-		
-		if (duration != null && duration < 0)
-			throw new IllegalArgumentException("Duration must be positive, or null");
-		
-		assertManager();
-		
-		this.systemRequired = false;
-		this.displayOrder = 0;
-		this.name = name;
-		this.description = description;
-		this.duration = duration;
-		this.active = active;
-		
-		if(guarantor == null)
-			this.guarantor = EmbargoGuarantor.DEFAULT;
-		else
-			this.guarantor = guarantor;
-		
-		//this.submissions = new ArrayList<Submission>();
-	}
+    @Column(nullable = false)
+    private Boolean isActive;
 
-	@Override
-	public JpaEmbargoTypeImpl save() {
-		assertManager();
+    @Column(nullable = false)
+    private Boolean isSystemRequired;
 
-		return super.save();
-	}
-	
-	@Override
-	public JpaEmbargoTypeImpl delete() {
-		assertManager();
-		
-		if (isSystemRequired())
-			throw new IllegalStateException("Unable to delete the embargo '"+name+"' because it is required by the system.");
-		
-//		JpaSubmissionRepositoryImpl subRepo = Spring.getBeanOfType(JpaSubmissionRepositoryImpl.class);
-		// Tell the indexer about all the submissions that will be affected by
-		// this deletion.
-//		TypedQuery<Long> affectedQuery = em().createQuery(
-//				"SELECT sub.id "+
-//				"FROM JpaSubmissionImpl AS sub "+
-//				"WHERE sub.embargoType = :embargo ",
-//				Long.class);
-//		affectedQuery.setParameter("embargo", this);
-		List<Long> affectedIds = new ArrayList<Long>();
-		
-//		Iterator<Submission> submissionsItr = subRepo.findAllSubmissions();
-//		while(submissionsItr.hasNext()) {
-//			Submission sub = submissionsItr.next();
-//			if(sub.getEmbargoTypes().contains(this)) {
-//				affectedIds.add(sub.getId());
-//				sub.getEmbargoTypes().remove(this);
-//				sub.save();
-//			}
-//		}
-		
-		for (Submission sub : getSubmissions()) {
-			affectedIds.add(sub.getId());
-			sub.removeEmbargoType(this);
-        }
-		
-		Logger.info("Indexer affected IDs: " + affectedIds.size());
-		Indexer indexer = Spring.getBeanOfType(Indexer.class);
-		indexer.updated(affectedIds);		
-		
-		// Delete all values associated with this definition
-//		em().createQuery(
-//			"UPDATE JpaSubmissionImpl AS sub "+
-//		    "SET sub.embargoType = null "+
-//			"WHERE sub.embargoType = :embargo"
-//			).setParameter("embargo", this)
-//			.executeUpdate();
-		
-		
-		return super.delete();
-	}
-	
-    @Override
-    public int getDisplayOrder() {
-        return displayOrder;
+    @Column(nullable = false)
+    private EmbargoGuarantor guarantor;
+
+    public EmbargoType() {
+        isSystemRequired(false);
+        setGuarantor(EmbargoGuarantor.DEFAULT);
     }
 
-    @Override
-    public void setDisplayOrder(int displayOrder) {
-    	
-    	assertManager();
-    	
-        this.displayOrder = displayOrder;
+    public EmbargoType(String name, String description, Integer duration, Boolean isActive) {
+        this();
+        setName(name);
+        setDescription(description);
+        setDuration(duration);
+        isActive(isActive);
     }
 
-	@Override
-	public String getName() {
-		return name;
-	}
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
 
-	@Override
-	public void setName(String name) {
-		
-		if (name == null || name.length() == 0)
-			throw new IllegalArgumentException("Name is required");
-		
-		assertManager();
-		
-		// Just to be nice so that if you're not changing it we won't do the system required check.
-		if (name.equals(this.name))
-			return;
-		
-		if (isSystemRequired())
-			throw new IllegalStateException("Unable to rename the embargo '"+this.name+"' because it is required by the system.");
-		
-		this.name = name;
-	}
-	
-	@Override
-	public String getDescription() {
-		return description;
-	}
+    /**
+     * @param name
+     *            the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	@Override
-	public void setDescription(String description) {
-		
-		if (description == null || description.length() == 0)
-			throw new IllegalArgumentException("Description is required");
-		
-		assertManager();
-		
-		// Just to be nice so that if you're not changing it we won't do the system required check.
-		if (description.equals(this.description))
-			return;
-		
-		if (isSystemRequired())
-			throw new IllegalStateException("Unable to change the embargo '"+name+"' because it is required by the system.");
-		
-		this.description = description;
-	}
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
 
-	@Override
-	public Integer getDuration() {
-		return duration;
-	}
+    /**
+     * @param description
+     *            the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	@Override
-	public void setDuration(Integer duration) {
-		
-		if (duration != null && duration < 0)
-			throw new IllegalArgumentException("Duration must be positive, or null");
-		
-		assertManager();
-		
-		// Just to be nice so that if you're not changing it we won't do the system required check.
-		if(duration == null && duration == this.duration)
-			return;
-		else if (duration == null && duration != this.duration) {
-			// do nothing
-		}
-		else if (duration.equals(this.duration))
-			return;
-		
-		if (isSystemRequired())
-			throw new IllegalStateException("Unable to change the embargo '"+name+"' because it is required by the system.");
-		
-		this.duration = duration;
-	}
-	
-	@Override
-	public boolean isActive() {
-		return active;
-	}
+    /**
+     * @return the duration
+     */
+    public Integer getDuration() {
+        return duration;
+    }
 
-	@Override
-	public void setActive(boolean active) {
-		
-		assertManager();
-		this.active = active;
-	}
-	
-	@Override
-	public boolean isSystemRequired() {
-		return systemRequired;
-	}
-	
-	@Override
-	public void setSystemRequired(boolean systemRequired) {
-		assertAdministrator();
-		
-		this.systemRequired = systemRequired;
-	}
-	
-	@Override
-	public EmbargoGuarantor getGuarantor() {
-		return this.guarantor;
-	}
-	
-	public void setGuarantor(EmbargoGuarantor guarantor) {
-		if (guarantor == null)
-			throw new IllegalArgumentException("Guarantor is required");
-		
-		assertManager();
-		
-		// Just to be nice so that if you're not changing it we won't do the system required check.
-		if (guarantor.equals(this.guarantor))
-			return;
-		
-		if (isSystemRequired())
-			throw new IllegalStateException("Unable to change the embargo '"+name+"' because it is required by the system.");
-				
-		this.guarantor = guarantor;
-	}
+    /**
+     * @param duration
+     *            the duration to set
+     */
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
 
-	@Override
-	public List<Submission> getSubmissions() {
-		List<Submission> ret = new ArrayList<Submission>();
-		JpaSubmissionRepositoryImpl subRepo = Spring.getBeanOfType(JpaSubmissionRepositoryImpl.class);
-		Iterator<Submission> submissionsItr = subRepo.findAllSubmissions();
-		while(submissionsItr.hasNext()) {
-			Submission sub = submissionsItr.next();
-			if(sub.getEmbargoTypes().contains(this)) {
-				ret.add(sub);
-			}
-		}
-		return ret;
-	}
-	
+    /**
+     * @return the isActive
+     */
+    public Boolean isActive() {
+        return isActive;
+    }
+
+    /**
+     * @param isActive
+     *            the isActive to set
+     */
+    public void isActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    /**
+     * @return the isSystemRequired
+     */
+    public Boolean isSystemRequired() {
+        return isSystemRequired;
+    }
+
+    /**
+     * @param isSystemRequired
+     *            the isSystemRequired to set
+     */
+    public void isSystemRequired(Boolean isSystemRequired) {
+        this.isSystemRequired = isSystemRequired;
+    }
+
+    /**
+     * @return the guarantor
+     */
+    public EmbargoGuarantor getGuarantor() {
+        return guarantor;
+    }
+
+    /**
+     * @param guarantor
+     *            the guarantor to set
+     */
+    public void setGuarantor(EmbargoGuarantor guarantor) {
+        this.guarantor = guarantor;
+    }
+
 }
