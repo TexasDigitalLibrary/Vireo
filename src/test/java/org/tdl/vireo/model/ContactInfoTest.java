004ca2631debd4036b9152fa72264213c943ced3
unknown <rmathew@library.tamu.edu>
2015-10-23 14:16:58 -0500

    ContactInfoTest extends AbstractEntityTest class

--- src/test/java/org/tdl/vireo/model/ContactInfoTest.java
+++ src/test/java/org/tdl/vireo/model/ContactInfoTest.java
@@ -6,85 +6,74 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 import org.tdl.vireo.Application;
 import org.tdl.vireo.annotations.Order;
-import org.tdl.vireo.model.repo.AddressRepo;
-import org.tdl.vireo.model.repo.ContactInfoRepo;
 import org.tdl.vireo.runner.OrderedRunner;
 
 @RunWith(OrderedRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
-public class ContactInfoTest {
+public class ContactInfoTest extends AbstractEntityTest {
 
-    private static final String TEST_ADDRESS1 = "101. E. 21st St.";
-    private static final String TEST_ADDRESS2 = "PCL 1.333";
-    private static final String TEST_CITY = "Austin";
-    private static final String TEST_STATE = "Texas";
-    private static final String TEST_POSTAL_CODE = "78759";
-    private static final String TEST_COUNTRY = "USA";
-    private static final String TEST_PHONE = "512-495-4418";
-    private static final String TEST_EMAIL = "admin@tdl.org";
+	@Before
+	public void setUp() {
+		assertEquals("The contactInfo repository is not empty!", 0, contactInfoRepo.count());
 
-    private static Address testAddress;
+		testAddress = addressRepo.create(TEST_ADDRESS1, TEST_ADDRESS2, TEST_CITY, TEST_STATE, TEST_POSTAL_CODE,
+				TEST_COUNTRY);
+		assertEquals("The address dependency was not created successfully!", 1, addressRepo.count());
+	}
 
-    @Autowired
-    private ContactInfoRepo contactInfoRepo;
+	@Test
+	@Order(value = 1)
+	@Transactional
+	public void testCreate() {
+		ContactInfo testContactInfo = contactInfoRepo.create(testAddress, TEST_PHONE, TEST_EMAIL);
+		assertEquals("The contact info is not created", 1, contactInfoRepo.count());
+		assertEquals("Created testContactInfo does not contain the correct address", testAddress,
+				testContactInfo.getAddress());
+		assertEquals("Created testContactInfo does not contain the correct phone ", TEST_PHONE,
+				testContactInfo.getPhone());
+		assertEquals("Created testContactInfo does not contain the correct email ", TEST_EMAIL,
+				testContactInfo.getEmail());
+	}
 
-    @Autowired
-    private AddressRepo addressRepo;
+	@Test
+	@Order(value = 2)
+	@Transactional
+	public void testDuplication() {
+		contactInfoRepo.create(testAddress, TEST_PHONE, TEST_EMAIL);
+		contactInfoRepo.create(testAddress, TEST_PHONE, TEST_EMAIL);
+		assertEquals("Duplicate contact info entry is not saved", 2, contactInfoRepo.count());
+	}
 
-    @Before
-    public void setUp() {
-        assertEquals("The contactInfo repository is not empty!", 0, contactInfoRepo.count());
+	@Test
+	@Order(value = 3)
+	@Transactional
+	public void testDelete() {
+		ContactInfo testContactInfo = contactInfoRepo.create(testAddress, TEST_PHONE, TEST_EMAIL);
+		contactInfoRepo.delete(testContactInfo);
+		assertEquals("The contact info was not deleted", 0, contactInfoRepo.count());
+	}
 
-        testAddress = addressRepo.create(TEST_ADDRESS1, TEST_ADDRESS2, TEST_CITY, TEST_STATE, TEST_POSTAL_CODE, TEST_COUNTRY);
-        assertEquals("The address dependency was not created successfully!", 1, addressRepo.count());
-    }
+	@Override
+	public void testFind() {
+	}
 
-    @Test
-    @Order(value = 1)
-    @Transactional
-    public void testCreate() {
-        ContactInfo testContactInfo = contactInfoRepo.create(testAddress, TEST_PHONE, TEST_EMAIL);
-        assertEquals("The contact info is not created", 1, contactInfoRepo.count());
-        assertEquals("Created testContactInfo does not contain the correct address", testAddress, testContactInfo.getAddress());
-        assertEquals("Created testContactInfo does not contain the correct phone ", TEST_PHONE, testContactInfo.getPhone());
-        assertEquals("Created testContactInfo does not contain the correct email ", TEST_EMAIL, testContactInfo.getEmail());
-    }
+	@Test
+	@Order(value = 4)
+	@Transactional
+	public void testCascade() {
+		ContactInfo testContactInfo = contactInfoRepo.create(testAddress, TEST_PHONE, TEST_EMAIL);
+		contactInfoRepo.delete(testContactInfo);
+		assertEquals("Cascade delete did not happen for address", 0, addressRepo.count());
+	}
 
-    @Test
-    @Order(value = 2)
-    @Transactional
-    public void testDuplication() {
-        contactInfoRepo.create(testAddress, TEST_PHONE, TEST_EMAIL);
-        contactInfoRepo.create(testAddress, TEST_PHONE, TEST_EMAIL);
-        assertEquals("Duplicate contact info entry is not saved", 2, contactInfoRepo.count());
-    }
+	@After
+	public void cleanUp() {
+		addressRepo.deleteAll();
+		contactInfoRepo.deleteAll();
+	}
 
-    @Test
-    @Order(value = 3)
-    @Transactional
-    public void testDelete() {
-        ContactInfo testContactInfo = contactInfoRepo.create(testAddress, TEST_PHONE, TEST_EMAIL);
-        contactInfoRepo.delete(testContactInfo);
-        assertEquals("The contact info was not deleted", 0, contactInfoRepo.count());
-    }
-
-    @Test
-    @Order(value = 4)
-    @Transactional
-    public void testCascade() {
-        ContactInfo testContactInfo = contactInfoRepo.create(testAddress, TEST_PHONE, TEST_EMAIL);
-        contactInfoRepo.delete(testContactInfo);
-        assertEquals("Cascade delete did not happen for address", 0, addressRepo.count());
-    }
-
-    @After
-    public void cleanUp() {
-        addressRepo.deleteAll();
-        contactInfoRepo.deleteAll();
-    }
 }
