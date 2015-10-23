68fc6feaf4cedff2b4560c5e633c7bc58df9e7ee
unknown <rmathew@library.tamu.edu>
2015-10-23 14:29:26 -0500

    UserTest extends AbstractEntityTest class

--- src/test/java/org/tdl/vireo/model/UserTest.java
+++ src/test/java/org/tdl/vireo/model/UserTest.java
@@ -7,159 +7,109 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.transaction.annotation.Transactional;
 import org.tdl.vireo.Application;
 import org.tdl.vireo.annotations.Order;
-import org.tdl.vireo.enums.Role;
-import org.tdl.vireo.model.repo.AddressRepo;
-import org.tdl.vireo.model.repo.ContactInfoRepo;
-import org.tdl.vireo.model.repo.OrganizationCategoryRepo;
-import org.tdl.vireo.model.repo.OrganizationRepo;
-import org.tdl.vireo.model.repo.UserRepo;
 import org.tdl.vireo.runner.OrderedRunner;
 
 @RunWith(OrderedRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
-public class UserTest {
+public class UserTest extends AbstractEntityTest {
 
-    private static final String TEST_USER_EMAIL = "admin@tdl.org";
-    private static final String TEST_USER_FIRSTNAME = "TDL";
-    private static final String TEST_USER_LASTNAME = "Admin";
-    private static final Role TEST_USER_ROLE = Role.ADMINISTRATOR;
+	@Before
+	public void setUp() {
+		assertEquals("The user repository was not empty!", 0, userRepo.count());
+	}
 
-    private static final String TEST_SHIBBOLETH_AFFILIATION = "shib_affiliation";
+	@Test
+	@Order(value = 1)
+	@Transactional
+	public void testCreate() {
+		User testUser = userRepo.create(TEST_USER_EMAIL, TEST_USER_FIRSTNAME, TEST_USER_LASTNAME, TEST_USER_ROLE);
+		assertEquals("The user repository did not save the user!", 1, userRepo.count());
+		assertEquals("Saved user did not contain the correct email!", TEST_USER_EMAIL, testUser.getEmail());
+		assertEquals("Saved user did not contain the correct first name!", TEST_USER_FIRSTNAME,
+				testUser.getFirstName());
+		assertEquals("Saved user did not contain the correct last name!", TEST_USER_LASTNAME, testUser.getLastName());
+		assertEquals("Saved user did not contain the correct role!", TEST_USER_ROLE, testUser.getRole());
+	}
 
-    private static final String TEST_PREFERENCE_KEY = "key";
-    private static final String TEST_PREFERENCE_VALUE = "value";
+	@Test
+	@Order(value = 2)
+	public void testDuplication() {
+		userRepo.create(TEST_USER_EMAIL, TEST_USER_FIRSTNAME, TEST_USER_LASTNAME, TEST_USER_ROLE);
+		try {
+			userRepo.create(TEST_USER_EMAIL, TEST_USER_FIRSTNAME, TEST_USER_LASTNAME, TEST_USER_ROLE);
+		} catch (DataIntegrityViolationException e) {
+			/* SUCCESS */ }
+		assertEquals("The user repository duplicated the user!", 1, userRepo.count());
+	}
 
-    private static final String TEST_CURRENT_ADDRESS1 = "101. E. 21st St.";
-    private static final String TEST_CURRENT_ADDRESS2 = "PCL 1.333";
-    private static final String TEST_CURRENT_CITY = "Austin";
-    private static final String TEST_CURRENT_STATE = "Texas";
-    private static final String TEST_CURRENT_POSTAL_CODE = "78759";
-    private static final String TEST_CURRENT_COUNTRY = "USA";
-    private static final String TEST_CURRENT_PHONE = "512-495-4418";
-    private static final String TEST_CURRENT_EMAIL = "admin@tdl.org";
+	@Test
+	@Order(value = 3)
+	public void testFind() {
+		userRepo.create(TEST_USER_EMAIL, TEST_USER_FIRSTNAME, TEST_USER_LASTNAME, TEST_USER_ROLE);
+		User testUser = userRepo.findByEmail(TEST_USER_EMAIL);
+		assertNotEquals("Did not find user!", null, testUser);
+		assertEquals("Found user did not contain the correct email!", TEST_USER_EMAIL, testUser.getEmail());
+	}
 
-    private static final String TEST_PERMANENT_ADDRESS1 = "101. E. 21st St. <p>";
-    private static final String TEST_PERMANENT_ADDRESS2 = "PCL 1.333 <p>";
-    private static final String TEST_PERMANENT_CITY = "Austin <p>";
-    private static final String TEST_PERMANENT_STATE = "Texas <p>";
-    private static final String TEST_PERMANENT_POSTAL_CODE = "78759 <p>";
-    private static final String TEST_PERMANENT_COUNTRY = "USA <p>";
-    private static final String TEST_PERMANENT_PHONE = "512-495-4418 <p>";
-    private static final String TEST_PERMANENT_EMAIL = "admin@tdl.org <p>";
+	@Test
+	@Order(value = 4)
+	public void testDelete() {
+		User testUser = userRepo.create(TEST_USER_EMAIL, TEST_USER_FIRSTNAME, TEST_USER_LASTNAME, TEST_USER_ROLE);
+		userRepo.delete(testUser);
+		assertEquals("User did not delete!", 0, userRepo.count());
+	}
 
-    private static final String TEST_PARENT_CATEGORY_NAME = "Test Parent Category";
-    private static final int TEST_PARENT_CATEGORY_LEVEL = 0;
+	@Test
+	@Order(value = 5)
+	@Transactional
+	public void testCascade() {
+		Address currentAddress = addressRepo.create(TEST_CURRENT_ADDRESS1, TEST_CURRENT_ADDRESS2, TEST_CURRENT_CITY,
+				TEST_CURRENT_STATE, TEST_CURRENT_POSTAL_CODE, TEST_CURRENT_COUNTRY);
+		assertEquals("The address does not exist!", 1, addressRepo.count());
+		ContactInfo currentContactInfo = contactInfoRepo.create(currentAddress, TEST_CURRENT_PHONE, TEST_CURRENT_EMAIL);
+		assertEquals("The contact info does not exist!", 1, contactInfoRepo.count());
 
-    private static final String TEST_PARENT_ORGANIZATION_NAME = "Test Parent Organization";
+		Address permanentAddress = addressRepo.create(TEST_PERMANENT_ADDRESS1, TEST_PERMANENT_ADDRESS2,
+				TEST_PERMANENT_CITY, TEST_PERMANENT_STATE, TEST_PERMANENT_POSTAL_CODE, TEST_PERMANENT_COUNTRY);
+		assertEquals("The address does not exist!", 2, addressRepo.count());
+		ContactInfo permanentContactInfo = contactInfoRepo.create(permanentAddress, TEST_PERMANENT_PHONE,
+				TEST_PERMANENT_EMAIL);
+		assertEquals("The contact info does not exist!", 2, contactInfoRepo.count());
 
-    @Autowired
-    private UserRepo userRepo;
+		OrganizationCategory parentCategory = organizationCategoryRepo.create(TEST_PARENT_CATEGORY_NAME,
+				TEST_PARENT_CATEGORY_LEVEL);
+		assertEquals("The category does not exist!", 1, organizationCategoryRepo.count());
 
-    @Autowired
-    private AddressRepo addressRepo;
+		Organization organization = organizationRepo.create(TEST_PARENT_ORGANIZATION_NAME, parentCategory);
+		assertEquals("The organization does not exist!", 1, organizationRepo.count());
 
-    @Autowired
-    private ContactInfoRepo contactInfoRepo;
+		User testUser = userRepo.create(TEST_USER_EMAIL, TEST_USER_FIRSTNAME, TEST_USER_LASTNAME, TEST_USER_ROLE);
+		testUser.addOrganization(organization);
+		testUser.addPreference(TEST_PREFERENCE_KEY, TEST_PREFERENCE_VALUE);
+		testUser.addShibbolethAffiliation(TEST_SHIBBOLETH_AFFILIATION);
+		testUser.setPermanentContactInfo(permanentContactInfo);
+		testUser.setCurrentContactInfo(currentContactInfo);
+		testUser = userRepo.save(testUser);
 
-    @Autowired
-    private OrganizationCategoryRepo organizationCategoryRepo;
+		// test detach organization
+		testUser.removeOrganization(organization);
+		testUser = userRepo.save(testUser);
+		assertEquals("The organization was not detached from the user", 0, testUser.getOrganizations().size());
 
-    @Autowired
-    private OrganizationRepo organizationRepo;
+		//test delete user
+		userRepo.delete(testUser);
+		assertEquals("The testUser was not deleted!", 0, userRepo.count());
+	}
 
-    @Before
-    public void setUp() {
-        assertEquals("The user repository was not empty!", 0, userRepo.count());
-    }
-
-    @Test
-    @Order(value = 1)
-    @Transactional
-    public void testCreate() {
-        User testUser = userRepo.create(TEST_USER_EMAIL, TEST_USER_FIRSTNAME, TEST_USER_LASTNAME, TEST_USER_ROLE);
-        assertEquals("The user repository did not save the user!", 1, userRepo.count());
-        assertEquals("Saved user did not contain the correct email!", TEST_USER_EMAIL, testUser.getEmail());
-        assertEquals("Saved user did not contain the correct first name!", TEST_USER_FIRSTNAME, testUser.getFirstName());
-        assertEquals("Saved user did not contain the correct last name!", TEST_USER_LASTNAME, testUser.getLastName());
-        assertEquals("Saved user did not contain the correct role!", TEST_USER_ROLE, testUser.getRole());
-    }
-
-    @Test
-    @Order(value = 2)
-    public void testDuplication() {
-        userRepo.create(TEST_USER_EMAIL, TEST_USER_FIRSTNAME, TEST_USER_LASTNAME, TEST_USER_ROLE);
-        try {
-            userRepo.create(TEST_USER_EMAIL, TEST_USER_FIRSTNAME, TEST_USER_LASTNAME, TEST_USER_ROLE);
-        } catch (DataIntegrityViolationException e) {
-            /* SUCCESS */ }
-        assertEquals("The user repository duplicated the user!", 1, userRepo.count());
-    }
-
-    @Test
-    @Order(value = 3)
-    public void testFind() {
-        userRepo.create(TEST_USER_EMAIL, TEST_USER_FIRSTNAME, TEST_USER_LASTNAME, TEST_USER_ROLE);
-        User testUser = userRepo.findByEmail(TEST_USER_EMAIL);
-        assertNotEquals("Did not find user!", null, testUser);
-        assertEquals("Found user did not contain the correct email!", TEST_USER_EMAIL, testUser.getEmail());
-    }
-
-    @Test
-    @Order(value = 4)
-    public void testDelete() {
-        User testUser = userRepo.create(TEST_USER_EMAIL, TEST_USER_FIRSTNAME, TEST_USER_LASTNAME, TEST_USER_ROLE);
-        userRepo.delete(testUser);
-        assertEquals("User did not delete!", 0, userRepo.count());
-    }
-
-    @Test
-    @Order(value = 5)
-    @Transactional
-    public void testCascade() {
-        Address currentAddress = addressRepo.create(TEST_CURRENT_ADDRESS1, TEST_CURRENT_ADDRESS2, TEST_CURRENT_CITY, TEST_CURRENT_STATE, TEST_CURRENT_POSTAL_CODE, TEST_CURRENT_COUNTRY);
-        assertEquals("The address does not exist!", 1, addressRepo.count());
-        ContactInfo currentContactInfo = contactInfoRepo.create(currentAddress, TEST_CURRENT_PHONE, TEST_CURRENT_EMAIL);
-        assertEquals("The contact info does not exist!", 1, contactInfoRepo.count());
-
-        Address permanentAddress = addressRepo.create(TEST_PERMANENT_ADDRESS1, TEST_PERMANENT_ADDRESS2, TEST_PERMANENT_CITY, TEST_PERMANENT_STATE, TEST_PERMANENT_POSTAL_CODE, TEST_PERMANENT_COUNTRY);
-        assertEquals("The address does not exist!", 2, addressRepo.count());
-        ContactInfo permanentContactInfo = contactInfoRepo.create(permanentAddress, TEST_PERMANENT_PHONE, TEST_PERMANENT_EMAIL);
-        assertEquals("The contact info does not exist!", 2, contactInfoRepo.count());
-
-        OrganizationCategory parentCategory = organizationCategoryRepo.create(TEST_PARENT_CATEGORY_NAME, TEST_PARENT_CATEGORY_LEVEL);
-        assertEquals("The category does not exist!", 1, organizationCategoryRepo.count());
-
-        Organization organization = organizationRepo.create(TEST_PARENT_ORGANIZATION_NAME, parentCategory);
-        assertEquals("The organization does not exist!", 1, organizationRepo.count());
-
-        User testUser = userRepo.create(TEST_USER_EMAIL, TEST_USER_FIRSTNAME, TEST_USER_LASTNAME, TEST_USER_ROLE);
-        testUser.addOrganization(organization);
-        testUser.addPreference(TEST_PREFERENCE_KEY, TEST_PREFERENCE_VALUE);
-        testUser.addShibbolethAffiliation(TEST_SHIBBOLETH_AFFILIATION);
-        testUser.setPermanentContactInfo(permanentContactInfo);
-        testUser.setCurrentContactInfo(currentContactInfo);
-        testUser = userRepo.save(testUser);
-
-        //TODO test detach organization
-        testUser.removeOrganization(organization);
-        testUser = userRepo.save(testUser);
-        assertEquals("The organization was not detached from the user",0,testUser.getOrganizations().size());
-        
-        //TODO test delete user
-        userRepo.delete(testUser);
-        assertEquals("The testUser was not deleted!", 0, userRepo.count());
-    }
-
-    @After
-    public void cleanUp() {
-        userRepo.deleteAll();
-        organizationRepo.deleteAll();
-        organizationCategoryRepo.deleteAll();
-    }
+	@After
+	public void cleanUp() {
+		userRepo.deleteAll();
+		organizationRepo.deleteAll();
+		organizationCategoryRepo.deleteAll();
+	}
 }
