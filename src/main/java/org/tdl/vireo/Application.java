e0a7e5ffc3a26328abe3de8d205112c9948d4cc0
Gad Krumholz <gad.krumholz@austin.utexas.edu>
2015-11-10 17:02:53 -0600

    Got maven assembly and external application.properties working for vireo 4 deployed to tomcat and also as a stand-alone spring boot app. Requirement is a 'conf' directory.

--- src/main/java/org/tdl/vireo/Application.java
+++ src/main/java/org/tdl/vireo/Application.java
@@ -1,11 +1,16 @@
 package org.tdl.vireo;
 
+import java.io.File;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.net.URL;
 import java.util.jar.Manifest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.ansi.AnsiElement;
 import org.springframework.boot.ansi.AnsiOutput;
@@ -17,7 +22,9 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"edu.tamu.framework", "edu.tamu.auth", "org.tdl.vireo"})
-public class Application extends SpringBootServletInitializer implements Banner {
+public class Application extends SpringBootServletInitializer implements Banner, CommandLineRunner {
+    
+    final static Logger logger = LoggerFactory.getLogger(Application.class);
 
     private static final String[] BANNER = { "",
             "                                             ",
@@ -68,7 +75,7 @@ public class Application extends SpringBootServletInitializer implements Banner 
             throw new RuntimeException("Loading MANIFEST for class " + clz + " failed!", e);
         }
     }
-
+    
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         application.banner(this);
@@ -77,5 +84,33 @@ public class Application extends SpringBootServletInitializer implements Banner 
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Autowired
+    private Environment env;
+    
+    static {
+        String applicationClassPathRoot = Application.class.getResource("/").getPath();
+        File applicationClassPath = new File(applicationClassPathRoot);
+        // if we're running in an expanded war
+        if(applicationClassPath.exists() && applicationClassPath.isDirectory()) {
+            File customProps = new File(applicationClassPathRoot + "../../../conf/application.properties");
+            if(customProps.exists() && customProps.isFile()) {
+                System.setProperty("spring.config.location", "file://" + customProps.getAbsolutePath());
+            }
+        }
+        // if we're a jar or a war
+        else if(applicationClassPath.exists() && applicationClassPath.isFile() && (applicationClassPathRoot.endsWith(".jar") || applicationClassPathRoot.endsWith(".war"))) {
+            File customProps = new File(applicationClassPath.getParent() + "/conf/application.properties");
+            if(customProps.exists() && customProps.isFile()) {
+                System.setProperty("spring.config.location", "file://" + customProps.getAbsolutePath());
+            }
+        }
+    }
+    
+    @Override
+    public void run(String... arg0) throws Exception {
+        logger.info("Classpath root is: " + Application.class.getResource("/").getPath());
+        logger.info("RUNNING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! [" + env.getProperty("security.user.password") + "]");
     }
 }
