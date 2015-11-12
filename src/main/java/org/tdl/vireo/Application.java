9f669e088f0c3e97fd3e40a4987a0017fb7f3e5f
William Welling <wwelling@library.tamu.edu>
2015-11-11 19:47:22 -0600

    Improved data-toggle within ng-if of modal and dropdown.

--- src/main/java/org/tdl/vireo/Application.java
+++ src/main/java/org/tdl/vireo/Application.java
@@ -22,7 +22,7 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"edu.tamu.framework", "edu.tamu.auth", "org.tdl.vireo"})
-public class Application extends SpringBootServletInitializer implements Banner, CommandLineRunner {
+public class Application extends SpringBootServletInitializer implements Banner , CommandLineRunner {
     
     final static Logger logger = LoggerFactory.getLogger(Application.class);
 
@@ -113,4 +113,5 @@ public class Application extends SpringBootServletInitializer implements Banner,
         logger.info("Classpath root is: " + Application.class.getResource("/").getPath());
         logger.info("RUNNING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! [" + env.getProperty("security.user.password") + "]");
     }
+    
 }
