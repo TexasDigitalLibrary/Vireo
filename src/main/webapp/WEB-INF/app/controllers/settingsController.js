14b126f628516bbee9c349104332614a314f8630
Rincy Mathew <rmathew@tamu.edu>
2015-11-19 16:12:13 -0600

    Modified the settings controller

--- src/main/webapp/WEB-INF/app/controllers/settingsController.js
+++ src/main/webapp/WEB-INF/app/controllers/settingsController.js
@@ -1,12 +1,8 @@
 vireo.controller('SettingsController', function ($controller, $scope, $location, $routeParams, TabService, User) {
 	angular.extend(this, $controller('AbstractController', {$scope: $scope}));
 	$scope.user  = User.get();
-
-	$scope.clicked = '+';
-
-	$scope.changeSymbol = function() {
-		console.log("HERE");
-		$scope.clicked = $scope.clicked=='+'? '-':'+';
-	}
-
+	User.ready().then(function(){
+		$scope.user.displayName = $scope.user.firstName+ ' ' +$scope.user.lastName;
+	});
+console.log($scope.user);
 });
--- src/main/webapp/WEB-INF/app/directives/sideBoxDirective.js
+++ src/main/webapp/WEB-INF/app/directives/sideBoxDirective.js
@@ -7,6 +7,22 @@ vireo.directive("sidebox", function() {
 		scope: true,
 		link: function ($scope, element, attr) {	    	
 			$scope.heading = attr.heading;
+			$scope.clicked = '-';
+
+			$scope.changeSymbol = function() {
+				$scope.clicked = $scope.clicked=='-'? '+':'-';
+			}
+
 	    }
+	};
+});
+
+vireo.directive("sideboxsub", function() {
+	return {
+		template: '<div class="sidebox-sub-heading" ng-transclude></div>',
+		restrict: 'E',
+		replace: false,
+		transclude: true,
+		scope: true
 	};
 });
