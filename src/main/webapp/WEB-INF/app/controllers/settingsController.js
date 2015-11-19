f266678158b149fadf2f50b6277867dea837777d
Rincy Mathew <rmathew@tamu.edu>
2015-11-19 09:54:23 -0600

    Work in Progress for Settings Controller

--- src/main/webapp/WEB-INF/app/controllers/settingsController.js
+++ src/main/webapp/WEB-INF/app/controllers/settingsController.js
@@ -1,5 +1,12 @@
 vireo.controller('SettingsController', function ($controller, $scope, $location, $routeParams, TabService, User) {
 	angular.extend(this, $controller('AbstractController', {$scope: $scope}));
 	$scope.user  = User.get();
-	
+
+	$scope.clicked = '+';
+
+	$scope.changeSymbol = function() {
+		console.log("HERE");
+		$scope.clicked = $scope.clicked=='+'? '-':'+';
+	}
+
 });
