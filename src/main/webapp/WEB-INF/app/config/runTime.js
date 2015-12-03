d9e36b536392cb1de98e0483f3cd4b087918db1e
Rincy Mathew <rmathew@tamu.edu>
2015-12-03 14:49:37 -0600

    removed authservice get refresh token from runtime.js  and added ccemail to preferences for Profile Page

--- src/main/webapp/WEB-INF/app/config/runTime.js
+++ src/main/webapp/WEB-INF/app/config/runTime.js
@@ -3,41 +3,5 @@ vireo.run(function($rootScope, $http, AuthServiceApi) {
 	angular.element("body").fadeIn(1000);
 
 	// Add runtime tasks here
-
-	//Override Authservice Refresh
-	AuthServiceApi.getRefreshToken = function(cb) {
-
-		var url = appConfig.authService+"/refresh";
-		if(appConfig.mockRole) url += "&mock=" + appConfig.mockRole;
-
-		if (!AuthServiceApi.pendingRefresh) {
-			AuthServiceApi.pendingRefresh = $http.get(url, {withCredentials: true}).
-				then(function(response) {
-
-						StorageService.set('token', response.data.tokenAsString);
-
-						// This timeout ensures that pending request is not nulled to early
-						$timeout(function() {
-							delete AuthServiceApi.pendingRefresh;
-						});
-
-						if(cb) cb();
-					},
-					function(response) {
-						delete sessionStorage.token;
-
-						if(appConfig.mockRole) {
-							url += "&referer="+location.href
-							window.open(appConfig.authService + "/token?referer="+location.href + "&mock=" + appConfig.mockRole, "_self");
-						}
-						else {
-							url += "?referer="+location.href
-							window.open(appConfig.authService + "/token?referer="+location.href, "_self");
-						}
-
-				});
-		}
-		return AuthServiceApi.pendingRefresh;
-	};
 	
 });
