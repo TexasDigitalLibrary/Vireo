53e5a09fc4da52a6eba7b92bba190cbe87383037
jeremythuff <jeremythuff@gmail.com>
2015-12-08 15:50:21 -0600

    Integrated tool-tip

--- src/main/webapp/WEB-INF/app/directives/tooltipDirective.js
+++ src/main/webapp/WEB-INF/app/directives/tooltipDirective.js
@@ -1,12 +1,21 @@
-vireo.directive('tooltip', function ($timeout) {
+vireo.directive('tooltip', function ($timeout, $compile) {
 
 	return {
-		templateUrl: "views/directives/tooltip.html",
+		template: '<span class="tooltip-handle" ng-mousemove="positionTip($event)" ng-mouseover="showTip()" ng-mouseout="hideTip()" ng-click="toggleVisible()" ng-transclude></span>',
 		replace: true,
 		transclude: true,
 		restrict: 'A',
 		scope:true,
 		link: function($scope, elem, attr) {
+			
+			var tipTemplate = 	'<div class="tooltip-wrapper">'+
+									'<div ng-style="tipStyles" class="tip" ng-class="{\'tip-visible\': tipVisible}">'+
+										'<div class="tip-point"></div>' +
+										'<div class="tip-message">{{::tip}}</div>' +
+									'</div>' +
+								'</div>';
+			
+			angular.element("body").append($compile(tipTemplate)($scope));
 
 			$scope.tip = attr.tooltip;
 
@@ -31,8 +40,8 @@ vireo.directive('tooltip', function ($timeout) {
 			}
 
 			$scope.positionTip = function($event) {
-				$scope.tipStyles["top"] = $event.offsetY + 20;
-				$scope.tipStyles["left"] = $event.offsetX -	25;
+				$scope.tipStyles["top"] = $event.clientY + 20;
+				$scope.tipStyles["left"] = $event.clientX -	25;
 			}
 		}
 	};
