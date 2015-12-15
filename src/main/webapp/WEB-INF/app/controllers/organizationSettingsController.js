7a4e9a57039b04762ba94e175b7166382cb90468
jsavell <jsavell@library.tamu.edu>
2015-12-15 10:15:31 -0600

    use mock data to fill in org ui

--- src/main/webapp/WEB-INF/app/controllers/organizationSettingsController.js
+++ src/main/webapp/WEB-INF/app/controllers/organizationSettingsController.js
@@ -2,8 +2,13 @@ vireo.controller('OrganizationSettingsController', function ($controller, $scope
 	angular.extend(this, $controller('AbstractController', {$scope: $scope}));
 
 	$scope.newOrganization = {};
+
+//	$scope.organizations = OrganizationRepo.get();
+
+	$scope.organizations = [{"name":"Organization 1"},{"name":"Organization 2"}];
+
 	$scope.createOrganization = function(organization) {
-		OrganizationRepo.add(organization);
+//		OrganizationRepo.add(organization);
 		console.log("the org:");
 		console.log(organization);
 	};
