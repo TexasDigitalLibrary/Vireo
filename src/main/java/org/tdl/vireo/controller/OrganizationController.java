6bd037f8c7cdb4b60de31103c29349d1743abe12
jsavell <jsavell@library.tamu.edu>
2015-12-15 10:08:22 -0600

    (wip) create and all endpoints for org controller, with TODOs for model roadblocks

--- src/main/java/org/tdl/vireo/controller/OrganizationController.java
+++ src/main/java/org/tdl/vireo/controller/OrganizationController.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.tdl.vireo.model.Organization;
 import org.tdl.vireo.model.User;
+import org.tdl.vireo.model.repo.OrganizationCategoryRepo;
 import org.tdl.vireo.model.repo.OrganizationRepo;
 import org.tdl.vireo.model.repo.UserRepo;
 
@@ -36,7 +37,10 @@ public class OrganizationController {
     
     @Autowired
     private OrganizationRepo organizationRepo;
-    
+
+    @Autowired
+    private OrganizationCategoryRepo organizationCategoryRepo;
+
     @Autowired
     private ObjectMapper objectMapper;
     
@@ -45,21 +49,25 @@ public class OrganizationController {
     
     private static final Logger logger = Logger.getLogger(OrganizationController.class);
         
+    //TODO: Resolve model issues: infinite recursion due to org category relationship. JsonIdentityInfo annotation seems to help
     @ApiMapping("/all")
     @Auth(role="ROLE_MANAGER")
+    @Transactional
     public ApiResponse allOrganizations() {
         Map<String,List<Organization>> map = new HashMap<String,List<Organization>>();        
         map.put("list", organizationRepo.findAll());
         return new ApiResponse(SUCCESS, map);
     }
 
+    //TODO: Resolve model issues: lazy initialization error when trying to get an Org Cat from the repo
     @ApiMapping("/create")
     @Auth(role="ROLE_MANAGER")
     public ApiResponse createOrganization(@Data String data) {
         Map<String,String> map = new HashMap<String,String>();      
         try {
             map = objectMapper.readValue(data, new TypeReference<HashMap<String,String>>(){});
-            System.out.println(map.get("newOrganization").toString());
+//            System.out.println("cat name: "+organizationCategoryRepo.getOne(1L).getName());
+//            organizationRepo.create(map.get("name").toString(),organizationCategoryRepo.getOne(1L));
         } catch (Exception e) {
             e.printStackTrace();
         } 
