39477f7c8447113257775060cf0081e53ffc4351
jsavell <jsavell@library.tamu.edu>
2015-12-16 08:53:23 -0600

    mockup for organization categories

--- src/main/webapp/WEB-INF/app/controllers/organizationSettingsController.js
+++ src/main/webapp/WEB-INF/app/controllers/organizationSettingsController.js
@@ -2,14 +2,24 @@ vireo.controller('OrganizationSettingsController', function ($controller, $scope
 	angular.extend(this, $controller('AbstractController', {$scope: $scope}));
 
 	$scope.newOrganization = {};
+	$scope.newCategory = {};
 
 //	$scope.organizations = OrganizationRepo.get();
 
 	$scope.organizations = [{"name":"Organization 1"},{"name":"Organization 2"}];
+	$scope.organizationCategories = [{"name":"Category 1","level":1},{"name":"Category 2","level":2}];
 
 	$scope.createOrganization = function(organization) {
 //		OrganizationRepo.add(organization);
+		angular.extend($scope.organizations,[organization]);
 		console.log("the org:");
 		console.log(organization);
+	};
+
+	$scope.createOrganizationCategory = function(category) {
+//		OrganizationCategoryRepo.add(category);
+		console.log("the org category:");
+		console.log(category);
+		angular.extend($scope.categories,[category]);
 	};
 });
