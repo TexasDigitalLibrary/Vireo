26a1d152c84e9802ab52d8a49cdd470e1f1404ab
jsavell <jsavell@library.tamu.edu>
2015-12-16 09:52:07 -0600

    early days of a default settings service and preference object

---
+++ src/main/java/org/tdl/vireo/model/DefaultPreference.java
@@ -0,0 +1,8 @@
+package org.tdl.vireo.model;
+
+public class DefaultPreference extends AbstractPreference {
+
+    public DefaultPreference(String key, String value, String type) {
+        super(key, value, type);
+    }
+}
