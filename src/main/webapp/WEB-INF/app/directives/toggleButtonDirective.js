b44f15e05df2637779a6cef52409aef8e3e72c05
Simon Frazier <sfrazier@library.tamu.edu>
2016-02-19 15:49:34 -0600

    Improved toggle button.

--- src/main/webapp/WEB-INF/app/directives/toggleButtonDirective.js
+++ src/main/webapp/WEB-INF/app/directives/toggleButtonDirective.js
@@ -5,24 +5,23 @@ vireo.directive("togglebutton", function() {
 		scope: {
 			"label": "@",
 			"scopeValue": "=",
-			"toggleOptions": "@"
+                        "toggleOptions": "@",
+                        "toolTip": "@"
 		},
 		controller: function($scope) {
-			
 			$scope.setActive = function(scopeValue) {
 				$scope.scopeValue = scopeValue;
-			}	
+			}
 
-		}, 
+		},
 		link: function($scope, element, attr) {
-	        
+
 			var optionsObj = angular.fromJson($scope.toggleOptions)
-			
 			$scope.options = {};
 
 			for(var index in optionsObj) {
 				var option = optionsObj[index];
-				$scope.options["option"+ index] = { 
+				$scope.options["option"+ index] = {
 					gloss: option[Object.keys(option)[0]],
 					evaluation: Object.keys(option)[0]
 				}
@@ -30,4 +29,3 @@ vireo.directive("togglebutton", function() {
 		}
 	};
 });
-
--- src/main/webapp/WEB-INF/app/index.jsp
+++ src/main/webapp/WEB-INF/app/index.jsp
@@ -242,7 +242,7 @@
 	    <script src="directives/accordionDirective.js"></script>
 	    <script src="directives/tabsDirective.js"></script>
 	    <script src="directives/sideBoxDirective.js"></script>
- 		<script src="directives/togglebuttonDirective.js"></script>
+ 		<script src="directives/toggleButtonDirective.js"></script>
 	    <script src="directives/userSettingsDirective.js"></script>
 	    <script src="directives/legendDirective.js"></script>
 	    <script src="directives/shadowDirective.js"></script>	
