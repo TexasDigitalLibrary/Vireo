a42ffd1b622a8ab1c118c290c769fce8aa7d5ddb
William Welling <wwelling@library.tamu.edu>
2016-02-24 20:23:04 -0600

    Drag over trash style change.

--- src/main/webapp/WEB-INF/app/controllers/depositLocationRepoController.js
+++ src/main/webapp/WEB-INF/app/controllers/depositLocationRepoController.js
@@ -9,8 +9,52 @@ vireo.controller("DepositLocationRepoController", function ($controller, $scope,
 
 	$scope.dragging = false;
 
-	$scope.trashCan = 'deposit-location-trash';
-
+	$scope.trashCanId = 'deposit-location-trash';
+	
+	var trash = {
+		hover: false,
+		element: null
+	};
+		
+	$scope.dragControlListeners = {
+		dragStart: function(event) {
+			$scope.dragging = true;
+		},
+		dragMove: function(event) {
+			if(trash.hover) {
+				trash.hover = false;
+				trash.element.removeClass('dragging');
+			}
+		},
+		dragEnd: function(event) {
+			$scope.dragging = false;
+			if(trash.hover) {
+				var index = event.source.index + 1;
+				$scope.removeDepositLocation(index);
+				trash.element.removeClass('dragging');
+			}
+		},
+	    accept: function (sourceItemHandleScope, destSortableScope) {
+	    	var currentElement = destSortableScope.element;
+	    	if(currentElement[0].id == $scope.trashCanId) {
+	    		trash.hover = true;
+	    		trash.element = currentElement;
+     			trash.element.addClass('dragging');
+     		}
+     		else {	     			
+     			trash.hover = false;
+     		}
+	     	return sourceItemHandleScope.itemScope.sortableScope.$id === destSortableScope.$id;
+	    },
+	    orderChanged: function(event) {
+	    	if(!trash.hover) {
+	    		var src = event.source.index + 1;
+	    		var dest = event.dest.index + 1;
+	    		$scope.reorderDepositLocation(src, dest);
+	    	}
+	    }
+	};
+	
 	$scope.ready.then(function() {
 
 		$scope.createDepositLocation = function() {
@@ -25,40 +69,7 @@ vireo.controller("DepositLocationRepoController", function ($controller, $scope,
 		$scope.removeDepositLocation = function(index) {
 	    	DepositLocationRepo.remove(index);
 		};
-
-		var overTrash = false;
-
-		$scope.dragControlListeners = {
-			dragStart: function(event) {
-				$scope.dragging = true;
-			},
-			dragEnd: function(event) {
-				$scope.dragging = false;
-				if(overTrash) {
-					console.log('trash it!');
-					var index = event.source.index + 1;
-					$scope.removeDepositLocation(index);
-				}
-			},
-		    accept: function (sourceItemHandleScope, destSortableScope) {
-		    	var id = destSortableScope.element[0].id;
-	     		if(id == $scope.trashCan) {
-	     			overTrash = true;
-	     		}
-	     		else {
-	     			overTrash = false;
-	     		}
-		     	return sourceItemHandleScope.itemScope.sortableScope.$id === destSortableScope.$id;
-		    },
-		    orderChanged: function(event) {
-		    	if(!overTrash) {
-		    		var src = event.source.index + 1;
-		    		var dest = event.dest.index + 1;
-		    		$scope.reorderDepositLocation(src, dest);
-		    	}
-		    }
-		};
-
+		
 	});	
 
 });
