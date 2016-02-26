d3707022a481262f2d9531cb17bd348733b20f52
Gad Krumholz <gad.krumholz@austin.utexas.edu>
2016-02-26 13:33:59 -0600

    Fixed off-by-one error in toConfigPairsMap

--- src/main/java/org/tdl/vireo/controller/ConfigurableSettingsController.java
+++ src/main/java/org/tdl/vireo/controller/ConfigurableSettingsController.java
@@ -87,14 +87,24 @@ public class ConfigurableSettingsController {
     private Map<String, Map<String, String>> toConfigPairsMap(List<Configuration> configurations) {
         Map<String, Map<String,String>> typesToConfigPairs = new HashMap<String, Map<String,String>>();
         Map<String,String> items = new HashMap<String, String>();
+        // keep track of the last type.
         String lastType = "";
         for(Configuration configuration : configurations) {
+            // if the last type doesn't equal the current configuration's type
             if(!lastType.equals(configuration.getType())) {
+                // put all the items in the return map
                 typesToConfigPairs.put(lastType, items);
+                // start a new items map
                 items = new HashMap<String, String>();
+                // change the lastType
                 lastType = configuration.getType();
             }
+            // add the item to the items list
             items.put(configuration.getName(), configuration.getValue());
+        }
+        // the last configuration type will not be added to the return map, so we add it here.
+        if(!items.isEmpty()) {
+            typesToConfigPairs.put(lastType, items);
         }
         return typesToConfigPairs;
     }
