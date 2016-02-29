8a8ef2a374a8778bf0c30bc75bd271a94479a540
Gad Krumholz <gad.krumholz@austin.utexas.edu>
2016-02-29 11:39:18 -0600

    Fixed karma test not finding ngSanitize. cleaned up extra <p> in home view. fixed typo in lookAndFeel

--- src/main/webapp/WEB-INF/tests/karma.conf.js
+++ src/main/webapp/WEB-INF/tests/karma.conf.js
@@ -8,6 +8,7 @@ module.exports = function(config){
 
             'app/bower_components/jquery/dist/jquery.js',
             'app/bower_components/angular/angular.js',
+            'app/bower_components/angular-sanitize/angular-sanitize.min.js',
             'app/bower_components/angular-mocks/angular-mocks.js',
             'app/bower_components/angular-route/angular-route.js',
 
