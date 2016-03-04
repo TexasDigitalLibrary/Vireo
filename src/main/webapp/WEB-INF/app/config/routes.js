6dda9abb0484702a91fe1ae59a58d0eb0b642ce3
jeremythuff <jeremythuff@gmail.com>
2016-03-04 08:52:55 -0600

    Added error pages and routes

--- src/main/webapp/WEB-INF/app/config/routes.js
+++ src/main/webapp/WEB-INF/app/config/routes.js
@@ -15,26 +15,53 @@ vireo.config(['$routeProvider', '$locationProvider', '$anchorScrollProvider', fu
 		}).
 		when('/admin', {
 			redirectTo: '/admin/list',
+			access: ["ROLE_ADMIN"]
 		}).
 		when('/admin/list', {
 			templateUrl: 'views/admin/list.html',
+			access: ["ROLE_ADMIN"]
 		}).
 		when('/admin/view', {
 			templateUrl: 'views/admin/view.html',
+			access: ["ROLE_ADMIN"]
 		}).
 		when('/admin/log', {
 			templateUrl: 'views/admin/log.html',
+			access: ["ROLE_ADMIN"]
 		}).
 		when('/admin/settings', {
 			redirectTo: '/admin/settings/application',
+			access: ["ROLE_ADMIN"]
 		}).
 		when('/admin/settings/:tab', {
 			templateUrl: 'views/admin/settings/settings.html',
+			access: ["ROLE_ADMIN"],
 			controller: 'SettingsController',
 			reloadOnSearch: false
 		}).
-		otherwise({
-			redirectTo: '/home',
+		when('/home', {
 			templateUrl: 'views/home.html'
+		}).
+		when('/', {
+			redirectTo: '/home'
+		}).
+
+		// Error Routes
+		when('/error/403', {
+			templateUrl: 'views/errors/403.html',
+			controller: 'ErrorPageController'
+		}).
+		when('/error/404', {
+			templateUrl: 'views/errors/404.html',
+			controller: 'ErrorPageController'
+
+		}).
+		when('/error/500', {
+			templateUrl: 'views/errors/500.html',
+			controller: 'ErrorPageController'
+		}).
+		otherwise({
+			redirectTo: '/error/404'
 		});
+
 }]);
