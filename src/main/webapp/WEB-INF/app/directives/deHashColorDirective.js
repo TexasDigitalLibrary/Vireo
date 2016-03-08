ca1634922e12a3cc879fed714a7c5ddd91ae2a84
jeremythuff <jeremythuff@gmail.com>
2016-03-08 11:38:36 -0600

    Added a deHashColorDirective to remove the hash from color selectors

---
+++ src/main/webapp/WEB-INF/app/directives/deHashColorDirective.js
@@ -0,0 +1,30 @@
+vireo.directive('dehashcolor', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, element, attrs, ngModelController) {
+      ngModelController.$parsers.push(function(fromView) {
+       	
+       	var toModel = "";
+		if(fromView[0] != "#") {
+			toModel = "#" + fromView;
+		} else {
+			toModel = fromView;
+		}
+
+        return toModel; //converted
+
+      });
+
+      ngModelController.$formatters.push(function(fromModel) {
+        
+        var toView = "";
+		if(!fromModel) fromModel = "";
+
+		if(fromModel[0] == "#") toView = fromModel.substring(1, fromModel.length);
+
+        return toView; //converted
+
+      });
+    }
+  }
+});
