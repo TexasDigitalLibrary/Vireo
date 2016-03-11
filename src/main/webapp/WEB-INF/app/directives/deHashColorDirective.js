c692eb42100d628be023915b7b51b0560b94cbc3
jeremythuff <jeremythuff@gmail.com>
2016-03-11 15:22:30 -0600

    Changed spacing on dehashcolordirective

--- src/main/webapp/WEB-INF/app/directives/deHashColorDirective.js
+++ src/main/webapp/WEB-INF/app/directives/deHashColorDirective.js
@@ -1,30 +1,30 @@
 vireo.directive('dehashcolor', function() {
-  return {
-    require: 'ngModel',
-    link: function(scope, element, attrs, ngModelController) {
-      ngModelController.$parsers.push(function(fromView) {
-       	
-       	var toModel = "";
-		if(fromView[0] != "#") {
-			toModel = "#" + fromView;
-		} else {
-			toModel = fromView;
-		}
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModelController) {
+            ngModelController.$parsers.push(function(fromView) {
 
-        return toModel; //converted
+                var toModel = "";
+                if(fromView[0] != "#") {
+                    toModel = "#" + fromView;
+                } else {
+                    toModel = fromView;
+                }
 
-      });
+                return toModel; //converted
 
-      ngModelController.$formatters.push(function(fromModel) {
-        
-        var toView = "";
-		if(!fromModel) fromModel = "";
+            });
 
-		if(fromModel[0] == "#") toView = fromModel.substring(1, fromModel.length);
+            ngModelController.$formatters.push(function(fromModel) {
 
-        return toView; //converted
+                var toView = "";
+                if(!fromModel) fromModel = "";
 
-      });
+                if(fromModel[0] == "#") toView = fromModel.substring(1, fromModel.length);
+
+                return toView; //converted
+
+            });
+        }
     }
-  }
 });
