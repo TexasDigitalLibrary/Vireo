e2e035a50cd1460f21a745596077c7f35f5e5979
William Welling <wwelling@library.tamu.edu>
2016-04-07 16:07:06 -0500

    Use drag and drop list directive overriding functionality to disable reorering.

--- src/main/webapp/WEB-INF/app/controllers/settings/organizationCategoriesController.js
+++ src/main/webapp/WEB-INF/app/controllers/settings/organizationCategoriesController.js
@@ -1,18 +1,28 @@
-vireo.controller("OrganizationCategoriesController", function ($controller, $scope, $q, OrganizationCategoryRepoModel) {
+vireo.controller("OrganizationCategoriesController", function ($controller, $scope, $q, OrganizationCategoryRepoModel, DragAndDropListenerFactory) {
   angular.extend(this, $controller("AbstractController", {$scope: $scope}));
 
   $scope.organizationCategories = OrganizationCategoryRepoModel.get();
 
   $scope.ready = $q.all([OrganizationCategoryRepoModel.ready()]);
 
+  $scope.dragging = false;
+
+  $scope.trashCanId = 'organization-category-trash';
+
   $scope.ready.then(function() {
+
+    console.log($scope.organizationCategories)
 
     $scope.resetOrganizationCategories = function() {
       $scope.modalData = {'name':''};
     }
 
     $scope.resetOrganizationCategories();
-    console.info($scope);
+    
+    $scope.selectOrganizationCategory = function(index) {
+      $scope.resetOrganizationCategories();
+      $scope.modalData = $scope.organizationCategories.list[index];
+    };
 
     $scope.createOrganizationCategory = function() {
       OrganizationCategoryRepoModel.add($scope.modalData).then(function(){
@@ -35,6 +45,35 @@ vireo.controller("OrganizationCategoriesController", function ($controller, $sco
     //     $scope.resetOrganizationCategories();
     //   });
     // };
+
+    $scope.dragControlListeners = DragAndDropListenerFactory.buildDragControls({
+      trashId: $scope.trashCanId,
+      dragging: $scope.dragging,
+      select: $scope.selectOrganizationCategory,     
+      model: $scope.organizationCategories,
+      confirm: '#organizationCategoryConfirmRemoveModal',
+      reorder: null,
+      container: '#organization-categories'
+    });
+
+    console.log($scope.dragControlListeners);
+
+    var listener = $scope.dragControlListeners.getListener();
+
+   $scope.dragControlListeners.accept = function (sourceItemHandleScope, destSortableScope) {
+      var currentElement = destSortableScope.element;
+      if(listener.dragging && currentElement[0].id == listener.trash.id) {
+        listener.trash.hover = true;
+        listener.trash.element = currentElement;
+        listener.trash.element.addClass('dragging');
+      }
+      else {
+        listener.trash.hover = false;
+      }
+      return false;
+    };
+
+    $scope.dragControlListeners.orderChanged = function (event) {};
 
   });
 });
