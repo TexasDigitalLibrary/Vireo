2552a793817cf0e045671b00b02cf759a10fa117
Simon Frazier <sfrazier@library.tamu.edu>
2016-04-26 16:16:37 -0500

    Working mock of step reordering.

--- src/main/webapp/WEB-INF/app/controllers/settings/organization/organizationStepsController.js
+++ src/main/webapp/WEB-INF/app/controllers/settings/organization/organizationStepsController.js
@@ -2,18 +2,94 @@ vireo.controller("OrganizationStepsController", function ($controller, $scope, $
 
   angular.extend(this, $controller("AbstractController", {$scope: $scope}));
 
-  // TODO get real model data; also need to inject
-  // $scope.organizationSteps = OrganizationStepsRepo.get();
-  // $scope.ready = $q.all([OrganizationStepsRepo.ready()]);
-  // Mock for now:
 
-  $scope.steps = ['foo', 'bar', 'qux'];
+
+  //TODO grab the currently selected organization off the parent scope. (from organizationSettingsController);
+  //For now we mock with the following JSON:
+  $scope.stepOrder = [0,2,1];
+  $scope.steps = [{
+    id: 0,
+    name:'step name foo',
+    originatingOrganization:1,
+    containedByOrganizations:[0,1],
+    optional:true,
+    fieldProfiles:[
+      {name:'fp name 1'},
+      {name:'fp name 2'},
+      {name:'fp name 3'}
+    ],
+    fieldProfileOrder:[1,0,2],
+    notes:[{name:'note name', text: 'note text'}]
+  }, {
+    id: 1,
+    name:'step name bar',
+    originatingOrganization:1,
+    containedByOrganizations:[0,1],
+    optional:false,
+    fieldProfiles:[
+      {name:'fp name foo'},
+      {name:'fp name bar'},
+      {name:'fp name qux'}
+    ],
+    fieldProfileOrder:[1,0,2],
+    notes:[{name:'note name', text: 'note text'}]
+  },{
+    id: 2,
+    name:'step name qux',
+    originatingOrganization:1,
+    containedByOrganizations:[0,1],
+    optional:false,
+    fieldProfiles:[
+      {name:'fp name foo'},
+      {name:'fp name bar'},
+      {name:'fp name qux'}
+    ],
+    fieldProfileOrder:[1,0,2],
+    notes:[{name:'note name', text: 'note text'}]
+  }];
+  //End temporary mock JSON
+
+
+
+  var reorder = function(originIdx, destinationIdx){
+    if (!(originIdx == 0 && destinationIdx == -1) && !(originIdx == $scope.steps.length-1 && destinationIdx == $scope.steps.length)) {
+      var tmp = $scope.stepOrder[originIdx];
+      $scope.stepOrder[originIdx] = $scope.stepOrder[destinationIdx];
+      $scope.stepOrder[destinationIdx] = tmp;
+      tempRefreshStepOrder();
+    }
+  };
+
+  var stepForId = function(id){
+    for(var i = 0; i < $scope.steps.length; i++) {
+      if ($scope.steps[i].id == id) {
+        return $scope.steps[i];
+      }
+    }
+  }
+
+  var tempRefreshStepOrder = function(){
+    var tmp = ['a', 'b', 'c'];
+    for(var i=0; i<$scope.steps.length; ++i) {
+      tmp[i] = stepForId($scope.stepOrder[i]);
+    }
+    $scope.steps = tmp;
+    //TODO would save the organization (and thus the ordering) here; refresh from the broadcast.
+  };
 
   $scope.ready.then(function() {
 
+    $scope.reorderUp = function(originIdx) {
+      reorder(originIdx, originIdx-1); //TODO handle originIdx = 0
+    };
+
+    $scope.reorderDown = function(originIdx) {
+      reorder(originIdx, originIdx+1); //TODO handle originIdx = array.length-1
+    };
+
     $scope.resetOrganizationSteps = function() {
       $scope.modalData = {'name':''};
-    }
+    };
 
     $scope.resetOrganizationSteps();
     
@@ -46,9 +122,10 @@ vireo.controller("OrganizationStepsController", function ($controller, $scope, $
     };
 
     $scope.printState = function() {
-      console.info('the parent org is V');
-      console.info($scope.selectedOrganization);
-    }
+      tempRefreshStepOrder();
+    };
+    tempRefreshStepOrder();
+    console.info($scope.stepOrder);
 
   });
 });
