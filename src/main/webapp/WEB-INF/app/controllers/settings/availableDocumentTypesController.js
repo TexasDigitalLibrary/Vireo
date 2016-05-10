e61360909a99b7a08fb43b6c8d9404b506d96357
Gad Krumholz <gad.krumholz@austin.utexas.edu>
2016-05-10 14:55:10 -0500

    Implemented vir-202 for availableDocumentTypesController.js

--- src/main/webapp/WEB-INF/app/controllers/settings/availableDocumentTypesController.js
+++ src/main/webapp/WEB-INF/app/controllers/settings/availableDocumentTypesController.js
@@ -6,6 +6,8 @@ vireo.controller("AvailableDocumentTypesController", function ($controller, $sco
 	$scope.ready = AvailableDocumentTypesRepo.ready();
 
 	$scope.dragging = false;
+	
+	$scope.serverErrors = [];
 
 	$scope.trashCanId = 'available-document-types-trash';
 	
@@ -22,43 +24,59 @@ vireo.controller("AvailableDocumentTypesController", function ($controller, $sco
                 degreeLevel: 'UNDERGRADUATE'
             };
         };
+        
+        $scope.closeModal = function(modalId) {
+    		angular.element('#' + modalId).modal('hide');
+    		// clear all errors, but not infos or warnings
+    		if($scope.serverErrors !== undefined) {
+    			$scope.serverErrors.errors = undefined;
+    		}
+    	}
 
         $scope.resetDocumentTypes();
 
         $scope.createNewDocumentType = function(documentType) {
             AvailableDocumentTypesRepo.add(documentType).then(function(data) {
-            	var validationResponse = angular.fromJson(data.body).payload.ValidationResponse;
-                console.log(validationResponse);
-                $scope.resetDocumentTypes();
+            	$scope.serverErrors = angular.fromJson(data.body).payload.ValidationResponse;
+            	if($scope.serverErrors === undefined || $scope.serverErrors.errors.length == 0) {
+            		$scope.resetDocumentTypes();
+            		$scope.closeModal("availableDocumentTypesNewModal");
+            	}
             });
 	    };	
 
         $scope.launchEditModal = function(index) {
+        	$scope.serverErrors = [];
             $scope.modalData = $scope.documentTypes.list[index -1];
             angular.element('#availableDocumentTypesEditModal').modal('show');
 	    };	
 
         $scope.updateDocumentType = function(){
             AvailableDocumentTypesRepo.update($scope.modalData).then(function(data) {
-            	var validationResponse = angular.fromJson(data.body).payload.ValidationResponse;
-                console.log(validationResponse);
-                $scope.resetDocumentTypes();
+            	$scope.serverErrors = angular.fromJson(data.body).payload.ValidationResponse;
+            	if($scope.serverErrors === undefined || $scope.serverErrors.errors.length == 0) {
+            		$scope.resetDocumentTypes();
+            		$scope.closeModal("availableDocumentTypesEditModal");
+            	}
             });
         }
 
         $scope.removeDocumentType = function(index){
             AvailableDocumentTypesRepo.remove(index).then(function(data) {
-            	var validationResponse = angular.fromJson(data.body).payload.ValidationResponse;
-                console.log(validationResponse);
-                $scope.resetDocumentTypes();
+            	$scope.serverErrors = angular.fromJson(data.body).payload.ValidationResponse;
+            	if($scope.serverErrors === undefined || $scope.serverErrors.errors.length == 0) {
+            		$scope.resetDocumentTypes();
+            		$scope.closeModal("availableDocumentTypesConfirmRemoveModal");
+            	}
             });
         }
 
         $scope.reorderDocumentTypes = function(src, dest) {
             AvailableDocumentTypesRepo.reorder(src, dest).then(function(data) {
-            	var validationResponse = angular.fromJson(data.body).payload.ValidationResponse;
-                console.log(validationResponse);
-                $scope.resetDocumentTypes();
+            	$scope.serverErrors = angular.fromJson(data.body).payload.ValidationResponse;
+            	if($scope.serverErrors === undefined || $scope.serverErrors.errors.length == 0) {
+            		$scope.resetDocumentTypes();
+            	}
             });
         };
 
@@ -72,9 +90,10 @@ vireo.controller("AvailableDocumentTypesController", function ($controller, $sco
             }
             else if($scope.sortAction == 'sort') {
                 AvailableDocumentTypesRepo.sort(column).then(function(data) {
-                	var validationResponse = angular.fromJson(data.body).payload.ValidationResponse;
-                    console.log(validationResponse);
-                    $scope.resetDocumentTypes();
+                	$scope.serverErrors = angular.fromJson(data.body).payload.ValidationResponse;
+                	if($scope.serverErrors === undefined || $scope.serverErrors.errors.length == 0) {
+                		$scope.resetDocumentTypes();
+                	}
                 });
                 $scope.sortAction = 'confirm';
             }
