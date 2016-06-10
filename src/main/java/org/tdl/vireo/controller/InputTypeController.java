ae3472c9c9ccd09524ffa8cf21cf4d3fbc6908e0
Simon Frazier <sfrazier@library.tamu.edu>
2016-06-10 14:57:17 -0500

    Cleanup of inputTypes server controller.

--- src/main/java/org/tdl/vireo/controller/InputTypeController.java
+++ src/main/java/org/tdl/vireo/controller/InputTypeController.java
@@ -1,39 +1,22 @@
 package org.tdl.vireo.controller;
 
 import static edu.tamu.framework.enums.ApiResponseType.SUCCESS;
-import static edu.tamu.framework.enums.ApiResponseType.VALIDATION_WARNING;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.tdl.vireo.enums.InputType;
-import org.tdl.vireo.model.ControlledVocabulary;
-import org.tdl.vireo.model.ControlledVocabularyCache;
-import org.tdl.vireo.model.VocabularyWord;
-import org.tdl.vireo.model.repo.ControlledVocabularyRepo;
-import org.tdl.vireo.model.repo.VocabularyWordRepo;
-import org.tdl.vireo.service.ControlledVocabularyCachingService;
 import org.tdl.vireo.service.ValidationService;
 
 import edu.tamu.framework.aspect.annotation.ApiMapping;
-import edu.tamu.framework.aspect.annotation.ApiValidatedModel;
-import edu.tamu.framework.aspect.annotation.ApiVariable;
 import edu.tamu.framework.aspect.annotation.Auth;
-import edu.tamu.framework.aspect.annotation.InputStream;
 import edu.tamu.framework.model.ApiResponse;
-import edu.tamu.framework.validation.ModelBindingResult;
 
 /**
  * Controller in which to manage controlled vocabulary.
@@ -51,12 +34,6 @@ public class InputTypeController {
     @Autowired
     private ValidationService validationService;
 
-	// private Map<String, List<Organization>> getAll() {
-	// 	Map<String, List<Organization>> map = new HashMap<String, List<Organization>>();
-	// 	map.put("list", organizationRepo.findAll());
-	// 	return map;
-	// }
-
 	/**
      * Endpoint to request all input types.
      * 
@@ -64,15 +41,8 @@ public class InputTypeController {
      */
     @ApiMapping("/all")
     @Auth(role = "MANAGER")
-    // @Transactional
     public ApiResponse getAllInputTypes() {
-        System.out.println("\n\n\nthe enum.values is VV");
-        System.out.println(InputType.values());
         List<InputType> inputTypes = new ArrayList<InputType>(Arrays.asList(InputType.values()));
-        for (InputType type : inputTypes) {
-            System.out.println(type.toString());
-        }
-        System.out.println("\n\n");
         return new ApiResponse(SUCCESS, inputTypes);
     }
 
