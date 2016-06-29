22ecc5f57184eaf6378ce73c77a68f9bce45c4d4
Simon Frazier <sfrazier@library.tamu.edu>
2016-06-29 15:19:26 -0500

    Table displays available data. Removed debugging code.

--- src/main/webapp/WEB-INF/app/controllers/submission/submissionViewController.js
+++ src/main/webapp/WEB-INF/app/controllers/submission/submissionViewController.js
@@ -7,11 +7,4 @@ vireo.controller("SubmissionViewController", function ($controller, $scope, NgTa
 		$scope.tableParams.reload();
 	});
 
-	$scope.print = function(){
-		// $scope.submissions = SubmissionRepo.getAll(true);
-		console.info($scope.submissions);
-		// $scope.tableParams = new NgTableParams({}, {filterDelay: 0, dataset: $scope.submissions}); 
-		// console.info('data: ', data);
-		$scope.tableParams.reload();
-	};
 });
