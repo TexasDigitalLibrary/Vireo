dc264f1f0aa0a3a4295a0a7b85a8c7292bdee9c2
Simon Frazier <sfrazier@library.tamu.edu>
2016-06-30 16:21:42 -0500

    Removed old console.infos.

--- src/main/webapp/WEB-INF/app/controllers/submission/submissionViewController.js
+++ src/main/webapp/WEB-INF/app/controllers/submission/submissionViewController.js
@@ -1,7 +1,6 @@
 vireo.controller("SubmissionViewController", function ($controller, $scope, NgTableParams, SubmissionRepo) {
 
 	SubmissionRepo.manualGetAllPromise().then(function(submissions){
-		console.info('controller getCachePromise then submissions is ', submissions);
 		$scope.submissions = submissions;
 		$scope.tableParams = new NgTableParams({}, {filterDelay: 0, dataset: $scope.submissions}); 
 		$scope.tableParams.reload();
