b12b1e3eaf0b23628be4f3dbca08edb68fb54eb1
Simon Frazier <sfrazier@library.tamu.edu>
2016-07-05 13:06:38 -0500

    ETD table reload WIP

--- src/main/webapp/WEB-INF/app/controllers/submission/submissionViewController.js
+++ src/main/webapp/WEB-INF/app/controllers/submission/submissionViewController.js
@@ -1,9 +1,25 @@
 vireo.controller("SubmissionViewController", function ($controller, $scope, NgTableParams, SubmissionRepo) {
 
-	SubmissionRepo.manualGetAllPromise().then(function(submissions){
-		$scope.submissions = submissions;
-		$scope.tableParams = new NgTableParams({}, {filterDelay: 0, dataset: $scope.submissions}); 
-		$scope.tableParams.reload();
-	});
+	// SubmissionRepo.manualGetAllPromise().then(function(submissions){
+	// 	$scope.submissions = submissions;
+	// 	$scope.tableParams = new NgTableParams({}, {filterDelay: 0, dataset: $scope.submissions}); 
+	// 	$scope.tableParams.reload();
+	// });
+
+  console.info(SubmissionRepo);
+
+  $scope.submissions = SubmissionRepo.getAll();
+
+  $scope.tableParams = new NgTableParams({}, {filterDelay: 0, dataset: $scope.submissions}); 
+
+  SubmissionRepo.ready().then(function(){
+	console.info('ready!!');
+	console.info($scope.submissions);
+  })
+
+  SubmissionRepo.listen(function(){
+	console.info($scope.submissions);
+	$scope.tableParams.reload();
+  });
 
 });
