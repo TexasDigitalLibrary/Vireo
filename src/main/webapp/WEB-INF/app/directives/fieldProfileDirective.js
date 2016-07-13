32769a778038ec94f145d492d856396667979745
jeremythuff <jeremythuff@gmail.com>
2016-07-13 16:42:50 -0500

    Reworked division of html and styles on student submissions

--- src/main/webapp/WEB-INF/app/directives/fieldProfileDirective.js
+++ src/main/webapp/WEB-INF/app/directives/fieldProfileDirective.js
@@ -8,8 +8,38 @@ vireo.directive("field",  function() {
 		},
 		link: function($scope) {
 
-			$scope.values = [""];
+			$scope.submission = $scope.$parent.submission;
 
+			$scope.values = $scope.submission.fieldValues;
+
+			$scope.addFieldValue = function() {
+				$scope.submission.addFieldValue($scope.profile.predicate);
+			};
+
+			$scope.removeFieldValue = function(value) {
+				var indexOfValue = $scope.submission.fieldValues.indexOf(value);
+				$scope.submission.fieldValues.splice(indexOfValue, 1);
+			};
+
+			$scope.filterValuesByPredicate = function(value) {
+				return $scope.profile.predicate.id === value.predicate.id;
+			};
+
+			$scope.showRemove = function(value) {
+				return $scope.profile.repeatable && !$scope.first(value);
+			};
+
+			$scope.showAdd = function(value) {
+				return $scope.profile.repeatable && $scope.first(value);
+			};
+
+			$scope.first = function(value) {
+				return $scope.submission.findFieldValuesByPredicate($scope.profile.predicate).indexOf(value) === 0
+			};
+
+			if(!$scope.submission.findFieldValuesByPredicate($scope.profile.predicate).length) {
+				$scope.addFieldValue();
+			}
 			$scope.includeTemplateUrl = "views/inputtype/"+$scope.profile.inputType.name.toLowerCase().replace("_", "-")+".html";
 		}
 	};
--- src/main/webapp/WEB-INF/app/model/submission.js
+++ src/main/webapp/WEB-INF/app/model/submission.js
@@ -2,9 +2,62 @@ var submissionModel = function () {
 
 	return function Submission() {
 		
+		var submission = this;
+
 		// additional model methods and variables
 
-		return this;
+
+		submission.findFieldValuesByPredicate = function(predicate) {
+
+			var foundFieldValues = [];
+
+			for(var i in submission.fieldValues) {
+
+				var fieldValue = submission.fieldValues[i];
+
+				if(fieldValue.predicate.value == predicate.value) {
+					foundFieldValues.push(fieldValue);
+				}
+
+			}
+
+			return foundFieldValues;
+
+		};
+
+		submission.findFieldValueById = function(id) {
+
+			var foundFieldValue = null;
+
+			for(var i in submission.fieldValues) {
+
+				var fieldValue = submission.fieldValues[i];
+
+				if(fieldValue.id == id) {
+					foundFieldValue = fieldValue;
+					break;
+				}
+
+			}
+
+			return foundFieldValue;
+
+		};
+
+		submission.addFieldValue = function(predicate) {
+			var fieldValue = {
+				id: null,
+				value: "",
+				predicate: predicate
+			};
+
+			submission.fieldValues.push(fieldValue);
+
+			return fieldValue;
+
+		};
+
+		return submission;
 	}
 
 }
