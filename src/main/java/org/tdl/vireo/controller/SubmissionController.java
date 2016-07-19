a6c0afbcd2ddb3e54f1cb4ceea9ffeff2f355d34
William Welling <wwelling@library.tamu.edu>
2016-07-19 09:49:28 -0500

    Styled drag and drop interface for submission view columns.

--- src/main/java/org/tdl/vireo/controller/SubmissionController.java
+++ src/main/java/org/tdl/vireo/controller/SubmissionController.java
@@ -2,6 +2,10 @@ package org.tdl.vireo.controller;
 
 import static edu.tamu.framework.enums.ApiResponseType.SUCCESS;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
@@ -51,9 +55,7 @@ public class SubmissionController {
     @Auth(role = "STUDENT")
     @Transactional
     public ApiResponse getAllByUser(@ApiCredentials Credentials credentials) {
-        
         User submitter = userRepo.findByEmail(credentials.getEmail());
-        
         return new ApiResponse(SUCCESS, submissionRepo.findAllBySubmitter(submitter));
     }
 
@@ -91,6 +93,69 @@ public class SubmissionController {
         }   
                
         return new ApiResponse(SUCCESS, fieldValue);
+    }
+    
+    @ApiMapping("/all-columns")
+    @Auth(role = "STUDENT")
+    public ApiResponse getSubmissionViewColumns() {
+        List<String> columns =  new ArrayList<String>(Arrays.asList(new String[] {
+            "ID",
+            "Student name",
+            "Status",
+            "Assigned to",
+            "Document title",
+            "Submission date",
+            "Approval date",
+            "Embargo type",
+            "Student email",
+            "Institutional ID",
+            "Document keywords",
+            "Document abstract",
+            "Document subjects",
+            "Published material",
+            "Document language",
+            "Graduation semester",
+            "Defense date",
+            "Primary document",
+            "License agreement date",
+            "Committee approval date",
+            "Committee embargo approval date",
+            "Committee members",
+            "Committee contact email",
+            "Degree",
+            "Degree level",
+            "Program",
+            "College",
+            "Department",
+            "Major",
+            "Document type",
+            "UMI release",
+            "Custom actions",
+            "Deposit ID",
+            "Notes",
+            "Last event",
+            "Event time",
+            "ORCID"
+        }));
+        System.out.println(columns);
+        return new ApiResponse(SUCCESS, columns);
+    }
+    
+    @ApiMapping("/columns-by-user")
+    @Auth(role = "STUDENT")
+    public ApiResponse getSubmissionViewColumnsByUser(@ApiCredentials Credentials credentials) {
+        List<String> columns =  new ArrayList<String>(Arrays.asList(new String[] {
+            "ID",
+            "Student name",
+            "Status",
+            "Assigned to",
+            "Document title",
+            "Submission date",
+            "Approval date",
+            "Embargo type"
+        }));
+        System.out.println(columns);
+        return new ApiResponse(SUCCESS, columns);
     }
 
 }
--- src/main/webapp/WEB-INF/app/controllers/submission/submissionViewController.js
+++ src/main/webapp/WEB-INF/app/controllers/submission/submissionViewController.js
@@ -1,17 +1,74 @@
-vireo.controller("SubmissionViewController", function ($controller, $scope, NgTableParams, SubmissionRepo) {
+vireo.controller("SubmissionViewController", function ($controller, $filter, $q, $scope, NgTableParams, SubmissionRepo, WsApi) {
 
 	angular.extend(this, $controller('AbstractController', {$scope: $scope}));
-	
+
   	$scope.submissions = SubmissionRepo.getAll();
+
+  	$scope.columns = [];
+
+  	$scope.userColumns = [];
+
+  	$scope.resultsPerPageOptions = [20, 40, 60, 100, 200, 400, 500, 1000];
+
+  	$scope.resultsPerPage = 100;
+
+  	var allColumnsPromise = WsApi.fetch({
+		'endpoint': '/private/queue',
+		'controller': 'submission',
+		'method': 'all-columns'
+	});
+
+	var userColumnsPromise = WsApi.fetch({
+		'endpoint': '/private/queue',
+		'controller': 'submission',
+		'method': 'columns-by-user'
+	});
+
+	$q.all([allColumnsPromise, userColumnsPromise]).then(function(data) {
+		angular.extend($scope.userColumns, angular.fromJson(data[1].body).payload["ArrayList<String>"]);
+		angular.extend($scope.columns, $filter('exclude')(angular.fromJson(data[0].body).payload["ArrayList<String>"], $scope.userColumns));
+	});
+
+	$scope.columnOptions = {
+		accept: function (sourceItemHandleScope, destSortableScope, destItemScope) {
+			return true;
+		},
+		itemMoved: function (event) {
+
+		},
+		orderChanged: function (event) {
+
+		},
+		containment: '#column-modal'
+	};
 
   	SubmissionRepo.ready().then(function() {
   		$scope.tableParams = new NgTableParams({}, {filterDelay: 0, dataset: $scope.submissions}); 
   		$scope.tableParams.reload();
-  		console.log($scope.submissions);
   	})
 
   	SubmissionRepo.listen(function() {
 		$scope.tableParams.reload();
   	});
 
+});
+
+vireo.filter('exclude', function() {
+    return function(input, exclude, prop) {
+        if (!angular.isArray(input))
+            return input;
+
+        if (!angular.isArray(exclude))
+            exclude = [];
+
+        if (prop) {
+            exclude = exclude.map(function byProp(item) {
+                return item[prop];
+            });
+        }
+
+        return input.filter(function byExclude(item) {
+            return exclude.indexOf(prop ? item[prop] : item) === -1;
+        });
+    };
 });
