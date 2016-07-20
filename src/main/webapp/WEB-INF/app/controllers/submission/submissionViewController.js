401777b4ab1fce457cdb39655c40decd5ea335aa
William Welling <wwelling@library.tamu.edu>
2016-07-20 12:25:31 -0500

    Fixed reset to defaults of submission list columns.

--- src/main/webapp/WEB-INF/app/controllers/submission/submissionViewController.js
+++ src/main/webapp/WEB-INF/app/controllers/submission/submissionViewController.js
@@ -1,79 +1,81 @@
-vireo.controller("SubmissionViewController", function ($controller, $filter, $q, $scope, NgTableParams, SubmissionRepo, SubmissionViewColumnRepo, ManagerSubmissionViewColumnRepo, WsApi) {
+vireo.controller("SubmissionViewController", function ($controller, $filter, $q, $scope, $timeout, NgTableParams, SubmissionRepo, SubmissionViewColumnRepo, ManagerSubmissionViewColumnRepo, WsApi) {
 
 	angular.extend(this, $controller('AbstractController', {$scope: $scope}));
 	
-  	$scope.page = 0;
-  	$scope.pageSize = 10;
-  	
-  	$scope.columns = [];
+	$scope.page = 0;
+	$scope.pageSize = 10;
 
-  	$scope.userColumns = [];
+	$scope.columns = [];
 
-  	$scope.resultsPerPageOptions = [10, 20, 40, 60, 100, 200, 400, 500, 1000];
+	$scope.userColumns = [];
 
-  	$scope.resultsPerPage = 100;
+	$scope.resultsPerPageOptions = [10, 20, 40, 60, 100, 200, 400, 500, 1000];
 
-  	$scope.itemMoved = false;
+	$scope.resultsPerPage = 100;
 
-  	var updateColumns = function() {
-  		
-  		$scope.userColumns = ManagerSubmissionViewColumnRepo.getAll();
+	$scope.itemMoved = false;
+
+	var updateColumns = function() {
+		$scope.userColumns = ManagerSubmissionViewColumnRepo.getAll();
 		
 		$scope.columns = $filter('exclude')(SubmissionViewColumnRepo.getAll(), $scope.userColumns, 'title');
-  		
-  		SubmissionRepo.query($scope.userColumns, $scope.page, $scope.pageSize).then(function(data) {
-  			
+
+		SubmissionRepo.query($scope.userColumns, $scope.page, $scope.pageSize).then(function(data) {
+
 			$scope.submissions = angular.fromJson(data.body).payload.PageImpl.content
 
 			$scope.tableParams = new NgTableParams({ }, 
-	  		{
-	  			filterDelay: 0, 
-	  			dataset: $scope.submissions
-	  		});
+			{
+				filterDelay: 0, 
+				dataset: $scope.submissions
+			});
 
-	  		$scope.tableParams.reload();
+			$scope.tableParams.reload();
 		});
-  	};
+	};
 
-  	SubmissionRepo.listen(function() {
-		updateColumns();
-  	});
-
-  	$q.all([SubmissionViewColumnRepo.ready(), ManagerSubmissionViewColumnRepo.ready()]).then(function(data) {
+	SubmissionRepo.listen(function() {
 		updateColumns();
 	});
 
-  	var listenForManagersSubmissionColumns = function() {
-  		return $q(function(resolve) {
-  			ManagerSubmissionViewColumnRepo.listen(function() {
-  				resolve();
-  			});
-  		});
-  	};
+	$q.all([SubmissionViewColumnRepo.ready(), ManagerSubmissionViewColumnRepo.ready()]).then(function(data) {
+		updateColumns();
+	});
 
-  	var listenForAllSubmissionColumns = function() {
-  		return $q(function(resolve) {
-  			SubmissionViewColumnRepo.listen(function() {
-  				resolve();
-  			});
-  		});
-  	};
+	var listenForManagersSubmissionColumns = function() {
+		return $q(function(resolve) {
+			ManagerSubmissionViewColumnRepo.listen(function() {
+				resolve();
+			});
+		});
+	};
+
+	var listenForAllSubmissionColumns = function() {
+		return $q(function(resolve) {
+			SubmissionViewColumnRepo.listen(function() {
+				resolve();
+			});
+		});
+	};
 
 	$scope.resetColumns = function() {
+		$timeout(function() {
+			$q.all(listenForAllSubmissionColumns(), listenForManagersSubmissionColumns()).then(function() {
+				updateColumns();
+			});
 
-		$q.all(listenForAllSubmissionColumns(), listenForManagersSubmissionColumns()).then(function() {
-	  		updateColumns();
-	  	});
+			ManagerSubmissionViewColumnRepo.reset();
+			SubmissionViewColumnRepo.reset();
 
-		SubmissionViewColumnRepo.reset();
-		ManagerSubmissionViewColumnRepo.reset();
+			$scope.closeModal();
 
-		$scope.closeModal();
-
-		$scope.itemMoved = false;
+			$scope.itemMoved = false;
+		});
 	};
 
 	$scope.resetColumnsToDefault = function() {
+		ManagerSubmissionViewColumnRepo.reset();
+		SubmissionViewColumnRepo.reset();
 		ManagerSubmissionViewColumnRepo.resetSubmissionViewColumns().then(function() {
 			$scope.resetColumns();
 		});
@@ -116,20 +118,20 @@ vireo.controller("SubmissionViewController", function ($controller, $filter, $q,
 });
 
 vireo.filter('exclude', function() {
-    return function(input, exclude, prop) {
-        if (!angular.isArray(input)) {
-            return input;
+	return function(input, exclude, prop) {
+		if (!angular.isArray(input)) {
+			return input;
 		}
-        if (!angular.isArray(exclude)) {
-        	exclude = [];
-        }
-        if (prop) {
-            exclude = exclude.map(function byProp(item) {
-                return item[prop];
-            });
-        }
-        return input.filter(function byExclude(item) {
-            return exclude.indexOf(prop ? item[prop] : item) === -1;
-        });
-    };
+		if (!angular.isArray(exclude)) {
+			exclude = [];
+		}
+		if (prop) {
+			exclude = exclude.map(function byProp(item) {
+				return item[prop];
+			});
+		}
+		return input.filter(function byExclude(item) {
+			return exclude.indexOf(prop ? item[prop] : item) === -1;
+		});
+	};
 });
