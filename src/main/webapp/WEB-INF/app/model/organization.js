2be96bae91602a6667fa714ff007fd5c91f11cb9
William Welling <wwelling@library.tamu.edu>
2016-08-02 14:07:20 -0500

    Fixed organization model.

--- src/main/webapp/WEB-INF/app/model/organization.js
+++ src/main/webapp/WEB-INF/app/model/organization.js
@@ -23,22 +23,23 @@ vireo.model("Organization", function ($q, RestApi) {
 					});
 				}
 			});
-
-			//Override
-			this.delete = function() {
-				var organization = this;
-				angular.extend(apiMapping.Organization.remove, {'data': organization}); //We use 'remove' in the mapping because delete is a js reserved word.
-				var promise = RestApi.post(apiMapping.Organization.remove);
-				promise.then(function(res) {
-					if(res.meta.type == "INVALID") {
-						angular.extend(organization, res.payload);
-						console.log(organization);
-					}
-				});
-				return promise;
-			};
-
-			return this;
 		};
+
+		//Override
+		this.delete = function() {
+			var organization = this;
+			angular.extend(apiMapping.Organization.remove, {'data': organization}); //We use 'remove' in the mapping because delete is a js reserved word.
+			var promise = RestApi.post(apiMapping.Organization.remove);
+			promise.then(function(res) {
+				if(res.meta.type == "INVALID") {
+					angular.extend(organization, res.payload);
+					console.log(organization);
+				}
+			});
+			return promise;
+		};
+
+		return this;
+
 	};
 });
