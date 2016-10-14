1222605a105d16e550576b82d6c7dde4c7b2f096
William Welling <wwelling@library.tamu.edu>
2016-10-14 09:19:02 -0500

    Replaced uib-collapse and ng-animate with manual css transition

--- src/main/webapp/WEB-INF/app/directives/fieldProfileDirective.js
+++ src/main/webapp/WEB-INF/app/directives/fieldProfileDirective.js
@@ -93,7 +93,6 @@ vireo.directive("field",  function() {
 				return preview;
 			};
 
-			console.log($scope.profile.inputType.name)
 			$scope.includeTemplateUrl = "views/inputtype/"+$scope.profile.inputType.name.toLowerCase().replace("_", "-")+".html";
 		}
 	};
