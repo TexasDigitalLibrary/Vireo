415342089418efae871d8a870cbbdaf3dbda4bf5
William Welling <wwelling@library.tamu.edu>
2016-10-14 13:42:34 -0500

    Fixed upload loago bug.

--- src/main/java/org/tdl/vireo/util/FileIOUtility.java
+++ src/main/java/org/tdl/vireo/util/FileIOUtility.java
@@ -18,27 +18,36 @@ import org.tdl.vireo.Application;
 @Service
 public class FileIOUtility {
     
-    public void write(byte[] bytes, String filePath) throws IOException {
-        Path path = Paths.get(Application.BASE_PATH + filePath);
-        Files.write(path, bytes);
+    public void write(byte[] bytes, String relativePath) throws IOException {
+        Files.write(Paths.get(getPath(relativePath)), bytes);
     }
 
-    public void write(InputStream is, String filePath) throws IOException {
+    public void write(InputStream is, String path) throws IOException {
         byte[] bytes = IOUtils.toByteArray(is);
-        write(bytes, filePath);
+        write(bytes, path);
     }
 
-    public void writeImage(InputStream stream, String filePath) throws IOException {
-
+    public void writeImage(InputStream stream, String relativePath) throws IOException {
         String inputData = IOUtils.toString(stream, "UTF-8");
         String[] imageData = inputData.split(";");
         String[] encodedData = imageData[1].split(",");
         String[] mimeData = imageData[0].split(":");
         String fileExtension = mimeData[1].split("/")[1];
-        
         BufferedImage image = ImageIO.read(new ByteArrayInputStream(Base64.getDecoder().decode(encodedData[1])));
-        ImageIO.write(image, fileExtension, Files.newOutputStream(Paths.get(Application.BASE_PATH + filePath)));
-
+        Path path = Paths.get(getPath(relativePath));
+        Path parentDir = path.getParent();
+        if (!Files.exists(parentDir)) {
+            Files.createDirectories(parentDir);
+        }
+        ImageIO.write(image, fileExtension, Files.newOutputStream(path));
+    }
+    
+    private String getPath(String relativePath) {
+    	String path = Application.BASE_PATH + relativePath;
+        if(path.contains(":") && path.charAt(0) == '/') {
+        	path = path.substring(1, path.length());
+        }    
+        return path;
     }
 
 }
