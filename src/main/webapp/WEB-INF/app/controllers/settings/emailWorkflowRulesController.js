e9ea6fd2b97f9ec1ae3aee2849a120728345a711
Jeremy Huff <jeremythuff@gmail.com>
2016-11-18 15:10:38 -0600

    Added new input type contact

--- src/main/webapp/WEB-INF/app/controllers/settings/emailWorkflowRulesController.js
+++ src/main/webapp/WEB-INF/app/controllers/settings/emailWorkflowRulesController.js
@@ -9,11 +9,36 @@ vireo.controller("EmailWorkflowRulesController", function($controller, $scope, $
 		$scope.newTemplate = $scope.emailTemplates[0];
 	});
 
-	$scope.addRule = function() {
+	$scope.openAddEmailWorkflowRuleModal = function(id) {
+		$scope.recipientTypes = [		
+			"Submitter",
+			"Assignee",
+			"Organization"
+		];
+
+		console.log($scope.getSelectedOrganization());
+
+		angular.forEach($scope.getSelectedOrganization().aggregateWorkflowSteps, function(aggregateWorkflowStep) {
+			angular.forEach(aggregateWorkflowStep.aggregateFieldProfiles, function(aggregateFieldProfile) {
+				if(aggregateFieldProfile.inputType.name === "INPUT_CONTACT") {
+					console.log(aggregateFieldProfile);
+					$scope.recipientTypes.push(aggregateFieldProfile.fieldGlosses[0].value);
+				}
+			});
+		});
+
+		$scope.newRecipientType = $scope.recipientTypes[0];
+
+		$scope.openModal(id);
+
+	}
+
+	$scope.addEmailWorkflowRule = function() {
 		console.log("Add rule");
 	};
 
 	$scope.resetEmailWorkflowRule = function() {
+		$scope.newRecipient = $scope.recipients[0];
 		$scope.closeModal();
 	};
 
@@ -21,8 +46,8 @@ vireo.controller("EmailWorkflowRulesController", function($controller, $scope, $
 
 
 	$q.all([SubmissionStateRepo.ready(), EmailTemplateRepo.ready()]).then(function() {
-
-
+		
+		$scope.resetEmailWorkflowRule();
 
 	});
 
