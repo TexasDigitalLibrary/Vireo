8bfc758a432c8250b85fb8ee4d364abf900ff53d
jeremythuff <jeremythuff@gmail.com>
2016-11-28 09:30:33 -0600

    Now displaying an advance button on student submission

--- src/main/webapp/WEB-INF/app/controllers/submission/studentSubmissionController.js
+++ src/main/webapp/WEB-INF/app/controllers/submission/studentSubmissionController.js
@@ -1,14 +1,29 @@
 vireo.controller("StudentSubmissionController", function ($controller, $scope, $routeParams, StudentSubmissionRepo, Submission) {
 
 	angular.extend(this, $controller('AbstractController', {$scope: $scope}));
+
+	$scope.onLastStep = function() {
+		return true;
+	};
 	
 	StudentSubmissionRepo.findSubmissionById($routeParams.submissionId).then(function(data) {
 		$scope.submission = new Submission(angular.fromJson(data.body).payload.Submission);
 		$scope.setAcitveStep($scope.submission.submissionWorkflowSteps[0]);
+
+
+		$scope.onLastStep = function() {
+			var currentStepIndex = $scope.submission.submissionWorkflowSteps.indexOf($scope.nextStep);
+			return currentStepIndex === -1;
+		};
+
 	});
 
 	$scope.setAcitveStep = function(step) {
-		$scope.activeStep = step;
+		if(step) {
+			var stepIndex = $scope.submission.submissionWorkflowSteps.indexOf(step);
+			$scope.nextStep = $scope.submission.submissionWorkflowSteps[stepIndex+1];
+			$scope.activeStep = step;
+		}
 	};
 
 });
