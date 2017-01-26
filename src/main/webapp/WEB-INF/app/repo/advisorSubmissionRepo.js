7a2d6a3be311b5d4a68fa51d46496cbb8cae255b
jeremythuff <jeremythuff@gmail.com>
2017-01-26 12:41:56 -0600

    Refactored required field review template as a directive

--- src/main/webapp/WEB-INF/app/repo/advisorSubmissionRepo.js
+++ src/main/webapp/WEB-INF/app/repo/advisorSubmissionRepo.js
@@ -4,20 +4,11 @@ vireo.repo("AdvisorSubmissionRepo", function AdvisorSubmissionRepo(WsApi) {
 
 	AdvisorSubmissionRepo.findSubmissionByhash = function(hash) {
 
-		console.log(hash);
-
 		angular.extend(AdvisorSubmissionRepo.mapping.getByHash, {
 			'method': 'advisor-review/' + hash
 		});
 
-		console.log(AdvisorSubmissionRepo.mapping.getByHash);
-		var promise = WsApi.fetch(AdvisorSubmissionRepo.mapping.getByHash);
-
-		promise.then(function(res) {
-			console.log(res);
-		});
-
-		return promise;
+		return WsApi.fetch(AdvisorSubmissionRepo.mapping.getByHash);
 	};
 
 	return AdvisorSubmissionRepo;
