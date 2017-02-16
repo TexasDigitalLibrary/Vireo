0bce9bb83888c99b5b0aa02967088c2e7717f7ed
William Welling <wwelling@library.tamu.edu>
2017-02-16 15:35:17 -0600

    Fixed login unauthorized warning bug.

--- src/main/webapp/WEB-INF/app/controllers/abstractController.js
+++ src/main/webapp/WEB-INF/app/controllers/abstractController.js
@@ -1,54 +1,54 @@
-vireo.controller('AbstractController', function ($scope, $window, ModalService, StorageService, RestApi) {
+vireo.controller('AbstractController', function($scope, $window, ModalService, StorageService, RestApi) {
 
-	angular.extend($scope, ModalService);
-	
-	$scope.modals = ModalService;
+  angular.extend($scope, ModalService);
 
-	$scope.isAnonymous = function() {
-		return (sessionStorage.role == appConfig.anonymousRole);
-	};
+  $scope.modals = ModalService;
 
-	$scope.isUser = function() {
-		return (sessionStorage.role == "STUDENT");
-	};
-	
-	$scope.isStudent = function() {
-		return (sessionStorage.role == "STUDENT");
-	};
-	
-	$scope.isReviewer = function() {
-		return (sessionStorage.role == "REVIEWER");
-	};
-	
-	$scope.isManager = function() {
-		return (sessionStorage.role == "MANAGER");
-	};
+  $scope.isAnonymous = function() {
+    return sessionStorage.token === undefined || sessionStorage.role === appConfig.anonymousRole;
+  };
 
-	$scope.isAdmin = function() {
-		return (sessionStorage.role == "ADMINISTRATOR");
-	};
+  $scope.isUser = function() {
+    return sessionStorage.role === "STUDENT";
+  };
 
-	$scope.reportError = function(alert) {
-		RestApi.post({
-			controller: 'report', 
-			method: 'error',
-			data: alert
-		}).then(function() {
-			angular.element("#reportModal").modal('show');
-		}, function(response) {
-			if(response.data == null || response.data.message != "EXPIRED_JWT") {
-				var subject = 'Error Report';
-				var body = 'Error Report\n\nchannel: ' + alert.channel +
-						   '\ntime: ' + new Date(alert.time) +
-						   '\ntype: ' + alert.type + 
-						   '\nmessage: ' + alert.message;
-	    		$window.location.href = "mailto:"+ coreConfig.alerts.email + 
-	    							    "?subject=" + escape(subject) + 
-	    								"&body=" + escape(body); 
-			}
-		}, function() {
-			
-		});
-	};
+  $scope.isStudent = function() {
+    return sessionStorage.role === "STUDENT";
+  };
+
+  $scope.isReviewer = function() {
+    return sessionStorage.role === "REVIEWER";
+  };
+
+  $scope.isManager = function() {
+    return sessionStorage.role === "MANAGER";
+  };
+
+  $scope.isAdmin = function() {
+    return sessionStorage.role === "ADMINISTRATOR";
+  };
+
+  $scope.reportError = function(alert) {
+    RestApi.post({
+      controller: 'report',
+      method: 'error',
+      data: alert
+    }).then(function() {
+      angular.element("#reportModal").modal('show');
+    }, function(response) {
+      if (response.data == null || response.data.message != "EXPIRED_JWT") {
+        var subject = 'Error Report';
+        var body = 'Error Report\n\nchannel: ' + alert.channel +
+          '\ntime: ' + new Date(alert.time) +
+          '\ntype: ' + alert.type +
+          '\nmessage: ' + alert.message;
+        $window.location.href = "mailto:" + coreConfig.alerts.email +
+          "?subject=" + escape(subject) +
+          "&body=" + escape(body);
+      }
+    }, function() {
+
+    });
+  };
 
 });
--- src/main/webapp/WEB-INF/app/directives/userSettingsDirective.js
+++ src/main/webapp/WEB-INF/app/directives/userSettingsDirective.js
@@ -1,35 +1,44 @@
-vireo.directive('displayname', function(UserSettings) {
+vireo.directive('displayname', function($controller, UserService, UserSettings) {
   return {
     template: '<span>{{userSettings.displayName}}</span>',
     restrict: 'E',
     scope: true,
     link: function($scope, elem) {
-      $scope.userSettings = new UserSettings();
-      $scope.userSettings.fetch();
+      angular.extend(this, $controller("AbstractController", {
+        $scope: $scope
+      }));
+      if (!$scope.isAnonymous()) {
+        $scope.userSettings = new UserSettings();
+        $scope.userSettings.fetch();
+      }
     }
   };
 });
 
-vireo.directive('usersettings', function(UserSettings) {
+vireo.directive('usersettings', function($controller, UserSettings) {
   return {
     template: '<span>{{displayValue}}</span>',
     restrict: 'E',
     scope: true,
     link: function($scope, elem, attr) {
+      angular.extend(this, $controller("AbstractController", {
+        $scope: $scope
+      }));
 
-      $scope.userSettings = new UserSettings();
-      $scope.userSettings.fetch();
+      if (!$scope.isAnonymous()) {
+        $scope.userSettings = new UserSettings();
+        $scope.userSettings.fetch();
 
-      $scope.displayValue = "";
+        $scope.displayValue = "";
 
-      $scope.userSettings.ready().then(function() {
-        for (var a in attr) {
-          if (a.indexOf("$") == -1 && (typeof $scope.userSettings[a] != 'undefined')) {
-            $scope.displayValue += $scope.userSettings[a] + " ";
+        $scope.userSettings.ready().then(function() {
+          for (var a in attr) {
+            if (a.indexOf("$") == -1 && (typeof $scope.userSettings[a] != 'undefined')) {
+              $scope.displayValue += $scope.userSettings[a] + " ";
+            }
           }
-        }
-      });
-
+        });
+      }
     }
   };
 });
