cc197297578d7ace32fdbbcb1cf95a47e701b934
William Welling <wwelling@library.tamu.edu>
2017-02-16 11:00:49 -0600

    Using latest version of Weaver UI Core.

--- src/main/webapp/WEB-INF/app/directives/userSettingsDirective.js
+++ src/main/webapp/WEB-INF/app/directives/userSettingsDirective.js
@@ -1,33 +1,35 @@
-vireo.directive('displayname', function (UserSettings) {
-	return {
-		template: '<span>{{userSettings.displayName}}</span>',
-		restrict: 'E',
-		scope:true,
-		link: function($scope, elem) {
-			$scope.userSettings = new UserSettings();
-		}
-	};
+vireo.directive('displayname', function(UserSettings) {
+  return {
+    template: '<span>{{userSettings.displayName}}</span>',
+    restrict: 'E',
+    scope: true,
+    link: function($scope, elem) {
+      $scope.userSettings = new UserSettings();
+      $scope.userSettings.fetch();
+    }
+  };
 });
 
-vireo.directive('usersettings', function (UserSettings) {
-	return {
-		template: '<span>{{displayValue}}</span>',
-		restrict: 'E',
-		scope:true,
-		link: function($scope, elem, attr) {
+vireo.directive('usersettings', function(UserSettings) {
+  return {
+    template: '<span>{{displayValue}}</span>',
+    restrict: 'E',
+    scope: true,
+    link: function($scope, elem, attr) {
 
-			$scope.userSettings = new UserSettings();
+      $scope.userSettings = new UserSettings();
+      $scope.userSettings.fetch();
 
-			$scope.displayValue = "";
+      $scope.displayValue = "";
 
-			$scope.userSettings.ready().then(function() {
-				for(var a in attr) {
-					if(a.indexOf("$") == -1 && (typeof $scope.userSettings[a] != 'undefined')) {
-						$scope.displayValue += $scope.userSettings[a] + " ";
-					}
-				}
-			});
+      $scope.userSettings.ready().then(function() {
+        for (var a in attr) {
+          if (a.indexOf("$") == -1 && (typeof $scope.userSettings[a] != 'undefined')) {
+            $scope.displayValue += $scope.userSettings[a] + " ";
+          }
+        }
+      });
 
-		}
-	};
+    }
+  };
 });
