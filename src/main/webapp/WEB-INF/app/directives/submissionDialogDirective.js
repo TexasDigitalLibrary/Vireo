4a6d3eb7523104a357223b3dcb54131a4e7846d7
William Welling <wwelling@library.tamu.edu>
2017-02-21 14:48:03 -0600

    Added style to submission dialog directive.

--- src/main/webapp/WEB-INF/app/directives/submissionDialogDirective.js
+++ src/main/webapp/WEB-INF/app/directives/submissionDialogDirective.js
@@ -1,3 +1,14 @@
 vireo.directive("submissiondialog", function() {
-    return {templateUrl: "views/directives/submissionDialog.html", scope: {}}
+    return {
+        templateUrl: "views/directives/submissionDialog.html",
+        scope: {
+            submission: '='
+        },
+        link: function($scope, element, attr, parent) {
+            $scope.ac = ['-', '+'];
+            $scope.toggle = function() {
+                $scope.ac.reverse()
+            }
+        }
+    }
 });
