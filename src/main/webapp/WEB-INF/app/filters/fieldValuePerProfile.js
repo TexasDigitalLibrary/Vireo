7c37ff675721aae4675e06199e163ef3298b8a16
William Welling <wwelling@library.tamu.edu>
2017-02-21 08:13:52 -0600

    Created new filter for document field value per profile.

--- src/main/webapp/WEB-INF/app/filters/fieldValuePerProfile.js
+++ src/main/webapp/WEB-INF/app/filters/fieldValuePerProfile.js
@@ -9,15 +9,3 @@ vireo.filter('fieldValuePerProfile', function() {
         return fieldProfileValues;
     }
 });
-
-vireo.filter('documentFieldValuePerProfile', function() {
-    return function(fieldValues) {
-        var documentFieldValuePerProfileValues = [];
-        angular.forEach(fieldValues, function(fieldValue) {
-            if (fieldValue.fieldPredicate.documentTypePredicate) {
-                documentFieldValuePerProfileValues.push(fieldValue);
-            }
-        });
-        return documentFieldValuePerProfileValues;
-    }
-});
