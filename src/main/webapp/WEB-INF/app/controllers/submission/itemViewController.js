24d8c85e403d97eefe479b412f845b0e493763ed
jeremythuff <jeremythuff@gmail.com>
2017-02-22 08:40:05 -0600

    Added more/less to action log list

--- src/main/webapp/WEB-INF/app/controllers/submission/itemViewController.js
+++ src/main/webapp/WEB-INF/app/controllers/submission/itemViewController.js
@@ -8,6 +8,14 @@ vireo.controller("ItemViewController", function($anchorScroll, $controller, $loc
 
   $scope.allUsers = UserRepo.getAll();
 
+
+  $scope.actionLogLimit = 10;
+  $scope.actionLogCurrentLimit = $scope.actionLogLimit;
+
+  $scope.updateActionLogLimit = function() {
+    $scope.actionLogCurrentLimit=$scope.actionLogCurrentLimit===$scope.actionLogLimit?$scope.submission.actionLogs.length:$scope.actionLogLimit;
+  }
+
   var ready = $q.all([FieldPredicateRepo.ready(), SubmissionRepo.findSubmissionById($routeParams.id), UserRepo.ready(), SubmissionStateRepo.ready()]);
 
   ready.then(function() {
