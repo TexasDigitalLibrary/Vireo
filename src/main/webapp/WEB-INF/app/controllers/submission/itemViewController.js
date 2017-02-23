aad2ccd9c9f554c37e46f6e6b62d91135885f932
William Welling <wwelling@library.tamu.edu>
2017-02-23 16:06:21 -0600

    Added upload files section.

--- src/main/webapp/WEB-INF/app/controllers/submission/itemViewController.js
+++ src/main/webapp/WEB-INF/app/controllers/submission/itemViewController.js
@@ -7,6 +7,7 @@ vireo.controller("ItemViewController", function($anchorScroll, $controller, $loc
     $scope.allUsers = UserRepo.getAll();
 
     $scope.actionLogLimit = 10;
+
     $scope.actionLogCurrentLimit = $scope.actionLogLimit;
 
     $scope.updateActionLogLimit = function() {
