4e646277f590d6b076dfbebdd52cb664a08cdc4d
William Welling <wwelling@library.tamu.edu>
2017-04-28 09:21:47 -0500

    Some useful UI debugging display.

--- src/main/webapp/WEB-INF/app/controllers/organizationSettingsController.js
+++ src/main/webapp/WEB-INF/app/controllers/organizationSettingsController.js
@@ -1,46 +1,46 @@
-vireo.controller('OrganizationSettingsController', function($controller, $scope, $q, AccordionService, OrganizationRepo, SidebarService) {
+vireo.controller('OrganizationSettingsController', function ($controller, $scope, $q, AccordionService, OrganizationRepo, SidebarService) {
 
-	angular.extend(this, $controller('AbstractController', {
-		$scope: $scope
-	}));
+    angular.extend(this, $controller('AbstractController', {
+        $scope: $scope
+    }));
 
-	$scope.organizationRepo = OrganizationRepo;
+    $scope.organizationRepo = OrganizationRepo;
 
-	SidebarService.addBox({
-		"title": "Create Organization",
-		"viewUrl": "views/sideboxes/organization.html"
-	});
+    SidebarService.addBox({
+        "title": "Create Organization",
+        "viewUrl": "views/sideboxes/organization.html"
+    });
 
-	$scope.organizations = OrganizationRepo.getAll();
+    $scope.organizations = OrganizationRepo.getAll();
 
-	$scope.activeManagementPane = 'edit';
+    $scope.activeManagementPane = 'edit';
 
-	$scope.newOrganization = OrganizationRepo.getNewOrganization();
+    $scope.newOrganization = OrganizationRepo.getNewOrganization();
 
-	$scope.setSelectedOrganization = function(organization) {
-		if (OrganizationRepo.getSelectedOrganization().id !== organization.id) {
-			AccordionService.closeAll();
-		}
-		OrganizationRepo.setSelectedOrganization(organization, true);
-		$scope.newOrganization.parent = OrganizationRepo.getSelectedOrganization();
-	};
+    $scope.setSelectedOrganization = function (organization) {
+        if (OrganizationRepo.getSelectedOrganization().id !== organization.id) {
+            AccordionService.closeAll();
+        }
+        OrganizationRepo.setSelectedOrganization(organization, true);
+        $scope.newOrganization.parent = OrganizationRepo.getSelectedOrganization();
+    };
 
-	$scope.getSelectedOrganization = function() {
-		return OrganizationRepo.getSelectedOrganization();
-	};
+    $scope.getSelectedOrganization = function () {
+        return OrganizationRepo.getSelectedOrganization();
+    };
 
-	$scope.activateManagementPane = function(pane) {
-		$scope.activeManagementPane = pane;
-	};
+    $scope.activateManagementPane = function (pane) {
+        $scope.activeManagementPane = pane;
+    };
 
-	$scope.managementPaneIsActive = function(pane) {
-		return ($scope.activeManagementPane === pane);
-	};
+    $scope.managementPaneIsActive = function (pane) {
+        return ($scope.activeManagementPane === pane);
+    };
 
-	$scope.ready = $q.all([ OrganizationRepo.ready() ]);
+    $scope.ready = $q.all([OrganizationRepo.ready()]);
 
-	$scope.ready.then(function() {
-		$scope.newOrganization.parent = $scope.organizations[0];
-	});
+    $scope.ready.then(function () {
+        $scope.newOrganization.parent = $scope.organizations[0];
+    });
 
 });
