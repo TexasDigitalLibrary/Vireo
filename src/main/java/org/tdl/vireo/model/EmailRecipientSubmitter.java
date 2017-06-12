da1dd6577a117a25f6fa95c4afaed43332e21e42
jeremythuff <jeremythuff@gmail.com>
2017-06-12 16:37:35 -0500

    Changed formating in submitter email recipient

--- src/main/java/org/tdl/vireo/model/EmailRecipientSubmitter.java
+++ src/main/java/org/tdl/vireo/model/EmailRecipientSubmitter.java
@@ -5,23 +5,22 @@ import java.util.List;
 
 import javax.persistence.Entity;
 
-
 @Entity
 public class EmailRecipientSubmitter extends AbstractEmailRecipient implements EmailRecipient {
 
-    public EmailRecipientSubmitter() {
-        setName("Submitter");
-    }
+	public EmailRecipientSubmitter() {
+		setName("Submitter");
+	}
 
-    @Override
-    public List<String> getEmails(Submission submission) {
+	@Override
+	public List<String> getEmails(Submission submission) {
 
-        List<String> emails = new ArrayList<String>();
+		List<String> emails = new ArrayList<String>();
 
-        emails.add(submission.getSubmitter().getSetting("preferedEmail"));
+		emails.add(submission.getSubmitter().getSetting("preferedEmail"));
 
-        return emails;
+		return emails;
 
-    }
+	}
 
 }
