18bbdfddc249e4ce2ac09f9c76242359e0c1313f
jeremythuff <jeremythuff@gmail.com>
2017-06-12 16:45:05 -0500

    Further reduced the json annotation for email recipients

--- src/main/java/org/tdl/vireo/model/EmailRecipient.java
+++ src/main/java/org/tdl/vireo/model/EmailRecipient.java
@@ -8,13 +8,7 @@ import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 
 @JsonTypeInfo(
 use = JsonTypeInfo.Id.MINIMAL_CLASS,
-include = JsonTypeInfo.As.PROPERTY,
 property = "_class")
-@JsonSubTypes({
-    @Type(value=EmailRecipientAssignee.class), 
-    @Type(value=EmailRecipientContact.class),
-    @Type(value=EmailRecipientOrganization.class),
-    @Type(value=EmailRecipientSubmitter.class)})
 public interface EmailRecipient {
 
     public List<String> getEmails(Submission submission);
