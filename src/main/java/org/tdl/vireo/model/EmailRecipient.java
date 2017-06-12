4ba4a0a339990a1af39084fe1e64b0d297e0d829
jeremythuff <jeremythuff@gmail.com>
2017-06-12 16:51:16 -0500

    Further reduced annotation for email recipient

--- src/main/java/org/tdl/vireo/model/EmailRecipient.java
+++ src/main/java/org/tdl/vireo/model/EmailRecipient.java
@@ -3,10 +3,9 @@ package org.tdl.vireo.model;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 
-@JsonTypeInfo(
-use = JsonTypeInfo.Id.MINIMAL_CLASS,
-property = "_class")
+@JsonTypeInfo(use = Id.MINIMAL_CLASS)
 public interface EmailRecipient {
 
     public List<String> getEmails(Submission submission);
