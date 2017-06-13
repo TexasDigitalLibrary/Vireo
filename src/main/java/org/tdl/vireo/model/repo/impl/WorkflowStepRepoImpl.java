6c697b6c5bd907a053037b2c1394ba7876111a4b
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2017-06-13 15:03:08 -0500

    Added broadcast of organizations to the workflow step repo's update method

--- src/main/java/org/tdl/vireo/model/repo/impl/WorkflowStepRepoImpl.java
+++ src/main/java/org/tdl/vireo/model/repo/impl/WorkflowStepRepoImpl.java
@@ -1,11 +1,14 @@
 package org.tdl.vireo.model.repo.impl;
 
+import static edu.tamu.framework.enums.ApiResponseType.SUCCESS;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.tdl.vireo.exception.ComponentNotPresentOnOrgException;
 import org.tdl.vireo.exception.WorkflowStepNonOverrideableException;
 import org.tdl.vireo.model.FieldProfile;
@@ -18,6 +21,8 @@ import org.tdl.vireo.model.repo.NoteRepo;
 import org.tdl.vireo.model.repo.OrganizationRepo;
 import org.tdl.vireo.model.repo.WorkflowStepRepo;
 import org.tdl.vireo.model.repo.custom.WorkflowStepRepoCustom;
+
+import edu.tamu.framework.model.ApiResponse;
 
 public class WorkflowStepRepoImpl implements WorkflowStepRepoCustom {
 
@@ -32,6 +37,9 @@ public class WorkflowStepRepoImpl implements WorkflowStepRepoCustom {
 
     @Autowired
     private OrganizationRepo organizationRepo;
+    
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
 
     @Override
     public WorkflowStep create(String name, Organization originatingOrganization) {
@@ -286,7 +294,7 @@ public class WorkflowStepRepoImpl implements WorkflowStepRepoCustom {
             }
 
         }
-
+        simpMessagingTemplate.convertAndSend("/channel/organizations", new ApiResponse(SUCCESS, organizationRepo.findAll()));
         return resultingWorkflowStep;
     }
 
