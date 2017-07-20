c019fe96e90bf1c4786034db8b93f81fea97b865
William Welling <wwelling@library.tamu.edu>
2017-07-20 07:58:47 -0500

    fixed lint error

--- src/main/webapp/WEB-INF/app/controllers/settings/fieldProfileManagementController.js
+++ src/main/webapp/WEB-INF/app/controllers/settings/fieldProfileManagementController.js
@@ -1,6 +1,8 @@
-vireo.controller("FieldProfileManagementController", function($q, $controller, $scope, $filter, DragAndDropListenerFactory, DocumentTypeRepo, FieldProfileRepo, OrganizationRepo, ControlledVocabularyRepo, FieldGlossRepo, FieldPredicateRepo, InputTypeRepo, WorkflowStepRepo, ConfigurationRepo) {
+vireo.controller("FieldProfileManagementController", function ($q, $controller, $scope, $filter, DragAndDropListenerFactory, DocumentTypeRepo, FieldProfileRepo, OrganizationRepo, ControlledVocabularyRepo, FieldGlossRepo, FieldPredicateRepo, InputTypeRepo, WorkflowStepRepo, ConfigurationRepo) {
 
-    angular.extend(this, $controller("AbstractController", {$scope: $scope}));
+    angular.extend(this, $controller("AbstractController", {
+        $scope: $scope
+    }));
 
     $scope.shibbolethAttributes = ConfigurationRepo.getAllMapByType().shibboleth;
 
@@ -40,7 +42,7 @@ vireo.controller("FieldProfileManagementController", function($q, $controller, $
 
     $scope.ready = $q.all([ControlledVocabularyRepo.ready(), FieldPredicateRepo.ready(), FieldGlossRepo.ready(), InputTypeRepo.ready(), DocumentTypeRepo.ready()]);
 
-    $scope.ready.then(function() {
+    $scope.ready.then(function () {
 
         $scope.$watch("step", function handleStepChanged(newStep, oldStep) {
             $scope.resetFieldProfiles();
@@ -50,14 +52,14 @@ vireo.controller("FieldProfileManagementController", function($q, $controller, $
             $scope.dragControlListeners.getListener().confirm.remove.modal = '#fieldProfilesConfirmRemoveModal-' + $scope.step.id;
         });
 
-        FieldPredicateRepo.ready().then(function() {
+        FieldPredicateRepo.ready().then(function () {
             $scope.buildFilteredPredicateList();
         });
-        FieldPredicateRepo.listen(function() {
+        FieldPredicateRepo.listen(function () {
             $scope.buildFilteredPredicateList();
         });
 
-        $scope.inputTypeChanged = function() {
+        $scope.inputTypeChanged = function () {
             if ($scope.modalData.inputType.name == "INPUT_FILE") {
                 $scope.inputFile = true;
                 $scope.modalData.fieldPredicate = $scope.documentData.documentType.fieldPredicate;
@@ -66,11 +68,11 @@ vireo.controller("FieldProfileManagementController", function($q, $controller, $
             }
         };
 
-        $scope.documentTypeChanged = function() {
+        $scope.documentTypeChanged = function () {
             $scope.modalData.fieldPredicate = $scope.documentData.documentType.fieldPredicate;
         };
 
-        $scope.resetFieldProfiles = function() {
+        $scope.resetFieldProfiles = function () {
             $scope.workflowStepRepo.clearValidationResults();
             $scope.fieldProfileRepo.clearValidationResults();
             $scope.fieldPredicateRepo.clearValidationResults();
@@ -83,7 +85,7 @@ vireo.controller("FieldProfileManagementController", function($q, $controller, $
 
             var position = 1;
 
-            angular.forEach($scope.step.aggregateFieldProfiles, function(fieldProfile) {
+            angular.forEach($scope.step.aggregateFieldProfiles, function (fieldProfile) {
                 fieldProfile.position = position;
                 position++;
             });
@@ -115,19 +117,19 @@ vireo.controller("FieldProfileManagementController", function($q, $controller, $
 
         $scope.resetFieldProfiles();
 
-        $scope.updateFieldGloss = function(glossValue) {
-            $scope.createFieldGloss(glossValue,true);
+        $scope.updateFieldGloss = function (glossValue) {
+            $scope.createFieldGloss(glossValue, true);
         };
 
-        $scope.createFieldGloss = function(glossValue, preservePredicate) {
-            var preservePredicate = (typeof preservePredicate !== 'undefined') ?  preservePredicate : false;
+        $scope.createFieldGloss = function (glossValue, preservePredicate) {
+            preservePredicate = preservePredicate !== undefined && preservePredicate != null ? preservePredicate : false;
             // TODO set the language dynamically.
             // For now, the language must be 'English' so that's in name will match that existing on the server.
             $scope.modalData.fieldGlosses[0] = {
                 'value': glossValue,
                 'language': 'English'
             };
-            FieldGlossRepo.create($scope.modalData.fieldGlosses[0]).then(function(response) {
+            FieldGlossRepo.create($scope.modalData.fieldGlosses[0]).then(function (response) {
                 var body = angular.fromJson(response.body);
                 if (body.meta.type == 'SUCCESS') {
                     angular.extend($scope.modalData.fieldGlosses[0], body.payload.FieldGloss);
@@ -139,8 +141,11 @@ vireo.controller("FieldProfileManagementController", function($q, $controller, $
             });
         };
 
-        $scope.createFieldPredicate = function() {
-            FieldPredicateRepo.create({value: $scope.modalData.fieldPredicate, documentTypePredicate: false}).then(function(response) {
+        $scope.createFieldPredicate = function () {
+            FieldPredicateRepo.create({
+                value: $scope.modalData.fieldPredicate,
+                documentTypePredicate: false
+            }).then(function (response) {
                 var body = angular.fromJson(response.body);
                 if (body.meta.type == "SUCCESS") {
                     $scope.modalData.fieldPredicate = body.payload.FieldPredicate;
@@ -148,16 +153,16 @@ vireo.controller("FieldProfileManagementController", function($q, $controller, $
             });
         };
 
-        $scope.createFieldProfile = function() {
+        $scope.createFieldProfile = function () {
             WorkflowStepRepo.addFieldProfile($scope.step, $scope.modalData);
         };
 
-        $scope.selectFieldProfile = function(index) {
+        $scope.selectFieldProfile = function (index) {
             var fieldProfile = $scope.step.aggregateFieldProfiles[index];
             angular.extend($scope.modalData, fieldProfile);
 
             if ($scope.modalData.fieldPredicate.documentTypePredicate) {
-                angular.forEach($scope.documentTypes, function(documentType) {
+                angular.forEach($scope.documentTypes, function (documentType) {
                     if (documentType.fieldPredicate.id == $scope.modalData.fieldPredicate.id) {
                         angular.extend($scope.documentData.documentType, documentType);
                         $scope.inputTypeChanged();
@@ -167,24 +172,24 @@ vireo.controller("FieldProfileManagementController", function($q, $controller, $
 
         };
 
-        $scope.editFieldProfile = function(index) {
+        $scope.editFieldProfile = function (index) {
             $scope.selectFieldProfile(index - 1);
             $scope.openModal('#fieldProfilesEditModal-' + $scope.step.id);
         };
 
-        $scope.updateFieldProfile = function() {
+        $scope.updateFieldProfile = function () {
             WorkflowStepRepo.updateFieldProfile($scope.step, $scope.modalData);
         };
 
-        $scope.removeFieldProfile = function() {
+        $scope.removeFieldProfile = function () {
             WorkflowStepRepo.removeFieldProfile($scope.step, $scope.modalData);
         };
 
-        $scope.reorderFieldProfiles = function(src, dest) {
+        $scope.reorderFieldProfiles = function (src, dest) {
             WorkflowStepRepo.reorderFieldProfile($scope.step, src, dest);
         };
 
-        $scope.isEditable = function(fieldProfile) {
+        $scope.isEditable = function (fieldProfile) {
             var editable = fieldProfile.overrideable;
             if (!editable) {
                 editable = fieldProfile.originatingWorkflowStep == $scope.step.id && $scope.organizationRepo.getSelectedOrganization().originalWorkflowSteps.indexOf(fieldProfile.originatingWorkflowStep) > -1;
@@ -192,7 +197,7 @@ vireo.controller("FieldProfileManagementController", function($q, $controller, $
             return editable;
         };
 
-        $scope.openNewModal = function(id) {
+        $scope.openNewModal = function (id) {
             $scope.openModal('#fieldProfilesNewModal-' + id);
         };
 
@@ -206,8 +211,8 @@ vireo.controller("FieldProfileManagementController", function($q, $controller, $
             container: '#fieldProfiles'
         });
 
-        $scope.buildFilteredPredicateList = function() {
-            $scope.filteredPredicates = $filter('filter')($scope.fieldPredicates, function(predicate) {
+        $scope.buildFilteredPredicateList = function () {
+            $scope.filteredPredicates = $filter('filter')($scope.fieldPredicates, function (predicate) {
                 return !predicate.documentTypePredicate;
             });
         };
