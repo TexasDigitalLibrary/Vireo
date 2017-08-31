28c7b151a1fd14ef37457861c4168a8638bac083
jeremythuff <jeremythuff@gmail.com>
2017-08-31 11:18:58 -0500

    Added route change listener to remove modal bg

--- src/main/webapp/WEB-INF/app/config/runTime.js
+++ src/main/webapp/WEB-INF/app/config/runTime.js
@@ -1,8 +1,14 @@
-vireo.run(function($route, $rootScope, $location) {
+vireo.run(function($route, $rootScope, $location, ModalService, $timeout) {
 
 	angular.element("body").fadeIn(1000);
 
-	// Add runtime tasks here
+  // Add runtime tasks here
+  
+  $rootScope.$on("$routeChangeStart", function (evt, next, current) {
+      ModalService.closeModal();
+      //this should be incorporated with the clodeModal method in weaver-ui-core
+      angular.element('.modal-backdrop').remove();
+  });
 
 	// Allow the passing of an additional parameter which 
 	// Will allow the path to be changed with out route reload
