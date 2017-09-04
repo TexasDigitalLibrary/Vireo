cceff43f2707d5c5ec31ffba7a4a9b5c4aa70446
William Welling <wwelling@library.tamu.edu>
2017-09-04 09:28:09 -0500

    Update InputTypeRepo.java

--- src/main/java/org/tdl/vireo/model/repo/InputTypeRepo.java
+++ src/main/java/org/tdl/vireo/model/repo/InputTypeRepo.java
@@ -6,7 +6,6 @@ import org.tdl.vireo.model.repo.custom.InputTypeRepoCustom;
 
 public interface InputTypeRepo extends JpaRepository<InputType, Long>, InputTypeRepoCustom {
 
-	// Name is not a unique constraint of input type, this method might fail
     public InputType findByName(String name);
 
 }
