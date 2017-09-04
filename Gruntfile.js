d35e515146b6b03677b0f0f0cd761e6a66ef9953
William Welling <wwelling@library.tamu.edu>
2017-09-03 19:15:06 -0500

    not using any mins, having grunt minify for production

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,35 +54,35 @@ module.exports = function (grunt) {
             },
             vendor: {
                 src: [
-                    'node_modules/jquery/dist/jquery.min.js',
-                    'node_modules/bootstrap/dist/js/bootstrap.min.js',
+                    'node_modules/jquery/dist/jquery.js',
+                    'node_modules/bootstrap/dist/js/bootstrap.js',
 
-                    'node_modules/sockjs-client/dist/sockjs.min.js',
-                    'node_modules/stompjs/lib/stomp.min.js',
+                    'node_modules/sockjs-client/dist/sockjs.js',
+                    'node_modules/stompjs/lib/stomp.js',
 
-                    'node_modules/angular/angular.min.js',
+                    'node_modules/angular/angular.js',
 
-                    'node_modules/angular-sanitize/angular-sanitize.min.js',
-                    'node_modules/angular-route/angular-route.min.js',
-                    'node_modules/angular-loader/angular-loader.min.js',
-                    'node_modules/angular-messages/angular-messages.min.js',
-                    'node_modules/angular-mocks/angular-mocks.min.js',
+                    'node_modules/angular-sanitize/angular-sanitize.js',
+                    'node_modules/angular-route/angular-route.js',
+                    'node_modules/angular-loader/angular-loader.js',
+                    'node_modules/angular-messages/angular-messages.js',
+                    'node_modules/angular-mocks/angular-mocks.js',
 
-                    'node_modules/ng-csv/build/ng-csv.min.js',
+                    'node_modules/ng-csv/build/ng-csv.js',
 
-                    'node_modules/ng-sortable/dist/ng-sortable.min.js',
+                    'node_modules/ng-sortable/dist/ng-sortable.js',
 
-                    'node_modules/ng-table/dist/ng-table.min.js',
+                    'node_modules/ng-table/dist/ng-table.js',
 
-                    'node_modules/ng-file-upload/ng-file-upload-shim.min.js',
-                    'node_modules/ng-file-upload/ng-file-upload.min.js',
+                    'node_modules/ng-file-upload/ng-file-upload-shim.js',
+                    'node_modules/ng-file-upload/ng-file-upload.js',
 
-                    'node_modules/tinymce/tinymce.min.js',
-                    'node_modules/angular-ui-tinymce/dist/tinymce.min.js',
+                    'node_modules/tinymce/tinymce.js',
+                    'node_modules/angular-ui-tinymce/src/tinymce.js',
 
                     'node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
                     
-                    'node_modules/file-saver/FileSaver.min.js'
+                    'node_modules/file-saver/FileSaver.js'
                 ],
                 dest: '<%= build.app %>/resources/scripts/vendor_concat.js'
             },
