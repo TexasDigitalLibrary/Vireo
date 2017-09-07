bd2654beacb182c3170067c9a9e45c9d72389998
jeremythuff <jeremythuff@gmail.com>
2017-09-07 14:31:17 -0500

    Fixed variable name

--- src/main/webapp/WEB-INF/app/controllers/submission/adminSubmissionViewController.js
+++ src/main/webapp/WEB-INF/app/controllers/submission/adminSubmissionViewController.js
@@ -18,7 +18,7 @@ vireo.controller("AdminSubmissionViewController", function ($anchorScroll, $cont
         EmailTemplateRepo.getAll(),
         FieldPredicateRepo.getAll(),
         DepositLocationRepo.getAll(),
-        userModel.ready()
+        userSettingsUnfetched.ready()
     ]);
 
     ready.then(function (resolved) {
