58d0aea498246c9f4dc7eb5343e7d9e4d6e81f30
William Welling <wwelling@library.tamu.edu>
2017-09-12 08:24:52 -0500

    refactored missing configuration repo

--- src/main/webapp/WEB-INF/app/controllers/submission/newSubmissionController.js
+++ src/main/webapp/WEB-INF/app/controllers/submission/newSubmissionController.js
@@ -1,4 +1,4 @@
-vireo.controller('NewSubmissionController', function($controller, $location, $q, $scope, OrganizationRepo, StudentSubmissionRepo, ConfigurationRepo, SubmissionStates) {
+vireo.controller('NewSubmissionController', function($controller, $location, $q, $scope, OrganizationRepo, StudentSubmissionRepo, ManagedConfigurationRepo, SubmissionStates) {
 
     angular.extend(this, $controller('AbstractController', {$scope: $scope}));
 
@@ -6,9 +6,7 @@ vireo.controller('NewSubmissionController', function($controller, $location, $q,
 
     $scope.selectedOrganization = OrganizationRepo.getSelectedOrganization();
 
-    ConfigurationRepo.ready().then(function() {
-        $scope.configuration = ConfigurationRepo.getAll();
-    });
+    $scope.configuration = ManagedConfigurationRepo.getAll();
 
     var studentSubmissions = StudentSubmissionRepo.getAll();
 
