6f30a578f8b24c222d93662ff43d46d56f571df0
William Welling <wwelling@library.tamu.edu>
2017-10-11 16:27:40 -0500

    using weaver ordered repo for degree repo, removed jpa query

--- src/main/java/org/tdl/vireo/model/repo/DegreeRepo.java
+++ src/main/java/org/tdl/vireo/model/repo/DegreeRepo.java
@@ -1,21 +1,17 @@
 package org.tdl.vireo.model.repo;
 
-import java.util.List;
-
 import org.tdl.vireo.aspect.annotation.EntityCV;
 import org.tdl.vireo.model.Degree;
 import org.tdl.vireo.model.DegreeLevel;
 import org.tdl.vireo.model.repo.custom.DegreeRepoCustom;
 
-import edu.tamu.weaver.data.model.repo.WeaverRepo;
+import edu.tamu.weaver.data.model.repo.WeaverOrderedRepo;
 
 @EntityCV(name = "Degrees")
-public interface DegreeRepo extends WeaverRepo<Degree>, EntityControlledVocabularyRepo<Degree>, DegreeRepoCustom {
+public interface DegreeRepo extends WeaverOrderedRepo<Degree>, EntityControlledVocabularyRepo<Degree>, DegreeRepoCustom {
 
     public Degree findByNameAndLevel(String name, DegreeLevel level);
 
     public Degree findByPosition(Long position);
-
-    public List<Degree> findAllByOrderByPositionAsc();
 
 }
