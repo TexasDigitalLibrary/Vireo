ed69488b58175e72bdd4ab339fb64a7f32e3a898
William Welling <wwelling@library.tamu.edu>
2017-10-23 08:05:13 -0500

    added json identity info on list of controlled vocabularies

--- src/main/java/org/tdl/vireo/model/AbstractFieldProfile.java
+++ src/main/java/org/tdl/vireo/model/AbstractFieldProfile.java
@@ -20,6 +20,9 @@ import javax.persistence.UniqueConstraint;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import edu.tamu.weaver.validation.model.ValidatingBaseEntity;
 
 @Entity
@@ -57,6 +60,7 @@ public abstract class AbstractFieldProfile<FP> extends ValidatingBaseEntity {
     private List<FieldGloss> fieldGlosses;
 
     @ManyToMany(cascade = { REFRESH }, fetch = EAGER)
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, scope = ControlledVocabulary.class, property = "id")
     @Fetch(FetchMode.SELECT)
     private List<ControlledVocabulary> controlledVocabularies;
 
--- src/main/webapp/WEB-INF/app/repo/organizationRepo.js
+++ src/main/webapp/WEB-INF/app/repo/organizationRepo.js
@@ -140,16 +140,16 @@ vireo.repo("OrganizationRepo", function OrganizationRepo($q, Organization, RestA
     };
 
     this.restoreDefaults = function(organization) {
-			angular.extend(this.mapping.restoreDefaults, {'data': organization});
-			var promise = RestApi.post(apiMapping.Organization.restoreDefaults);
-      promise.then(function (res) {
-        if (angular.fromJson(res.body)&&angular.fromJson(res.body).meta.status === "INVALID") {
-            angular.extend(organizationRepo, angular.fromJson(res.body).payload);
-            console.log(organizationRepo);
-        }
-      });
-			return promise;
-		};
+    	angular.extend(this.mapping.restoreDefaults, {'data': organization});
+    	var promise = RestApi.post(apiMapping.Organization.restoreDefaults);
+        promise.then(function (res) {
+            if (angular.fromJson(res.body)&&angular.fromJson(res.body).meta.status === "INVALID") {
+                angular.extend(organizationRepo, angular.fromJson(res.body).payload);
+                console.log(organizationRepo);
+            }
+        });
+		return promise;
+	};
 
     this.updateWorkflowStep = function (workflowStep) {
         organizationRepo.clearValidationResults();
