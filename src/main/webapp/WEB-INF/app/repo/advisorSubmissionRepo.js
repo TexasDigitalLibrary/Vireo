e509cf99d888ae7b014bbc0d0af1fa7f0a15ce30
William Welling <wwelling@library.tamu.edu>
2017-10-26 16:10:16 -0500

    fixed advisor review page

--- src/main/webapp/WEB-INF/app/repo/advisorSubmissionRepo.js
+++ src/main/webapp/WEB-INF/app/repo/advisorSubmissionRepo.js
@@ -1,24 +1,25 @@
-vireo.repo("AdvisorSubmissionRepo", function AdvisorSubmissionRepo(AdvisorSubmission, WsApi) {
+vireo.repo("AdvisorSubmissionRepo", function AdvisorSubmissionRepo($q, AdvisorSubmission, WsApi) {
 
-	var advisorSubmissionRepo = this;
+    var advisorSubmissionRepo = this;
 
     advisorSubmissionRepo.findSubmissionByhash = function (hash) {
         var defer = $q.defer();
         advisorSubmissionRepo.clearValidationResults();
         angular.extend(advisorSubmissionRepo.mapping.getByHash, {
-			'method': 'advisor-review/' + hash
-		});
+            'method': 'advisor-review/' + hash
+        });
         var fetchPromise = WsApi.fetch(advisorSubmissionRepo.mapping.getByHash);
-        fetchPromise.then(function(res) {
+        fetchPromise.then(function (res) {
             var resObj = angular.fromJson(res.body);
             if (resObj.meta.status !== "ERROR") {
-                advisorSubmissionRepo.add(resObj.payload.Submission);
-                defer.resolve(advisorSubmissionRepo.findById(id));
+                var submission = resObj.payload.Submission;
+                advisorSubmissionRepo.add(submission);
+                defer.resolve(advisorSubmissionRepo.findById(submission.id));
             }
         });
         return defer.promise;
     };
 
-	return advisorSubmissionRepo;
+    return advisorSubmissionRepo;
 
 });
