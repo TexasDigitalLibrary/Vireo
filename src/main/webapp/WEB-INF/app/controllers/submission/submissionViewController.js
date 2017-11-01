a7e7a01a084e9bb11135dc34afb2b337aa1b2c90
William Welling <wwelling@library.tamu.edu>
2017-11-01 08:57:59 -0500

    fixed upload file section of submission view

--- src/main/webapp/WEB-INF/app/controllers/submission/submissionViewController.js
+++ src/main/webapp/WEB-INF/app/controllers/submission/submissionViewController.js
@@ -1,6 +1,8 @@
-vireo.controller("SubmissionViewController", function($controller, $filter, $q, $scope, $routeParams, FieldPredicateRepo, FileUploadService, StudentSubmissionRepo, StudentSubmission, SubmissionStates) {
+vireo.controller("SubmissionViewController", function ($controller, $filter, $q, $scope, $routeParams, FieldPredicateRepo, FileUploadService, StudentSubmissionRepo, StudentSubmission, SubmissionStates) {
 
-    angular.extend(this, $controller('AbstractController', {$scope: $scope}));
+    angular.extend(this, $controller('AbstractController', {
+        $scope: $scope
+    }));
 
     $scope.fieldPredicate = {};
 
@@ -8,37 +10,37 @@ vireo.controller("SubmissionViewController", function($controller, $filter, $q, 
 
     $scope.SubmissionStates = SubmissionStates;
 
-    StudentSubmissionRepo.findSubmissionById($routeParams.submissionId).then(function(submission) {
+    StudentSubmissionRepo.findSubmissionById($routeParams.submissionId).then(function (submission) {
         $scope.loaded = true;
         $scope.submission = new StudentSubmission(submission);
     });
 
-    var deleteFieldValue = function(fieldValue) {
+    var deleteFieldValue = function (fieldValue) {
         fieldValue.removing = true;
-        FileUploadService.removeFile($scope.submission, fieldValue).then(function() {
+        FileUploadService.removeFile($scope.submission, fieldValue).then(function () {
             delete fieldValue.removing;
         });
     };
 
-    $scope.updateActionLogLimit = function() {
+    $scope.updateActionLogLimit = function () {
         $scope.actionLogCurrentLimit = $scope.actionLogCurrentLimit === $scope.actionLogLimit ? $scope.submission.actionLogs.length : $scope.actionLogLimit;
     };
 
     $scope.message = '';
 
-    $scope.addMessage = function() {
+    $scope.addMessage = function () {
         $scope.messaging = true;
-        $scope.submission.addMessage($scope.message).then(function() {
+        $scope.submission.addMessage($scope.message).then(function () {
             $scope.messaging = false;
             $scope.message = '';
         });
     };
 
-    $scope.getFileType = function(fieldPredicate) {
+    $scope.getFileType = function (fieldPredicate) {
         return FileUploadService.getFileType(fieldPredicate);
     };
 
-    $scope.isPrimaryDocument = function(fieldPredicate) {
+    $scope.isPrimaryDocument = function (fieldPredicate) {
         return $scope.getFileType(fieldPredicate) == 'PRIMARY';
     };
 
@@ -48,7 +50,7 @@ vireo.controller("SubmissionViewController", function($controller, $filter, $q, 
 
     $scope.archivingManuscript = false;
 
-    $scope.queueRemove = function(fieldValue) {
+    $scope.queueRemove = function (fieldValue) {
         var index = $scope.removeQueue.indexOf(fieldValue);
         if (index >= 0) {
             $scope.removeQueue.splice(index, 1);
@@ -57,27 +59,27 @@ vireo.controller("SubmissionViewController", function($controller, $filter, $q, 
         }
     };
 
-    $scope.removeAdditionalUploads = function() {
+    $scope.removeAdditionalUploads = function () {
         $scope.removingUploads = true;
         var removePromises = [];
         for (var i in $scope.removeQueue) {
             var fieldValue = $scope.removeQueue[i];
             removePromises.push(deleteFieldValue(fieldValue));
         }
-        $q.all(removePromises).then(function() {
+        $q.all(removePromises).then(function () {
             $scope.removingUploads = false;
         });
     };
 
-    $scope.cancelUpload = function() {
+    $scope.cancelUpload = function () {
         $scope.removeQueue = [];
         $scope.removingUploads = false;
         $scope.closeModal();
     };
 
-    $scope.archiveManuscript = function() {
+    $scope.archiveManuscript = function () {
         $scope.archivingManuscript = true;
-        FileUploadService.archiveFile($scope.submission, $scope.submission.primaryDocumentFieldValue).then(function() {
+        FileUploadService.archiveFile($scope.submission, $scope.submission.primaryDocumentFieldValue).then(function () {
             $scope.archivingManuscript = false;
             $scope.submission.addFieldValue($scope.submission.getPrimaryDocumentFieldProfile().fieldPredicate);
         });
