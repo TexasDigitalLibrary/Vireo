11788e53b5c607a4cd422037eff204e42d2553be
William Welling <wwelling@library.tamu.edu>
2017-11-20 18:14:56 -0600

    fixed xml template resolver, minor refactor

--- src/main/java/org/tdl/vireo/Application.java
+++ src/main/java/org/tdl/vireo/Application.java
@@ -6,13 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 
 import edu.tamu.weaver.WeaverInitializer;
 
-@SpringBootApplication
 @ComponentScan(basePackages = { "edu.tamu.*", "org.tdl.*" })
+@SpringBootApplication(exclude = { ThymeleafAutoConfiguration.class})
 public class Application extends WeaverInitializer {
 
     private final static Logger LOG = LoggerFactory.getLogger(Application.class);
---
+++ src/main/java/org/tdl/vireo/config/XmlTemplateConfig.java
@@ -0,0 +1,33 @@
+package org.tdl.vireo.config;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+
+@Configuration
+public class XmlTemplateConfig implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver xmlTemplateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext(applicationContext);
+        resolver.setPrefix("classpath:/formats/");
+        resolver.setSuffix(".xml");
+        resolver.setTemplateMode("XML");
+        resolver.setCharacterEncoding(UTF_8.name());
+        resolver.setCheckExistence(true);
+        resolver.setCacheable(false);
+        return resolver;
+    }
+
+}
