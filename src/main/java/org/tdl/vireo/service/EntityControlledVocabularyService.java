a02ce8f02a32849b3bd8422448f6c1c26a5bcddd
William Welling <wwelling@library.tamu.edu>
2017-12-08 07:27:50 -0600

    refactored updated entity utility package location

--- src/main/java/org/tdl/vireo/service/EntityControlledVocabularyService.java
+++ src/main/java/org/tdl/vireo/service/EntityControlledVocabularyService.java
@@ -24,7 +24,7 @@ import org.tdl.vireo.model.repo.ControlledVocabularyRepo;
 import org.tdl.vireo.model.repo.EntityControlledVocabularyRepo;
 import org.tdl.vireo.model.repo.LanguageRepo;
 
-import edu.tamu.weaver.utility.EntityUtility;
+import edu.tamu.weaver.data.utility.EntityUtility;
 
 @Service
 public class EntityControlledVocabularyService {
