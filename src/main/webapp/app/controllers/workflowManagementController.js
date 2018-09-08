557215b9db7ec797534168edfe80ce292a7f3a11
William Welling <wwelling@outlook.com>
2018-09-08 12:13:08 -0500

    update workflow management settings and file naming cleanup

--- src/main/webapp/app/controllers/workflowManagementController.js
+++ src/main/webapp/app/controllers/workflowManagementController.js
@@ -2,6 +2,9 @@ vireo.controller('WorkflowManagementController', function ($controller, $scope, 
 
     angular.extend(this, $controller('AbstractController', {$scope: $scope}));
 
-    SidebarService.addBoxes([]);
+    SidebarService.addBoxes([{
+        "title": "Workflow Management",
+        "viewUrl": "views/sideboxes/workflowManagement.html"
+    }]);
 
 });
