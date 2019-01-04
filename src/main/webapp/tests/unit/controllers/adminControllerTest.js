6cfe03fafdab8767434091c9d2ff0561db12e142
Kevin Day <kday@library.tamu.edu>
2019-01-04 10:51:26 -0600

    Implement Tests for AdminController

--- src/main/webapp/tests/unit/controllers/adminControllerTest.js
+++ src/main/webapp/tests/unit/controllers/adminControllerTest.js
@@ -1,13 +1,15 @@
 describe('controller: AdminController', function () {
 
-    var controller, scope;
+    var controller, scope, location;
 
     var initializeController = function(settings) {
         inject(function ($controller, $location, $rootScope, $window, _ModalService_, _RestApi_, _StorageService_, _WsApi_) {
             installPromiseMatchers();
+            location = $location;
             scope = $rootScope.$new();
 
             sessionStorage.role = settings && settings.role ? settings.role : "ROLE_ADMIN";
+            sessionStorage.token = settings && settings.token ? settings.token : "faketoken";
 
             controller = $controller('AdminController', {
                 $scope: scope,
@@ -39,6 +41,68 @@ describe('controller: AdminController', function () {
     describe('Is the controller defined', function () {
         it('should be defined', function () {
             expect(controller).toBeDefined();
+        });
+    });
+
+    describe('Are the scope methods defined', function () {
+        it('isList should be defined', function () {
+            expect(scope.isList).toBeDefined();
+            expect(typeof scope.isList).toEqual("function");
+        });
+        it('isLog should be defined', function () {
+            expect(scope.isLog).toBeDefined();
+            expect(typeof scope.isLog).toEqual("function");
+        });
+        it('isSettings should be defined', function () {
+            expect(scope.isSettings).toBeDefined();
+            expect(typeof scope.isSettings).toEqual("function");
+        });
+        it('isView should be defined', function () {
+            expect(scope.isView).toBeDefined();
+            expect(typeof scope.isView).toEqual("function");
+        });
+    });
+
+    describe('Do the scope methods work as expected', function () {
+        it('isList should return a boolean', function () {
+            var result;
+
+            result = scope.isList();
+            expect(result).toBe(false);
+
+            spyOn(location, 'path').and.returnValue('/admin/list');
+            result = scope.isList();
+            expect(result).toBe(true);
+        });
+        it('isLog should return a boolean', function () {
+            var result;
+
+            result = scope.isLog();
+            expect(result).toBe(false);
+
+            spyOn(location, 'path').and.returnValue('/admin/log');
+            result = scope.isLog();
+            expect(result).toBe(true);
+        });
+        it('isSettings should return a boolean', function () {
+            var result;
+
+            result = scope.isSettings();
+            expect(result).toBe(false);
+
+            spyOn(location, 'path').and.returnValue('/admin/settings');
+            result = scope.isSettings();
+            expect(result).toBe(true);
+        });
+        it('isView should return a boolean', function () {
+            var result;
+
+            result = scope.isView();
+            expect(result).toBe(false);
+
+            spyOn(location, 'path').and.returnValue('/admin/view');
+            result = scope.isView();
+            expect(result).toBe(true);
         });
     });
 });
