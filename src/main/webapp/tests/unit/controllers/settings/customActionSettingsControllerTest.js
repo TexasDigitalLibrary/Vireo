cc1e431ec0b52f86c7e48b68090bb61311e1cbaf
Kevin Day <kday@library.tamu.edu>
2019-01-08 11:26:21 -0600

    Fix ordering of tests in CustomActionSettingsController

--- src/main/webapp/tests/unit/controllers/settings/customActionSettingsControllerTest.js
+++ src/main/webapp/tests/unit/controllers/settings/customActionSettingsControllerTest.js
@@ -56,25 +56,13 @@ describe('controller: CustomActionSettingsController', function () {
     });
 
     describe('Are the scope methods defined', function () {
-        it('resetCustomAction should be defined', function () {
-            expect(scope.resetCustomAction).toBeDefined();
-            expect(typeof scope.resetCustomAction).toEqual("function");
-        });
         it('createCustomAction should be defined', function () {
             expect(scope.createCustomAction).toBeDefined();
             expect(typeof scope.createCustomAction).toEqual("function");
         });
-        it('selectCustomAction should be defined', function () {
-            expect(scope.selectCustomAction).toBeDefined();
-            expect(typeof scope.selectCustomAction).toEqual("function");
-        });
         it('editCustomAction should be defined', function () {
             expect(scope.editCustomAction).toBeDefined();
             expect(typeof scope.editCustomAction).toEqual("function");
-        });
-        it('updateCustomAction should be defined', function () {
-            expect(scope.updateCustomAction).toBeDefined();
-            expect(typeof scope.updateCustomAction).toEqual("function");
         });
         it('removeCustomAction should be defined', function () {
             expect(scope.removeCustomAction).toBeDefined();
@@ -84,25 +72,21 @@ describe('controller: CustomActionSettingsController', function () {
             expect(scope.reorderCustomAction).toBeDefined();
             expect(typeof scope.reorderCustomAction).toEqual("function");
         });
+        it('resetCustomAction should be defined', function () {
+            expect(scope.resetCustomAction).toBeDefined();
+            expect(typeof scope.resetCustomAction).toEqual("function");
+        });
+        it('selectCustomAction should be defined', function () {
+            expect(scope.selectCustomAction).toBeDefined();
+            expect(typeof scope.selectCustomAction).toEqual("function");
+        });
+        it('updateCustomAction should be defined', function () {
+            expect(scope.updateCustomAction).toBeDefined();
+            expect(typeof scope.updateCustomAction).toEqual("function");
+        });
     });
 
     describe('Do the scope methods work as expected', function () {
-        it('resetCustomAction should reset the custom action', function () {
-            var cad = new mockCustomActionDefinition(q);
-            scope.forms = [];
-            scope.modalData = cad;
-
-            spyOn(scope.customActionRepo, "clearValidationResults");
-            spyOn(cad, "refresh");
-            spyOn(scope, "closeModal");
-
-            scope.resetCustomAction();
-
-            expect(scope.customActionRepo.clearValidationResults).toHaveBeenCalled();
-            expect(cad.refresh).toHaveBeenCalled();
-            expect(scope.closeModal).toHaveBeenCalled();
-            expect(scope.modalData.isStudentVisible).toBe(false);
-        });
         it('createCustomAction should create a new custom action', function () {
             scope.modalData = new mockCustomActionDefinition(q);
 
@@ -112,18 +96,6 @@ describe('controller: CustomActionSettingsController', function () {
 
             expect(CustomActionDefinitionRepo.create).toHaveBeenCalled();
         });
-        it('selectCustomAction should select a custom action', function () {
-            scope.modalData = null;
-            scope.customActions = [
-                new mockCustomActionDefinition(q),
-                new mockCustomActionDefinition(q)
-            ];
-            scope.customActions[1].mock(mockCustomActionDefinition2);
-
-            scope.selectCustomAction(1);
-
-            expect(scope.modalData).toBe(scope.customActions[1]);
-        });
         it('editCustomAction should open a modal', function () {
             spyOn(scope, "selectCustomAction");
             spyOn(scope, "openModal");
@@ -132,15 +104,6 @@ describe('controller: CustomActionSettingsController', function () {
 
             expect(scope.selectCustomAction).toHaveBeenCalled();
             expect(scope.openModal).toHaveBeenCalled();
-        });
-        it('updateCustomAction should should save a custom action', function () {
-            scope.modalData = new mockCustomActionDefinition(q);
-
-            spyOn(scope.modalData, "save");
-
-            scope.updateCustomAction();
-
-            expect(scope.modalData.save).toHaveBeenCalled();
         });
         it('removeCustomAction should delete a custom action', function () {
             scope.modalData = new mockCustomActionDefinition(q);
@@ -157,6 +120,43 @@ describe('controller: CustomActionSettingsController', function () {
             scope.reorderCustomAction("a", "b");
 
             expect(CustomActionDefinitionRepo.reorder).toHaveBeenCalled();
+        });
+        it('resetCustomAction should reset the custom action', function () {
+            var cad = new mockCustomActionDefinition(q);
+            scope.forms = [];
+            scope.modalData = cad;
+
+            spyOn(scope.customActionRepo, "clearValidationResults");
+            spyOn(cad, "refresh");
+            spyOn(scope, "closeModal");
+
+            scope.resetCustomAction();
+
+            expect(scope.customActionRepo.clearValidationResults).toHaveBeenCalled();
+            expect(cad.refresh).toHaveBeenCalled();
+            expect(scope.closeModal).toHaveBeenCalled();
+            expect(scope.modalData.isStudentVisible).toBe(false);
+        });
+        it('selectCustomAction should select a custom action', function () {
+            scope.modalData = null;
+            scope.customActions = [
+                new mockCustomActionDefinition(q),
+                new mockCustomActionDefinition(q)
+            ];
+            scope.customActions[1].mock(mockCustomActionDefinition2);
+
+            scope.selectCustomAction(1);
+
+            expect(scope.modalData).toBe(scope.customActions[1]);
+        });
+        it('updateCustomAction should should save a custom action', function () {
+            scope.modalData = new mockCustomActionDefinition(q);
+
+            spyOn(scope.modalData, "save");
+
+            scope.updateCustomAction();
+
+            expect(scope.modalData.save).toHaveBeenCalled();
         });
     });
 
