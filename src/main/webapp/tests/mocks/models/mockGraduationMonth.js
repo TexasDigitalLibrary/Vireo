22a82ccd5e7305525f2713723432760fd20482d8
Kevin Day <kday@library.tamu.edu>
2019-01-09 16:18:26 -0600

    Implement Tests for GraduationMonthRepoController

--- src/main/webapp/tests/mocks/models/mockGraduationMonth.js
+++ src/main/webapp/tests/mocks/models/mockGraduationMonth.js
@@ -1,25 +1,31 @@
 var dataGraduationMonth1 = {
-    id: 1
+    id: 1,
+    month: 1
 };
 
 var dataGraduationMonth2 = {
-    id: 2
+    id: 2,
+    month: 2
 };
 
 var dataGraduationMonth3 = {
-    id: 3
+    id: 3,
+    month: 3
 };
 
 var dataGraduationMonth4 = {
-    id: 4
+    id: 4,
+    month: 4
 };
 
 var dataGraduationMonth5 = {
-    id: 5
+    id: 5,
+    month: 5
 };
 
 var dataGraduationMonth6 = {
-    id: 6
+    id: 6,
+    month: 6
 };
 
 var mockGraduationMonth = function($q) {
