e1f9c08533f3d3bc0368e26670e46316f47e343b
Kevin Day <kday@library.tamu.edu>
2019-01-11 13:53:47 -0600

    Implement Tests for StudentSubmissionController

--- src/main/webapp/tests/unit/controllers/submission/studentSubmissionControllerTest.js
+++ src/main/webapp/tests/unit/controllers/submission/studentSubmissionControllerTest.js
@@ -1,14 +1,23 @@
 describe('controller: StudentSubmissionController', function () {
 
-    var controller, scope;
+    var controller, q, location, routeParams, scope, timeout;
 
     var initializeController = function(settings) {
-        inject(function ($anchorScroll, $controller, $location, $rootScope, $routeParams, $timeout, $window, _ManagedConfigurationRepo_, _ModalService_, _RestApi_, _StorageService_, _StudentSubmissionRepo_, _WsApi_) {
+        inject(function ($anchorScroll, $controller, $location, $q, $rootScope, $routeParams, $timeout, $window, _ManagedConfigurationRepo_, _ModalService_, _RestApi_, _StorageService_, _StudentSubmissionRepo_, _WsApi_) {
             installPromiseMatchers();
             scope = $rootScope.$new();
 
+            location = $location;
+            q = $q;
+            routeParams = $routeParams;
+            timeout = $timeout;
+
             sessionStorage.role = settings && settings.role ? settings.role : "ROLE_ADMIN";
             sessionStorage.token = settings && settings.token ? settings.token : "faketoken";
+
+            if (settings && settings.stepNum !== undefined) {
+                $routeParams.stepNum = settings.stepNum;
+            }
 
             controller = $controller('StudentSubmissionController', {
                 $anchorScroll: $anchorScroll,
@@ -51,6 +60,73 @@ describe('controller: StudentSubmissionController', function () {
     describe('Is the controller defined', function () {
         it('should be defined', function () {
             expect(controller).toBeDefined();
+        });
+    });
+
+    describe('Are the scope methods defined', function () {
+        it('onLastStep should be defined', function () {
+            expect(scope.onLastStep).toBeDefined();
+            expect(typeof scope.onLastStep).toEqual("function");
+        });
+        it('reviewSubmission should be defined', function () {
+            expect(scope.reviewSubmission).toBeDefined();
+            expect(typeof scope.reviewSubmission).toEqual("function");
+        });
+        it('setActiveStep should be defined', function () {
+            expect(scope.setActiveStep).toBeDefined();
+            expect(typeof scope.setActiveStep).toEqual("function");
+        });
+        it('submit should be defined', function () {
+            expect(scope.submit).toBeDefined();
+            expect(typeof scope.submit).toEqual("function");
+        });
+    });
+
+    describe('Do the scope methods work as expected', function () {
+        it('onLastStep should return a boolean', function () {
+            var response;
+            scope.submission = new mockSubmission(q);
+            scope.submission.submissionWorkflowSteps = [ new mockWorkflowStep(q) ];
+
+            response = scope.onLastStep();
+
+            expect(typeof response).toBe("boolean");
+        });
+        it('reviewSubmission should call set the active step', function () {
+            spyOn(scope, "setActiveStep");
+
+            scope.reviewSubmission();
+
+            expect(scope.setActiveStep).toHaveBeenCalled();
+        });
+        it('setActiveStep should set the active step', function () {
+            var step = new mockWorkflowStep(q);
+            var hash = "test";
+            scope.submission = mockSubmission(q);
+            scope.submission.submissionWorkflowSteps = [ step ];
+
+            scope.submitting = true;
+            scope.setActiveStep(step, hash);
+
+            scope.submitting = false;
+            scope.setActiveStep(step, hash);
+
+            step.name = "review";
+            scope.setActiveStep(step, hash);
+
+            step = false;
+            initializeController({stepNum: 1});
+            scope.setActiveStep(step, hash);
+
+            timeout.flush();
+        });
+        it('submit should submit and change path', function () {
+            spyOn(location, "path");
+
+            scope.submit();
+            scope.$digest();
+
+            expect(location.path).toHaveBeenCalled();
         });
     });
 });
