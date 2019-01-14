b3b6fa04d351598d5c0d047afc29cb7016807f86
Kevin Day <kday@library.tamu.edu>
2019-01-14 16:46:29 -0600

    Implement tests for NamedSearchFilterGroup

--- src/main/webapp/tests/unit/models/namedSearchFilterGroupTest.js
+++ src/main/webapp/tests/unit/models/namedSearchFilterGroupTest.js
@@ -21,4 +21,58 @@ describe('model: NamedSearchFilterGroup', function () {
             expect(model).toBeDefined();
         });
     });
+
+    describe('Are the model methods defined', function () {
+        it('addFilter should be defined', function () {
+            expect(model.addFilter).toBeDefined();
+            expect(typeof model.addFilter).toEqual("function");
+        });
+        it('clearFilters should be defined', function () {
+            expect(model.clearFilters).toBeDefined();
+            expect(typeof model.clearFilters).toEqual("function");
+        });
+        it('removeFilter should be defined', function () {
+            expect(model.removeFilter).toBeDefined();
+            expect(typeof model.removeFilter).toEqual("function");
+        });
+        it('set should be defined', function () {
+            expect(model.set).toBeDefined();
+            expect(typeof model.set).toEqual("function");
+        });
+    });
+
+    describe('Are the model methods working as expected', function () {
+        it('addFilter should call WsApi', function () {
+            spyOn(WsApi, 'fetch');
+
+            model.addFilter();
+            scope.$digest();
+
+            expect(WsApi.fetch).toHaveBeenCalled();
+        });
+        it('clearFilters should call WsApi', function () {
+            spyOn(WsApi, 'fetch');
+
+            model.clearFilters();
+            scope.$digest();
+
+            expect(WsApi.fetch).toHaveBeenCalled();
+        });
+        it('removeFilter should call WsApi', function () {
+            spyOn(WsApi, 'fetch');
+
+            model.removeFilter();
+            scope.$digest();
+
+            expect(WsApi.fetch).toHaveBeenCalled();
+        });
+        it('set should call WsApi', function () {
+            spyOn(WsApi, 'fetch');
+
+            model.set();
+            scope.$digest();
+
+            expect(WsApi.fetch).toHaveBeenCalled();
+        });
+    });
 });
