531fc2f16b17281e261b259eeb89b89c10ff2388
jeremythuff <jeremythuff@gmail.com>
2019-01-24 10:25:17 -0600

    Added input type select to potential recipients

--- src/main/webapp/app/controllers/settings/emailWorkflowRulesController.js
+++ src/main/webapp/app/controllers/settings/emailWorkflowRulesController.js
@@ -29,9 +29,14 @@ vireo.controller("EmailWorkflowRulesController", function ($controller, $scope, 
             }
         ];
 
+        var resipientInputTypes = [
+          InputTypes.INPUT_CONTACT,
+          InputTypes.INPUT_CONTACT_SELECT
+        ];
+
         angular.forEach(OrganizationRepo.getSelectedOrganization().aggregateWorkflowSteps, function (aggregateWorkflowStep) {
             angular.forEach(aggregateWorkflowStep.aggregateFieldProfiles, function (aggregateFieldProfile) {
-                if (aggregateFieldProfile.inputType.name === InputTypes.INPUT_CONTACT) {
+                if (resipientInputTypes.indexOf(aggregateFieldProfile.inputType.name) !== -1) {
                     $scope.recipients.push({
                         name: aggregateFieldProfile.gloss,
                         type: EmailRecipientType.CONTACT,
