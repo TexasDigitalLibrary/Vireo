be79aa7bd89fdf5bdea1fefdf2f02ce2d7be4497
Christopher Starcher <cstarcher@gmail.com>
2019-01-29 15:43:07 -0600

    fixed gruntfile path

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,7 +75,7 @@ module.exports = function (grunt) {
 
                     'node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
 
-                    'node_modules/file-saver/dist/FileSaver.min.js'
+                    'node_modules/file-saver/src/FileSaver.min.js'
                 ],
                 dest: '<%= build.app %>/resources/scripts/vendor_concat.js'
             },
