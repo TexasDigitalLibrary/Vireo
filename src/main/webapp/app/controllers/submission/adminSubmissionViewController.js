10dc6d3242c7f63b6fb863021421cf8c6c0d3bbf
Christopher Starcher <cstarcher@gmail.com>
2019-03-08 13:26:33 -0600

    fixed submission date update in submission status sidebox

--- src/main/webapp/app/controllers/submission/adminSubmissionViewController.js
+++ src/main/webapp/app/controllers/submission/adminSubmissionViewController.js
@@ -53,6 +53,8 @@ vireo.controller("AdminSubmissionViewController", function ($anchorScroll, $cont
         });
         // update current submissions status
         angular.extend($scope.submission.submissionStatus, submission.submissionStatus);
+        // update current submission date
+        $scope.submission.submissionDate = $scope.submission.submissionDate ? angular.copy($scope.submission.submissionDate, submission.submissionDate) : $scope.submission.submissionDate = submission.submissionDate;
         // fetch file info
         $scope.submission.fetchDocumentTypeFileInfo();
     };
