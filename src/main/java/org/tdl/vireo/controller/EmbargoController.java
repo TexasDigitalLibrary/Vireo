b8268bc30e72f2a2a5275001d11ce4e68030dbaf
Christopher Starcher <cstarcher@gmail.com>
2022-04-01 11:18:23 -0500

    removed preauthorize from embargos mapping

--- src/main/java/org/tdl/vireo/controller/EmbargoController.java
+++ src/main/java/org/tdl/vireo/controller/EmbargoController.java
@@ -34,7 +34,6 @@ public class EmbargoController {
     private EmbargoRepo embargoRepo;
 
     @GetMapping("/all")
-    @PreAuthorize("hasRole('ANONYMOUS')")
     public ApiResponse getEmbargoes() {
         return new ApiResponse(SUCCESS, embargoRepo.findAllByOrderByGuarantorAscPositionAsc());
     }
