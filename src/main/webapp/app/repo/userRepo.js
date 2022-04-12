8a9e3449d87a3a404572b89ed014d4a1eaef0b67
Kevin Day <kday@library.tamu.edu>
2022-04-12 16:33:23 -0500

    1549: UI: Too many users, needs pagination to limit as well as a paginated table for "Who has Access" and "Add Member".
    Change existing endpoints and add new endpoints.
    - /assignable
    - /assignable/total
    - /unassignable
    - /unassignable/total
    
    These new endpoints now support pagination.
    
    Rather than changing the UI internal design, I opted to just create a new endpoint for getting the totals.
    These are needed for proper pagination support.
    
    There is a work-around (see code comments) that allows for Pageable to have a default value of 0.
    This means load all results without pagination.
    In such a case, filtering is still supported.
    
    In order to achive the filter, a synthetic DB column "name" is generated and is including at the DB level.
    This is achieved by utilizing `@Formula` annotation.
    This annotationr equired that I explicitly set the name annotation property for the firstName and lastName User properties.
    
    Several JPA functions are added to accomplish all of the necessary tasks.
    
    The UI loads the totals first and if the total is non-zero, then loads the actual data.
    
    The existing filter from Add Member is removed in favor of the ng-table filter.
    
    There is a lot more cleanup that could be done but for brevity and time-limits these potential improvements are ignored.
    
    Problems that are not resolved:
    - There is a flicker on filter changes or pagination that is annoying.
    - Changes to either form still require full page refreshes.

--- src/main/webapp/app/repo/userRepo.js
+++ src/main/webapp/app/repo/userRepo.js
@@ -26,22 +26,58 @@ vireo.repo("UserRepo", function UserRepo($timeout, TableFactory, User, WsApi) {
         repo: userRepo
     });
 
-    userRepo.getAssignableUsers = function() {
+    userRepo.getAssignableUsers = function(page, size, name) {
         var assignable = [];
+
+        if (size > 0) {
+            angular.extend(userRepo.mapping.assignable, {
+                query: {
+                  page: page,
+                  size: size,
+                  name: name
+                }
+            });
+        }
+
         WsApi.fetch(userRepo.mapping.assignable).then(function(response) {
             var resObj = angular.fromJson(response.body);
             if (resObj.meta.status === 'SUCCESS') {
                 var users = resObj.payload['ArrayList<User>'];
-                for(var i in users) {
+                for (var i in users) {
                     assignable.push(new User(users[i]));
                 }
             }
         });
+
         return assignable;
     };
 
-    userRepo.getUnassignableUsers = function() {
+    userRepo.getAssignableUsersTotal = function(name) {
+        var total = 0;
+
+        if (angular.isDefined(name)) {
+            angular.extend(userRepo.mapping.assignableTotal, {
+                query: {
+                  name: name
+                }
+            });
+        }
+
+        return WsApi.fetch(userRepo.mapping.assignableTotal);
+    };
+
+    userRepo.getUnassignableUsers = function(page, size, name) {
         var unassignable = [];
+
+        if (size > 0) {
+            angular.extend(userRepo.mapping.unassignable, {
+                query: {
+                  page: page,
+                  size: size,
+                  name: name
+                }
+            });
+        }
 
         WsApi.fetch(userRepo.mapping.unassignable).then(function(response) {
             var resObj = angular.fromJson(response.body);
@@ -56,6 +92,20 @@ vireo.repo("UserRepo", function UserRepo($timeout, TableFactory, User, WsApi) {
         });
 
         return unassignable;
+    };
+
+    userRepo.getUnassignableUsersTotal = function(name) {
+        var total = 0;
+
+        if (angular.isDefined(name)) {
+            angular.extend(userRepo.mapping.unassignableTotal, {
+                query: {
+                  name: name
+                }
+            });
+        }
+
+        return WsApi.fetch(userRepo.mapping.unassignableTotal);
     };
 
     WsApi.listen(userRepo.mapping.createListen).then(null, null, function (response) {
