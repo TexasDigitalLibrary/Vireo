d9b8dcbe7d2ac1b90b2ab4042eefae2d6074c3a9
William Welling <wwelling@outlook.com>
2023-05-03 16:28:12 -0500

    Update mock file service and submission unit test

--- src/main/webapp/tests/mocks/services/mockFileService.js
+++ src/main/webapp/tests/mocks/services/mockFileService.js
@@ -1,7 +1,12 @@
 angular.module("mock.fileService", []).service("FileService", function ($q) {
     var service = mockService($q);
 
-    service.download = function (submission, fieldValue) {
+    service.download = function (req) {
+        var download = {};
+        return download;
+    };
+
+    service.anonymousDownload = function (req) {
         var download = {};
         return download;
     };
