378d83a651708c7edf57e54689a5aa8564c58443
Kevin Day <kday@tamu.edu>
2023-07-28 10:37:08 -0500

    Issue 1842: Clean up the Organization side bar code.

--- src/main/webapp/app/controllers/sidebar/organizationSideBarController.js
+++ src/main/webapp/app/controllers/sidebar/organizationSideBarController.js
@@ -16,59 +16,56 @@ vireo.controller("OrganizationSideBarController", function ($controller, $scope,
 
     $scope.forms = {};
 
-    $scope.ready.then(function () {
+    $scope.organizationCategories = organizationCategories.filter(function (category) {
+        return !!category && category.name !== 'System';
+    });
 
-        $scope.organizationCategories = organizationCategories.filter(function (category) {
-            return category.name !== 'System';
-        });
+    $scope.reset = function () {
+        $scope.organizationRepo.clearValidationResults();
 
-        $scope.reset = function () {
-            $scope.organizationRepo.clearValidationResults();
-
-            for (var key in $scope.forms) {
-                if ($scope.forms[key] !== undefined && !$scope.forms[key].$pristine) {
-                    $scope.forms[key].$setPristine();
-                    $scope.forms[key].$setUntouched();
-                }
+        for (var key in $scope.forms) {
+            if ($scope.forms[key] !== undefined && !$scope.forms[key].$pristine) {
+                $scope.forms[key].$setPristine();
+                $scope.forms[key].$setUntouched();
             }
+        }
 
-            $scope.newOrganization = OrganizationRepo.resetNewOrganization();
+        $scope.newOrganization = OrganizationRepo.resetNewOrganization();
 
-            if ($scope.newOrganization.category === undefined) {
-                $scope.newOrganization.category = $scope.organizationCategories[0];
+        if ($scope.newOrganization.category === undefined) {
+            $scope.newOrganization.category = $scope.organizationCategories[0];
+        }
+
+        if ($scope.newOrganization.parent === undefined) {
+            $scope.newOrganization.parent = $scope.organizations[0];
+        }
+    };
+
+    $scope.createNewOrganization = function (hierarchical) {
+        $scope.creatingNewOrganization = true;
+        var parentOrganization = hierarchical === 'true' ? OrganizationRepo.newOrganization.parent : $scope.organizations[0];
+        OrganizationRepo.create({
+            "name": OrganizationRepo.newOrganization.name,
+            "category": OrganizationRepo.newOrganization.category,
+            "parentOrganization": {
+                "id": parentOrganization.id,
+                "name": parentOrganization.name,
+                "category": parentOrganization.category
             }
-
-            if ($scope.newOrganization.parent === undefined) {
-                $scope.newOrganization.parent = $scope.organizations[0];
-            }
-        };
-
-        $scope.createNewOrganization = function (hierarchical) {
-            $scope.creatingNewOrganization = true;
-            var parentOrganization = hierarchical === 'true' ? OrganizationRepo.newOrganization.parent : $scope.organizations[0];
-            OrganizationRepo.create({
-                "name": OrganizationRepo.newOrganization.name,
-                "category": OrganizationRepo.newOrganization.category,
-                "parentOrganization": {
-                    "id": parentOrganization.id,
-                    "name": parentOrganization.name,
-                    "category": parentOrganization.category
-                }
-            }, parentOrganization).then(function () {
-                $scope.creatingNewOrganization = false;
-                $scope.reset();
-            });
-        };
-
-        OrganizationRepo.getAllSpecific('tree').then(function (orgs) {
-            $scope.organizations.length = 0;
-
-            if (!!orgs && orgs.length > 0) {
-                angular.extend($scope.organizations, orgs);
-            }
-
+        }, parentOrganization).then(function () {
+            $scope.creatingNewOrganization = false;
             $scope.reset();
         });
+    };
+
+    OrganizationRepo.getAllSpecific('tree').then(function (orgs) {
+        $scope.organizations.length = 0;
+
+        if (!!orgs && orgs.length > 0) {
+            angular.extend($scope.organizations, orgs);
+        }
+
+        $scope.reset();
     });
 
 });
