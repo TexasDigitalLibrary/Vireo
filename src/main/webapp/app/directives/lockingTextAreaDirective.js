6b46c4a131dfd67468404cddb9c7ca17668c199a
Christopher Starcher <cstarcher@users.noreply.github.com>
2024-06-22 08:04:12 -0600

    added link functionality to tinymce

--- src/main/webapp/app/directives/lockingTextAreaDirective.js
+++ src/main/webapp/app/directives/lockingTextAreaDirective.js
@@ -56,9 +56,9 @@ vireo.directive("lockingtextarea", function ($timeout) {
                         save();
                     });
                 },
-                toolbar1: "formatselect bold italic underline | bullist numlist undo redo",
+                toolbar1: "formatselect bold italic underline | bullist numlist undo redo | link unlink",
                 theme: "silver",
-                plugins: "autoresize",
+                plugins: "autoresize link autolink",
                 menubar: false,
                 statusbar: false,
                 image_advtab: true,
