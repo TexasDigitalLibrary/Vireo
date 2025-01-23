5dfcd317a782c86aa651c1b12cc75c13f7b3a04a
Christopher Starcher <cstarcher@users.noreply.github.com>
2025-01-23 09:31:07 -0600

    add image upload to tinymce editor

--- src/main/webapp/app/directives/lockingTextAreaDirective.js
+++ src/main/webapp/app/directives/lockingTextAreaDirective.js
@@ -56,14 +56,39 @@ vireo.directive("lockingtextarea", function ($timeout) {
                         save();
                     });
                 },
-                toolbar1: "formatselect bold italic underline | bullist numlist undo redo | link unlink | code",
+                toolbar1: "formatselect bold italic underline | bullist numlist undo redo | link unlink | image | code",
                 theme: "silver",
-                plugins: "autoresize lists advlist link autolink code",
+                plugins: "autoresize lists advlist link autolink image code",
                 menubar: false,
                 statusbar: false,
                 image_advtab: true,
                 height: "100%",
-                width: "100%"
+                width: "100%",
+                file_picker_callback: function (callback, value, meta) {
+                    if (meta.filetype === 'image') {
+                        var input = document.createElement('input');
+                        input.setAttribute('type', 'file');
+                        input.setAttribute('accept', 'image/*');
+
+                        input.onchange = function () {
+                            var file = this.files[0];
+                            var reader = new FileReader();
+
+                            reader.onload = function () {
+                                var id = 'blobid' + (new Date()).getTime();
+                                var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+                                var base64 = reader.result.split(',')[1];
+                                var blobInfo = blobCache.create(id, file, base64);
+                                blobCache.add(blobInfo);
+
+                                callback(blobInfo.blobUri(), { title: file.name });
+                            };
+                            reader.readAsDataURL(file);
+                        };
+
+                        input.click();
+                    }
+                }
             };
 
             $scope.nonWysiwygTyping = function ($event) {
