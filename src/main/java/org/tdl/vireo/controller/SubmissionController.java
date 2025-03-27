957facbb7ff1184bb9cb84ca0e7162571845be04
William Welling <wwelling@tamu.edu>
2025-03-27 11:13:51 -0500

    Undo submission batch export error ApiResponse to be handled in another issue

--- src/main/java/org/tdl/vireo/controller/SubmissionController.java
+++ src/main/java/org/tdl/vireo/controller/SubmissionController.java
@@ -535,7 +535,7 @@ public class SubmissionController {
     private void handleBatchExportError(Exception e, HttpServletResponse response) throws IOException {
         LOG.info("Error With Export", e);
         String responseMessage = "Something went wrong with the export!";
-        ApiResponse apiResponse = ApiResponse.fromException(ERROR, responseMessage, e);
+        ApiResponse apiResponse = new ApiResponse(ERROR, responseMessage);
         response.reset();
         response.setContentType("application/json");
         response.getOutputStream().print(objectMapper.writeValueAsString(apiResponse));
