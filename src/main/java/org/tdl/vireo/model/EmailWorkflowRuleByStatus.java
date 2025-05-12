93e3d047c51000bec0d9d51359b84ce672bcc419
William Welling <wwelling@tamu.edu>
2025-05-12 11:15:14 -0500

    Correctly match previous table name for email workflow rules

--- src/main/java/org/tdl/vireo/model/EmailWorkflowRuleByStatus.java
+++ src/main/java/org/tdl/vireo/model/EmailWorkflowRuleByStatus.java
@@ -11,7 +11,7 @@ import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 @Entity
-@Table(name = "email_workflow_rules")
+@Table(name = "email_workflow_rule")
 public class EmailWorkflowRuleByStatus extends EmailWorkflowRule {
 
     @ManyToOne(fetch = EAGER, optional = false)
